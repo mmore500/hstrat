a87307d3402a0ef70d3a3694506640a90eabf749
Matthew Andres Moreno <mmore500.login+gpg@gmail.com>
2022-02-21 17:45:35 -0500

    Use inheritance to remove redundant code

--- hstrat/stratum_retention_condemners/StratumRetentionCondemnerPerfectResolution.py
+++ hstrat/stratum_retention_condemners/StratumRetentionCondemnerPerfectResolution.py
@@ -1,9 +1,14 @@
 import typing
 
 from ..HereditaryStratum import HereditaryStratum
+from ..stratum_retention_predicates \
+    import StratumRetentionPredicatePerfectResolution
 
 
-class StratumRetentionCondemnerPerfectResolution:
+class StratumRetentionCondemnerPerfectResolution(
+    # inherit CalcNumStrataRetainedUpperBound, etc.
+    StratumRetentionPredicatePerfectResolution,
+):
 
     def __call__(
         self: 'StratumRetentionCondemnerPerfectResolution',
@@ -14,34 +19,3 @@ class StratumRetentionCondemnerPerfectResolution:
         # see https://stackoverflow.com/a/13243870/17332200
         return
         yield
-
-    def __eq__(
-        self: 'StratumRetentionCondemnerPerfectResolution',
-        other: 'StratumRetentionCondemnerPerfectResolution',
-    ) -> bool:
-        if isinstance(other, self.__class__):
-            return self.__dict__ == other.__dict__
-        else:
-            return False
-
-    def CalcNumStrataRetainedUpperBound(
-        self: 'StratumRetentionCondemnerPerfectResolution',
-        num_strata_deposited: int,
-    ) -> int:
-        return num_strata_deposited
-
-    def CalcMrcaUncertaintyUpperBound(
-        self: 'StratumRetentionCondemnerPerfectResolution',
-        *,
-        first_num_strata_deposited: typing.Optional[int]=None,
-        second_num_strata_deposited: typing.Optional[int]=None,
-        actual_rank_of_mrca: typing.Optional[int]=None,
-    ) -> int:
-        return 0
-
-    def CalcRankAtColumnIndex(
-        self: 'StratumRetentionCondemnerPerfectResolution',
-        index: int,
-        num_strata_deposited: typing.Optional[int]=None,
-    ) -> int:
-        return index
