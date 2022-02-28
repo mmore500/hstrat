3a36676d0739889aac5c0222e3ff9e5be2703752
Matthew Andres Moreno <mmore500.login+gpg@gmail.com>
2022-02-28 11:58:25 -0500

    Bump version: 0.0.0 → 0.1.0

--- setup.py
+++ setup.py
@@ -48,6 +48,6 @@ setup(
     test_suite='tests',
     tests_require=test_requirements,
     url='https://github.com/mmore500/hstrat',
-    version='0.0.0',
+    version='0.1.0',
     zip_safe=False,
 )
