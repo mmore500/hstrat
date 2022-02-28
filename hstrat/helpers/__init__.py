281572131807c87a348a05eb69271e0e1e328be0
Matthew Andres Moreno <mmore500.login+gpg@gmail.com>
2022-02-28 16:28:29 -0500

    Remove unused helper

--- hstrat/helpers/__init__.py
+++ hstrat/helpers/__init__.py
@@ -1,8 +1,6 @@
 from .AnyTreeAscendingIter import AnyTreeAscendingIter
-from .attach_attrs import attach_attrs
 
 # adapted from https://stackoverflow.com/a/31079085
 __all__ = [
     'AnyTreeAscendingIter',
-    'attach_attrs',
 ]
