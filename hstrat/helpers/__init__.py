668a4b8a9ab4f67b9c0fcab701bb402d3db01642
Matthew Andres Moreno <mmore500.login+gpg@gmail.com>
2022-04-13 21:24:33 -0400

    Implement bit_ceil, bit_floor helpers

--- hstrat/helpers/__init__.py
+++ hstrat/helpers/__init__.py
@@ -1,6 +1,10 @@
 from .AnyTreeAscendingIter import AnyTreeAscendingIter
+from .bit_ceil import bit_ceil
+from .bit_floor import bit_floor
 
 # adapted from https://stackoverflow.com/a/31079085
 __all__ = [
     'AnyTreeAscendingIter',
+    'bit_ceil',
+    'bit_floor',
 ]
---
+++ hstrat/helpers/bit_floor.py
@@ -0,0 +1,13 @@
+
+def bit_floor(n: int):
+    """Calculate the largest power of two not greater than n.
+
+    If zero, returns zero."""
+
+    if n:
+        # see https://stackoverflow.com/a/14267825/17332200
+        exp = (n // 2).bit_length()
+        res = 1 << exp
+        return res
+    else:
+        return 0
