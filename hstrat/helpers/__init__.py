0bf1ef26d8eddf6ef72eba8312a81725a13c2d2f
Matthew Andres Moreno <mmore500.login+gpg@gmail.com>
2022-05-06 15:07:25 -0400

    Implement, test is_nonincreasing helper

--- hstrat/helpers/__init__.py
+++ hstrat/helpers/__init__.py
@@ -2,6 +2,7 @@ from .AnyTreeAscendingIter import AnyTreeAscendingIter
 from .bit_ceil import bit_ceil
 from .bit_floor import bit_floor
 from .is_nondecreasing import is_nondecreasing
+from .is_nonincreasing import is_nonincreasing
 
 # adapted from https://stackoverflow.com/a/31079085
 __all__ = [
@@ -9,4 +10,5 @@ __all__ = [
     'bit_ceil',
     'bit_floor',
     'is_nondecreasing',
+    'is_nonincreasing',
 ]
---
+++ hstrat/helpers/is_nonincreasing.py
@@ -0,0 +1,11 @@
+
+def is_nonincreasing(seq) -> bool:
+
+    list_ = [*seq]
+    return all(
+        a >= b
+        for a, b in zip(
+            list_,
+            list_[1:],
+        )
+    )
