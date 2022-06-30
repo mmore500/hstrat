8fd9e4a87333f07834ca2c7d0089f8d703ab2c5e
Matthew Andres Moreno <mmore500.login+gpg@gmail.com>
2022-06-29 19:27:11 -0600

    Implement and test div_range helper

--- hstrat/helpers/__init__.py
+++ hstrat/helpers/__init__.py
@@ -1,6 +1,7 @@
 from .AnyTreeAscendingIter import AnyTreeAscendingIter
 from .bit_ceil import bit_ceil
 from .bit_floor import bit_floor
+from .div_range import div_range
 from .is_nondecreasing import is_nondecreasing
 from .is_nonincreasing import is_nonincreasing
 from .memoize_generator import memoize_generator
@@ -10,6 +11,7 @@ __all__ = [
     'AnyTreeAscendingIter',
     'bit_ceil',
     'bit_floor',
+    'div_range',
     'is_nondecreasing',
     'is_nonincreasing',
     'memoize_generator',
