4d70ee4ea3e0c4a37764964350b7fc04f934c231
Matthew Andres Moreno <mmore500.login+gpg@gmail.com>
2022-08-03 15:30:58 -0700

    Implement and test all_same

--- hstrat2/helpers/__init__.py
+++ hstrat2/helpers/__init__.py
@@ -1,3 +1,4 @@
+from .all_same import all_same
 from .AnyTreeAscendingIter import AnyTreeAscendingIter
 from .bit_ceil import bit_ceil
 from .bit_floor import bit_floor
@@ -8,6 +9,7 @@ from .memoize_generator import memoize_generator
 
 # adapted from https://stackoverflow.com/a/31079085
 __all__ = [
+    'all_same',
     'AnyTreeAscendingIter',
     'bit_ceil',
     'bit_floor',
---
+++ hstrat2/helpers/all_same.py
@@ -0,0 +1,18 @@
+import typing
+
+
+#Adapted from https://stackoverflow.com/a/3787989
+def all_same(items: typing.Iterable) -> bool:
+    """Do all items in items compare equal?
+
+    Returns True if items is empty.
+    """
+
+    it = iter(items)
+
+    for first in it:
+        break
+    else:
+        return True  # empty case, note all([]) == True
+
+    return all(x == first for x in it)
