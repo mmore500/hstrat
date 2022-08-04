6956758a13a3010cbcb23c1c7f113c5424e17ef4
Matthew Andres Moreno <mmore500.login+gpg@gmail.com>
2022-08-03 23:08:51 -0700

    Implement and test pairwise helper

--- hstrat2/helpers/__init__.py
+++ hstrat2/helpers/__init__.py
@@ -6,6 +6,7 @@ from .div_range import div_range
 from .is_nondecreasing import is_nondecreasing
 from .is_nonincreasing import is_nonincreasing
 from .memoize_generator import memoize_generator
+from .pairwise import pairwise
 
 # adapted from https://stackoverflow.com/a/31079085
 __all__ = [
@@ -17,4 +18,5 @@ __all__ = [
     'is_nondecreasing',
     'is_nonincreasing',
     'memoize_generator',
+    'pairwise',
 ]
---
+++ hstrat2/helpers/pairwise.py
@@ -0,0 +1,21 @@
+import itertools as it
+import typing
+
+# adapted from https://docs.python.org/3/library/itertools.html
+def pairwise(iterable: typing.Iterable) -> typing.Iterator:
+    """Return successive overlapping pairs taken from the input iterable.
+
+    The number of 2-tuples in the output iterator will be one fewer than the
+    number of inputs. It will be empty if the input iterable has fewer than two
+    values.
+    """
+    # pairwise('ABCDEFG') --> AB BC CD DE EF FG
+    a, b = it.tee(iterable)
+    next(b, None)
+    return zip(a, b)
+
+# use library implementation if available (Python 3.10+)
+try:
+    pairwise = it.pairwise
+except AttributeError:
+    pass
