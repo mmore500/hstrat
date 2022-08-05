5636bdc9e8314a1ee38213a8a23f2178f2e690de
Matthew Andres Moreno <mmore500.login+gpg@gmail.com>
2022-08-04 20:51:01 -0700

    Test exact MRCA uncertainty w/ uneven branches

--- hstrat2/test/test_hstrat/test_stratum_retention_policies/test_nominal_resolution_policy/test_CalcMrcaUncertaintyExact.py
+++ hstrat2/test/test_hstrat/test_stratum_retention_policies/test_nominal_resolution_policy/test_CalcMrcaUncertaintyExact.py
@@ -1,3 +1,4 @@
+import itertools as it
 import numpy as np
 import pytest
 
@@ -49,6 +50,48 @@ def test_policy_consistency(time_sequence):
                     num_strata_deposited,
                     actual_mrca_rank,
                 ) == policy_requirement
+
+def test_policy_consistency_uneven_branches():
+    policy = nominal_resolution_policy.Policy()
+    spec = policy.GetSpec()
+    instance = nominal_resolution_policy.CalcMrcaUncertaintyExact(spec)
+    sample_durations = it.chain(
+        range(10**2),
+        np.logspace(7, 16, num=10, base=2, dtype='int'),
+    )
+    for num_strata_deposited_a in sample_durations:
+        ranks_a = set(policy.IterRetainedRanks(num_strata_deposited_a))
+        for num_strata_deposited_b in sample_durations:
+            ranks_b = set(policy.IterRetainedRanks(num_strata_deposited_b))
+            retained_ranks = np.fromiter(ranks_a & ranks_b, int)
+            least_num_strata_deposited = min(
+                num_strata_deposited_a,
+                num_strata_deposited_b,
+            )
+            for actual_mrca_rank in range(least_num_strata_deposited):
+                last_known_commonality = retained_ranks[
+                    retained_ranks <= actual_mrca_rank,
+                ].max(
+                    initial=0,
+                )
+                first_known_disparity = retained_ranks[
+                    retained_ranks > actual_mrca_rank,
+                ].min(
+                    initial=least_num_strata_deposited,
+                )
+                policy_requirement \
+                    =  first_known_disparity - last_known_commonality - 1
+                assert policy_requirement >= 0
+                for which in (
+                    instance,
+                    nominal_resolution_policy.CalcMrcaUncertaintyExact(spec),
+                ):
+                    assert which(
+                        policy,
+                        num_strata_deposited_a,
+                        num_strata_deposited_b,
+                        actual_mrca_rank,
+                    ) == policy_requirement
 
 def test_eq():
     policy = nominal_resolution_policy.Policy()
