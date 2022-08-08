dd2013e25344277f4c9fcca8c10d893c6780f977
Matthew Andres Moreno <mmore500.login+gpg@gmail.com>
2022-08-08 10:58:36 -0700

    Implement and test find_bounds helper

--- hstrat2/helpers/__init__.py
+++ hstrat2/helpers/__init__.py
@@ -3,6 +3,7 @@ from .AnyTreeAscendingIter import AnyTreeAscendingIter
 from .bit_ceil import bit_ceil
 from .bit_floor import bit_floor
 from .div_range import div_range
+from .find_bounds import find_bounds
 from .is_nondecreasing import is_nondecreasing
 from .is_nonincreasing import is_nonincreasing
 from .memoize_generator import memoize_generator
@@ -15,6 +16,7 @@ __all__ = [
     'bit_ceil',
     'bit_floor',
     'div_range',
+    'find_bounds',
     'is_nondecreasing',
     'is_nonincreasing',
     'memoize_generator',
---
+++ hstrat2/helpers/find_bounds.py
@@ -0,0 +1,48 @@
+from functools import reduce
+import operator
+import opytional as opyt
+import typing
+
+
+def find_bounds(
+    query: typing.Any,
+    iterable: typing.Iterable[typing.Any],
+    filter_below=operator.lt,
+    filter_above=operator.gt,
+    key=lambda x: x,
+) -> typing.Tuple[typing.Optional[typing.Any], typing.Optional[typing.Any]]:
+
+    def operation(accumulation, element):
+        cur_below, cur_above = accumulation
+
+        element = key(element)
+        maybe_below = element if filter_below(element, query) else None
+        maybe_above = element if filter_above(element, query) else None
+
+        res_below = opyt.apply_if_or_value(
+            cur_below,
+            lambda x: opyt.apply_if_or_value(
+                maybe_below,
+                lambda y: max(x, y),
+                x,
+            ),
+            maybe_below,
+        )
+        res_above = opyt.apply_if_or_value(
+            cur_above,
+            lambda x: opyt.apply_if_or_value(
+                maybe_above,
+                lambda y: min(x, y),
+                x,
+            ),
+            maybe_above,
+        )
+
+
+        return (res_below, res_above)
+
+    return reduce(
+        operation, # function
+        iterable, # iterable
+        (None, None), # initializer
+    )
