78266fd52f7aea9463435d8024bf40ca0bfda429
Matthew Andres Moreno <mmore500.login+gpg@gmail.com>
2022-08-09 17:14:24 -0700

    Rename visualizations -> plot

--- hstrat2/hstrat/__init__.py
+++ hstrat2/hstrat/__init__.py
@@ -2,11 +2,11 @@ from .HereditaryStratigraphicColumn import HereditaryStratigraphicColumn
 from .HereditaryStratigraphicColumnBundle \
     import HereditaryStratigraphicColumnBundle
 from .HereditaryStratum import HereditaryStratum
+from .plot import *
 from .stratum_ordered_stores import *
 from .stratum_retention_condemners import *
 from .stratum_retention_policies import *
 from .stratum_retention_predicates import *
-from .visualizations import *
 
 # adapted from https://stackoverflow.com/a/31079085
 __all__ = [
