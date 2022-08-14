2f749fd33c15d9c3c014ba96f4d7f3c525cdf985
Matthew Andres Moreno <mmore500.login+gpg@gmail.com>
2022-08-14 09:09:43 -0700

    Restore inadvertently disabled tests

--- hstrat2/test/test_hstrat/test_stratum_retention_policies/test_depth_proportional_resolution_tapered_policy/test_CalcMrcaUncertaintyAbsExact.py
+++ hstrat2/test/test_hstrat/test_stratum_retention_policies/test_depth_proportional_resolution_tapered_policy/test_CalcMrcaUncertaintyAbsExact.py
@@ -72,77 +72,77 @@ def test_policy_consistency(depth_proportional_resolution, time_sequence):
                     actual_mrca_rank,
                 ) == policy_requirement
 
-# @pytest.mark.parametrize(
-#     'depth_proportional_resolution',
-#     [
-#         1,
-#         2,
-#         3,
-#         7,
-#         42,
-#         97,
-#         100,
-#     ],
-# )
-# def test_policy_consistency_uneven_branches(depth_proportional_resolution):
-#     policy = depth_proportional_resolution_tapered_policy.Policy(depth_proportional_resolution)
-#     spec = policy.GetSpec()
-#     instance = depth_proportional_resolution_tapered_policy.CalcMrcaUncertaintyAbsExact(spec)
-#     sample_durations = it.chain(
-#         range(10**2),
-#         np.logspace(7, 16, num=10, base=2, dtype='int'),
-#     )
-#     for num_strata_deposited_a in sample_durations:
-#         ranks_a = set(policy.IterRetainedRanks(num_strata_deposited_a))
-#         for num_strata_deposited_b in sample_durations:
-#             ranks_b = set(policy.IterRetainedRanks(num_strata_deposited_b))
-#             retained_ranks = np.fromiter(ranks_a & ranks_b, int)
-#             least_num_strata_deposited = min(
-#                 num_strata_deposited_a,
-#                 num_strata_deposited_b,
-#             )
-#             for actual_mrca_rank in range(least_num_strata_deposited):
-#                 last_known_commonality = retained_ranks[
-#                     retained_ranks <= actual_mrca_rank,
-#                 ].max(
-#                     initial=0,
-#                 )
-#                 first_known_disparity = retained_ranks[
-#                     retained_ranks > actual_mrca_rank,
-#                 ].min(
-#                     initial=least_num_strata_deposited,
-#                 )
-#                 policy_requirement \
-#                     =  first_known_disparity - last_known_commonality - 1
-#                 assert policy_requirement >= 0
-#                 for which in (
-#                     instance,
-#                     depth_proportional_resolution_tapered_policy.CalcMrcaUncertaintyAbsExact(spec),
-#                 ):
-#                     assert which(
-#                         policy,
-#                         num_strata_deposited_a,
-#                         num_strata_deposited_b,
-#                         actual_mrca_rank,
-#                     ) == policy_requirement
-#
-# @pytest.mark.parametrize(
-#     'depth_proportional_resolution',
-#     [
-#         1,
-#         2,
-#         3,
-#         7,
-#         42,
-#         97,
-#         100,
-#     ],
-# )
-# def test_eq(depth_proportional_resolution):
-#     policy = depth_proportional_resolution_tapered_policy.Policy(depth_proportional_resolution)
-#     spec = policy.GetSpec()
-#     instance = depth_proportional_resolution_tapered_policy.CalcMrcaUncertaintyAbsExact(spec)
-#
-#     assert instance == instance
-#     assert instance == depth_proportional_resolution_tapered_policy.CalcMrcaUncertaintyAbsExact(spec)
-#     assert not instance == None
+@pytest.mark.parametrize(
+    'depth_proportional_resolution',
+    [
+        1,
+        2,
+        3,
+        7,
+        42,
+        97,
+        100,
+    ],
+)
+def test_policy_consistency_uneven_branches(depth_proportional_resolution):
+    policy = depth_proportional_resolution_tapered_policy.Policy(depth_proportional_resolution)
+    spec = policy.GetSpec()
+    instance = depth_proportional_resolution_tapered_policy.CalcMrcaUncertaintyAbsExact(spec)
+    sample_durations = it.chain(
+        range(10**2),
+        np.logspace(7, 16, num=10, base=2, dtype='int'),
+    )
+    for num_strata_deposited_a in sample_durations:
+        ranks_a = set(policy.IterRetainedRanks(num_strata_deposited_a))
+        for num_strata_deposited_b in sample_durations:
+            ranks_b = set(policy.IterRetainedRanks(num_strata_deposited_b))
+            retained_ranks = np.fromiter(ranks_a & ranks_b, int)
+            least_num_strata_deposited = min(
+                num_strata_deposited_a,
+                num_strata_deposited_b,
+            )
+            for actual_mrca_rank in range(least_num_strata_deposited):
+                last_known_commonality = retained_ranks[
+                    retained_ranks <= actual_mrca_rank,
+                ].max(
+                    initial=0,
+                )
+                first_known_disparity = retained_ranks[
+                    retained_ranks > actual_mrca_rank,
+                ].min(
+                    initial=least_num_strata_deposited,
+                )
+                policy_requirement \
+                    =  first_known_disparity - last_known_commonality - 1
+                assert policy_requirement >= 0
+                for which in (
+                    instance,
+                    depth_proportional_resolution_tapered_policy.CalcMrcaUncertaintyAbsExact(spec),
+                ):
+                    assert which(
+                        policy,
+                        num_strata_deposited_a,
+                        num_strata_deposited_b,
+                        actual_mrca_rank,
+                    ) == policy_requirement
+
+@pytest.mark.parametrize(
+    'depth_proportional_resolution',
+    [
+        1,
+        2,
+        3,
+        7,
+        42,
+        97,
+        100,
+    ],
+)
+def test_eq(depth_proportional_resolution):
+    policy = depth_proportional_resolution_tapered_policy.Policy(depth_proportional_resolution)
+    spec = policy.GetSpec()
+    instance = depth_proportional_resolution_tapered_policy.CalcMrcaUncertaintyAbsExact(spec)
+
+    assert instance == instance
+    assert instance == depth_proportional_resolution_tapered_policy.CalcMrcaUncertaintyAbsExact(spec)
+    assert not instance == None
