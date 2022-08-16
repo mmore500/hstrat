dfad08ed49a0aacd3cde73c34cd8577fa1ae0f61
Matthew Andres Moreno <mmore500.login+gpg@gmail.com>
2022-08-15 21:05:20 -0700

    Make worst case upper bound calc exactly tight

--- hstrat2/hstrat/stratum_retention_policies/_detail/CalcWorstCaseMrcaUncertaintyAbsUpperBound.py
+++ hstrat2/hstrat/stratum_retention_policies/_detail/CalcWorstCaseMrcaUncertaintyAbsUpperBound.py
@@ -20,12 +20,26 @@ class CalcWorstCaseMrcaUncertaintyAbsUpperBound:
         policy: typing.Optional['Policy'],
         first_num_strata_deposited: int,
         second_num_strata_deposited: int,
-        actual_rank_of_mrca: typing.Optional[int]=None,
+        actual_rank_of_mrca: int,
     ) -> int:
         """At most, how much absolute uncertainty to estimate rank of MRCA?
         Inclusive."""
 
-        return max(
+        # rectify negative-indexed actual_rank_of_mrca
+        if actual_rank_of_mrca is not None and actual_rank_of_mrca < 0:
+            least_last_rank = min(
+                first_num_strata_deposited - 1,
+                second_num_strata_deposited - 1,
+            )
+            actual_rank_of_mrca += least_last_rank
+            assert actual_rank_of_mrca >= 0
+
+        # assumes (required) retention of rank 0 and last rank
+        least_num_strata_deposited = min(
             first_num_strata_deposited,
             second_num_strata_deposited,
         )
+        if actual_rank_of_mrca == least_num_strata_deposited - 1:
+            return 0
+        else:
+            return max(least_num_strata_deposited - 2, 0)
