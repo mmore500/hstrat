936d7e594c15cb5554c0ecb1df5fa47c3eae34ca
Matthew Andres Moreno <mmore500.login+gpg@gmail.com>
2022-08-18 16:49:21 -0700

    Remove migrated-from condemners, predicates dirs

--- hstrat2/hstrat/__init__.py
+++ hstrat2/hstrat/__init__.py
@@ -5,10 +5,8 @@ from .HereditaryStratigraphicColumnBundle import (
 from .HereditaryStratum import HereditaryStratum
 from .plot import *
 from .stratum_ordered_stores import *
-from .stratum_retention_condemners import *
 from .stratum_retention_policies import *
 from .stratum_retention_policy_parameterizers import *
-from .stratum_retention_predicates import *
 
 # adapted from https://stackoverflow.com/a/31079085
 __all__ = [
