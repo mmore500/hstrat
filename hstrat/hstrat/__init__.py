8961bb3e1191228f36bb5ae68273a72f5e26585b
Matthew Andres Moreno <mmore500.login+gpg@gmail.com>
2022-08-21 01:01:48 -0700

    Bugfix: pack retention viz content into flat ns

--- hstrat/hstrat/__init__.py
+++ hstrat/hstrat/__init__.py
@@ -3,3 +3,4 @@
 from ..genome_instrumentation import *  # noqa: F401
 from ..phylogenetic_inference import *  # noqa: F401
 from ..stratum_retention_strategy import *  # noqa: F401
+from ..stratum_retention_viz import *  # noqa: F401
