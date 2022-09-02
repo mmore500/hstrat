9f3dc8b91099fcf8ddf55e35c2267b4d9805105b
Matthew Andres Moreno <mmore500.login+gpg@gmail.com>
2022-09-02 02:21:45 -0700

    Allow for compile failures during import process

--- hstrat/stratum_retention_strategy/stratum_retention_algorithms/fixed_resolution_algo/_PolicySpec_/__init__.py
+++ hstrat/stratum_retention_strategy/stratum_retention_algorithms/fixed_resolution_algo/_PolicySpec_/__init__.py
@@ -1,6 +1,16 @@
-import cppimport.import_hook
+from distutils.errors import CompileError
 
 from ._PolicySpec import PolicySpec
-from ._PolicySpecNative import PolicySpecNative
 
-impls = (PolicySpec, PolicySpecNative)
+impls = [
+    PolicySpec,
+]
+
+try:
+    import cppimport.import_hook
+    from ._PolicySpecNative import PolicySpecNative
+    impls.append(PolicySpecNative)
+except (CompileError, ImportError, SystemExit):
+    import os
+    os.environ["HSTRAT_NATIVE_ERROR"] = "1"
+    pass
--- hstrat/stratum_retention_strategy/stratum_retention_algorithms/fixed_resolution_algo/_enact/_GenDropRanks_/__init__.py
+++ hstrat/stratum_retention_strategy/stratum_retention_algorithms/fixed_resolution_algo/_enact/_GenDropRanks_/__init__.py
@@ -1,7 +1,18 @@
-import cppimport.import_hook
+from distutils.errors import CompileError
 
 from ._FromPredKeepRank import FromPredKeepRank
 from ._GenDropRanks import GenDropRanks
-from ._GenDropRanksNative import GenDropRanksNative
 
-impls = (FromPredKeepRank, GenDropRanks, GenDropRanksNative)
+impls = [
+    FromPredKeepRank,
+    GenDropRanks,
+]
+
+try:
+    import cppimport.import_hook
+    from ._GenDropRanksNative import GenDropRanksNative
+    impls.append(GenDropRanksNative)
+except (CompileError, ImportError, SystemExit):
+    import os
+    os.environ["HSTRAT_NATIVE_ERROR"] = "1"
+    pass
