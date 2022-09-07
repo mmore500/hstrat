2197ba499bea6be372d3d8b2b2060907ec1d1828
Matthew Andres Moreno <mmore500.login+gpg@gmail.com>
2022-09-07 01:16:52 -0700

    Refactor calc_definitive_max_rank_of_first_retained_disparity_between

--- hstrat/juxtaposition/_calc_definitive_max_rank_of_first_retained_disparity_between.py
+++ hstrat/juxtaposition/_calc_definitive_max_rank_of_first_retained_disparity_between.py
@@ -14,29 +14,34 @@ def calc_definitive_max_rank_of_first_retained_disparity_between(
 
     At most, how many depositions elapsed along the columns' lines of
     descent before the first mismatching strata at the same rank between
-    self and other?
+    self and second?
 
     Returns
     -------
     int, optional
         The number of depositions elapsed or None if no disparity (i.e.,
         both columns have same number of strata deposited and the most
-        recent stratum is common between self and other).
+        recent stratum is common between first and second).
 
     Notes
     -----
-    If no mismatching strata are found but self and other have different
+    If no mismatching strata are found but first and second have different
     numbers of strata deposited, this method returns one greater than the
     lesser of the columns' deposition counts.
     """
     confidence_level = 0.49
     assert (
-        self.CalcMinImplausibleSpuriousConsecutiveDifferentiaCollisions(
+        first.GetStratumDifferentiaBitWidth()
+        == second.GetStratumDifferentiaBitWidth()
+    )
+    assert (
+        first.CalcMinImplausibleSpuriousConsecutiveDifferentiaCollisions(
             significance_level=1.0 - confidence_level,
         )
         == 1
     )
-    return self.CalcRankOfFirstRetainedDisparityWith(
-        other,
+    return calc_rank_of_first_retained_disparity_between(
+        first,
+        second,
         confidence_level=confidence_level,
     )
