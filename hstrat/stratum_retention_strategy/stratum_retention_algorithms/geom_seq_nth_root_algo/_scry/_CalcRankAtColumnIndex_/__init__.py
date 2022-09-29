aa5da2bb3fcde2673aebf37c1aa67b1b7ba07c1e
Matthew Andres Moreno <mmore500.login+gpg@gmail.com>
2022-09-29 02:12:21 -0700

    Spliunter rest of scry impls

---
+++ hstrat/stratum_retention_strategy/stratum_retention_algorithms/geom_seq_nth_root_algo/_scry/_CalcRankAtColumnIndex_/__init__.py
@@ -0,0 +1,15 @@
+import opytional as opyt
+
+from ......_auxiliary_lib import hstrat_import_native
+from ._CalcRankAtColumnIndex import CalcRankAtColumnIndex
+
+impls = [
+    CalcRankAtColumnIndex,
+]
+
+# PolicySpecNative = opyt.apply_if(
+# hstrat_import_native("._CalcRankAtColumnIndexNative", __name__),
+#     lambda x: x.CalcRankAtColumnIndexNative,
+# )
+# if CalcRankAtColumnIndexNative is not None:
+#     impls.append(CalcRankAtColumnIndexNative)
