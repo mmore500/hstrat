bf99bf1cd8cab59eff6e5c754561f21fc56562f2
Matthew Andres Moreno <mmore500.login+gpg@gmail.com>
2022-10-01 00:19:01 -0700

    Test CalcMrcaUncertaintyRelExact impl consistency

--- test_hstrat/test_stratum_retention_strategy/test_stratum_retention_algorithms/test_recency_proportional_resolution_algo/test_CalcMrcaUncertaintyRelExact.py
+++ test_hstrat/test_stratum_retention_strategy/test_stratum_retention_algorithms/test_recency_proportional_resolution_algo/test_CalcMrcaUncertaintyRelExact.py
@@ -253,3 +253,56 @@ def test_negative_index(impl, recency_proportional_resolution):
             100,
             99 - diff,
         )
+
+
+@pytest.mark.parametrize(
+    "rep",
+    range(20),
+)
+@pytest.mark.parametrize(
+    "recency_proportional_resolution",
+    [
+        0,
+        1,
+        2,
+        3,
+        7,
+        42,
+        97,
+        100,
+    ],
+)
+def test_impl_consistency(rep, recency_proportional_resolution):
+    policy = recency_proportional_resolution_algo.Policy(
+        recency_proportional_resolution
+    )
+    spec = policy.GetSpec()
+
+    rng = np.random.default_rng(rep)
+
+    for num_strata_deposited_a in (
+        rng.integers(1, 2**5),
+        rng.integers(1, 2**10),
+        rng.integers(1, 2**32),
+    ):
+        for num_strata_deposited_b in (
+            num_strata_deposited_a,
+            num_strata_deposited_a + 107,
+            rng.integers(1, num_strata_deposited_a + 1),
+        ):
+            bound = min(num_strata_deposited_a, num_strata_deposited_b)
+            for actual_mrca_rank in [0, bound - 1, rng.integers(bound)]:
+                assert (
+                    len(
+                        {
+                            impl(spec)(
+                                policy,
+                                num_strata_deposited_a,
+                                num_strata_deposited_b,
+                                actual_mrca_rank,
+                            )
+                            for impl in recency_proportional_resolution_algo._scry._CalcMrcaUncertaintyRelExact_.impls
+                        }
+                    )
+                    == 1
+                )
