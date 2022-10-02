0422eb25104ae224b9e59ebc6a04f503dbad6046
Matthew Andres Moreno <mmore500.login+gpg@gmail.com>
2022-10-02 13:35:31 -0700

    Test over HereditaryStratum impls

--- test_hstrat/test_genome_instrumentation/test_HereditaryStratum.py
+++ test_hstrat/test_genome_instrumentation/test_HereditaryStratum.py
@@ -2,24 +2,34 @@ from copy import deepcopy
 import pickle
 import tempfile
 
-from hstrat import hstrat
+import pytest
+
+from hstrat import genome_instrumentation
 
 
-def test_deposition_rank():
+@pytest.mark.parametrize(
+    "impl",
+    genome_instrumentation._HereditaryStratum_.impls,
+)
+def test_deposition_rank(impl):
     assert (
-        hstrat.HereditaryStratum(
+        impl(
             deposition_rank=42,
         ).GetDepositionRank()
         == 42
     )
 
 
-def test_differentia_generation():
-    original1 = hstrat.HereditaryStratum(
+@pytest.mark.parametrize(
+    "impl",
+    genome_instrumentation._HereditaryStratum_.impls,
+)
+def test_differentia_generation(impl):
+    original1 = impl(
         deposition_rank=42,
     )
     copy1 = deepcopy(original1)
-    original2 = hstrat.HereditaryStratum(
+    original2 = impl(
         deposition_rank=42,
     )
 
@@ -32,17 +42,25 @@ def test_differentia_generation():
     assert copy1.GetDifferentia() != original2.GetDifferentia()
 
 
-def test_equality1():
-    assert hstrat.HereditaryStratum() != hstrat.HereditaryStratum()
-    stratum1 = hstrat.HereditaryStratum()
+@pytest.mark.parametrize(
+    "impl",
+    genome_instrumentation._HereditaryStratum_.impls,
+)
+def test_equality1(impl):
+    assert impl() != impl()
+    stratum1 = impl()
     stratum2 = stratum1
     assert stratum1 == stratum2
     assert stratum1 == deepcopy(stratum2)
 
 
-def test_equality2():
+@pytest.mark.parametrize(
+    "impl",
+    genome_instrumentation._HereditaryStratum_.impls,
+)
+def test_equality2(impl):
     def stratum_factory():
-        return hstrat.HereditaryStratum(deposition_rank=42)
+        return impl(deposition_rank=42)
 
     assert stratum_factory() != stratum_factory()
     stratum1 = stratum_factory()
@@ -51,8 +69,12 @@ def test_equality2():
     assert stratum1 == deepcopy(stratum2)
 
 
-def test_pickle():
-    original = hstrat.HereditaryStratum()
+@pytest.mark.parametrize(
+    "impl",
+    genome_instrumentation._HereditaryStratum_.impls,
+)
+def test_pickle(impl):
+    original = impl()
     with tempfile.TemporaryDirectory() as tmp_path:
         with open(f"{tmp_path}/data", "wb") as tmp_file:
             pickle.dump(original, tmp_file)
@@ -60,4 +82,4 @@ def test_pickle():
         with open(f"{tmp_path}/data", "rb") as tmp_file:
             reconstituted = pickle.load(tmp_file)
             assert reconstituted == original
-            assert reconstituted != hstrat.HereditaryStratum()
+            assert reconstituted != impl()
