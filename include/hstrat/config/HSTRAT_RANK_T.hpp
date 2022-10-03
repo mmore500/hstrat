8ae9ab7b0be47da712a1b3f1a6708dba79d62d6f
Matthew Andres Moreno <mmore500.login+gpg@gmail.com>
2022-10-02 20:34:59 -0700

    Add missing cstddef include

--- include/hstrat/config/HSTRAT_RANK_T.hpp
+++ include/hstrat/config/HSTRAT_RANK_T.hpp
@@ -2,6 +2,8 @@
 #ifndef HSTRAT_CONFIG_HSTRAT_RANK_T_HPP_INCLUDE
 #define HSTRAT_CONFIG_HSTRAT_RANK_T_HPP_INCLUDE
 
+#include <cstdint>
+
 #ifndef HSTRAT_RANK_T
 #define HSTRAT_RANK_T int64_t
 #endif // #ifndef HSTRAT_RANK_T
