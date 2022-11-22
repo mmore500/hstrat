7a88df9ca8b3d2f3b097d0367e4fd3cfc9f10f3c
Matthew Andres Moreno <mmore500.login+gpg@gmail.com>
2022-11-22 02:31:50 -0800

    Try passing string as seed instead of hashing

--- tests/test_hstrat/conftest.py
+++ tests/test_hstrat/conftest.py
@@ -9,6 +9,5 @@ import pytest
 @pytest.fixture(autouse=True)
 def reseed_random(request: pytest.FixtureRequest) -> None:
     """Reseed random number generator to ensure determinstic test."""
-    seed = hash(request.node.name) % 2**32
-    random.seed(seed)
-    np.random.seed(seed)
+    random.seed(request.node.name)
+    np.random.seed(random.randint(1, 2**32))
