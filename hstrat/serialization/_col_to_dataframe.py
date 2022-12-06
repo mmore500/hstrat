f9e0250a90df9d55d704833a77aa140be0d3a900
Matthew Andres Moreno <mmore500.login+gpg@gmail.com>
2022-12-06 02:12:29 -0800

    Implement ascii col print, add example

--- hstrat/serialization/_col_to_dataframe.py
+++ hstrat/serialization/_col_to_dataframe.py
@@ -2,13 +2,14 @@ import pandas as pd
 
 from ..genome_instrumentation import HereditaryStratigraphicColumn
 
+
 def col_to_dataframe(column: HereditaryStratigraphicColumn) -> pd.DataFrame:
     records = [
         {
-            'index' : index,
-            'rank' : column.GetRankAtColumnIndex(index),
-            'differentia' : stratum.GetDifferentia(),
+            "index": index,
+            "rank": column.GetRankAtColumnIndex(index),
+            "differentia": stratum.GetDifferentia(),
         }
         for index, stratum in enumerate(column.IterRetainedStrata())
     ]
-    return pd.DataFrame.from_records(records)
+    return pd.DataFrame.from_records(records).astype(int)
