ac0747a61a02f135b64d1841a16ffdf662cd4d9f
Santiago Rodriguez Papa <rodsan0@users.noreply.github.com>
2022-12-20 16:26:21 -0500

    Update requirements

--- requirements-dev/regenerate.sh
+++ requirements-dev/regenerate.sh
@@ -5,7 +5,10 @@
 cd "${0%/*}"
 
 ./py37/regenerate.sh
+# ./py38/regenerate.sh
+# ./py39/regenerate.sh
 ./py310/regenerate.sh
+# ./py311/regenerate.sh
 
 for f in py37/*.txt; do
   cat "${f}" | sed "s/==/>=/g" > "$(basename "${f}")"
