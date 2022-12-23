cd0c1be1f9eec26efccdd010f419b92bd167a5de
Santiago Rodriguez Papa <rodsan0@users.noreply.github.com>
2022-12-23 01:39:25 -0500

    Import Literal from typing_extensions on py37
    typing.Literal was implemented on py38
    Also, pin typing_extensions explicitly

--- hstrat/phylogenetic_inference/tree_reconstruction/__init__.py
+++ hstrat/phylogenetic_inference/tree_reconstruction/__init__.py
@@ -2,6 +2,13 @@
 
 __all__ = []
 
+from itertools import combinations
+from typing import Iterable
+
+try:
+    from typing import Literal
+except ImportError:
+    from typing_extensions import Literal
 from ..._auxiliary_lib import launder_impl_modules as _launder
 
 _launder([eval(item) for item in __all__], __name__)
