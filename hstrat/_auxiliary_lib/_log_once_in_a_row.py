c826462acad33851220bfd2cddb6dcdab192397f
Santiago Rodriguez Papa <rodsan0@users.noreply.github.com>
2022-12-27 17:30:23 -0500

    Update getLogging name to camelCase

--- hstrat/_auxiliary_lib/_log_once_in_a_row.py
+++ hstrat/_auxiliary_lib/_log_once_in_a_row.py
@@ -6,5 +6,5 @@ import logging
 # adapted from https://stackoverflow.com/a/66062313
 @lru_cache(1)
 def log_once_in_a_row(msg: str):
-    logger = logging.GetLogger("hstrat")
+    logger = logging.getLogger("hstrat")
     logger.info(msg)
