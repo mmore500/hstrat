4960d9ea94f83af05505ad9b544193618dbd21ef
Matthew Andres Moreno <mmore500.login+gpg@gmail.com>
2022-12-27 20:52:17 -0800

    Assert valid deposition count arg

--- include/hstrat/genome_instrumentation/HereditaryStratigraphicColumn.hpp
+++ include/hstrat/genome_instrumentation/HereditaryStratigraphicColumn.hpp
@@ -72,6 +72,7 @@ public:
   , store(std::get<0>(store_and_deposit_count))
   , num_strata_deposited(std::get<1>(store_and_deposit_count))
   {
+    assert(GetNumStrataRetained() <= GetNumStrataDeposited());
     if (num_strata_deposited == 0) {
       DepositStratum(initial_annotation);
     }
