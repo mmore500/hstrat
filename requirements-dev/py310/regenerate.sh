7bce2da87012019107b671d9dd6ee8a147837472
Matthew Andres Moreno <mmore500.login+gpg@gmail.com>
2022-12-31 12:22:52 -0800

    Use setuptools version compatible w pyproject.toml

--- requirements-dev/py310/regenerate.sh
+++ requirements-dev/py310/regenerate.sh
@@ -14,7 +14,7 @@ echo "generating requirements-minimal.txt"
 python3.10 -m piptools compile ../../pyproject.toml --extra pinned_dependencies_py310 -o requirements-minimal.txt
 
 echo "generating requirements-release.txt"
-python3.10 -m piptools compile ../../pyproject.toml --extra pinned_dependencies_py310 --extra release -o requirements-release.txt
+python3.10 -m piptools compile  --allow-unsafe ../../pyproject.toml --extra pinned_dependencies_py310 --extra release -o requirements-release.txt
 
 echo "generating requirements-testing.txt"
 python3.10 -m piptools compile ../../pyproject.toml --extra pinned_dependencies_py310 --extra testing_py310 -o requirements-testing.txt
--- requirements-dev/py37/regenerate.sh
+++ requirements-dev/py37/regenerate.sh
@@ -14,7 +14,7 @@ echo "generating requirements-minimal.txt"
 python3.7 -m piptools compile ../../pyproject.toml --extra pinned_dependencies_py37 -o requirements-minimal.txt
 
 echo "generating requirements-release.txt"
-python3.7 -m piptools compile ../../pyproject.toml --extra pinned_dependencies_py37 --extra release -o requirements-release.txt
+python3.7 -m piptools compile --allow-unsafe ../../pyproject.toml --extra pinned_dependencies_py37 --extra release -o requirements-release.txt
 
 echo "generating requirements-testing.txt"
 python3.7 -m piptools compile ../../pyproject.toml --extra pinned_dependencies_py37 --extra testing_py37 -o requirements-testing.txt
