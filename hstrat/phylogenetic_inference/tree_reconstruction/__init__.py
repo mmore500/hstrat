0b5b1082540b7e0654689196dcfdcb2438a7c23e
Santiago Rodriguez Papa <rodsan0@users.noreply.github.com>
2022-12-30 21:43:46 -0500

    Reorganize distance_matrix_helper

--- hstrat/phylogenetic_inference/tree_reconstruction/__init__.py
+++ hstrat/phylogenetic_inference/tree_reconstruction/__init__.py
@@ -2,10 +2,10 @@
 
 __all__ = []
 
-from ..._auxiliary_lib import launder_impl_modules as _launder
-
 from itertools import combinations
 from typing import Iterable
+
+from ..._auxiliary_lib import launder_impl_modules as _launder
 
 try:
     from typing import Literal
@@ -25,12 +25,6 @@ from ..pairwise import (
 )
 
 
-def _to_tril(matrix):
-    return [
-        row[:row_idx] + [0.0] for row_idx, row in enumerate(matrix.tolist())
-    ]
-
-
 def distance_matrix_helper(x, y):
     mrca_bounds = _calc_ranks_since_mrca_bounds_with(x, y)
     if mrca_bounds is not None:
@@ -39,21 +33,23 @@ def distance_matrix_helper(x, y):
         x, y
     )
 
-    if mrca_bounds is None:
-        if earliest_detectable_mrca:
-            return (earliest_detectable_mrca - 1) / 2
-        else:
-            return min(x, y) / 2
+    if mrca_bounds is not None:
+        return (mrca_lb + mrca_ub - 1) / 2  # ub is exclusive
+
+    if earliest_detectable_mrca is None:
+        return min(x, y) / 2
+
+    if earliest_detectable_mrca:
+        return (earliest_detectable_mrca - 1) / 2
 
     if _does_definitively_have_no_common_ancestor(x, y):
+        assert earliest_detectable_mrca == 0
         warn(
             f"""No common ancestor exists between {x} and {y}; returning a distance of 0.
             This behavior may change in the future (i.e., by returning two distinct trees.)
             """,
         )
         return 0
-
-    return (mrca_lb + mrca_ub - 1) / 2
 
 
 def calculate_distance_matrix(
@@ -78,6 +74,7 @@ def reconstruct_tree(
     distance_matrix: DistanceMatrix, algo: Literal["nj", "upgma"] = "nj"
 ):
     return getattr(DistanceTreeConstructor(), algo)(distance_matrix)
+
 
 _launder([eval(item) for item in __all__], __name__)
 del _launder  # prevent name from leaking
