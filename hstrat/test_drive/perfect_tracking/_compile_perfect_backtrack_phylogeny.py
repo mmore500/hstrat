a6835a63c971aee23e1f544037041929b73ad8cc
Matthew Andres Moreno <mmore500.login+gpg@gmail.com>
2023-01-09 00:32:41 -0800

    Write docstrings for perfect tracking utils

--- hstrat/test_drive/perfect_tracking/_compile_perfect_backtrack_phylogeny.py
+++ hstrat/test_drive/perfect_tracking/_compile_perfect_backtrack_phylogeny.py
@@ -12,6 +12,20 @@ from ._iter_perfect_backtrack_lineage import iter_perfect_backtrack_lineage
 def compile_perfect_backtrack_phylogeny(
     population: typing.Iterable[PerfectBacktrackHandle],
 ) -> pd.DataFrame:
+    """Compile phylogenetic history tracked using `PerfectBacktrackHandle`
+    breadcrumbs.
+
+    Parameters
+    ----------
+    population : iterable of `PerfectBactrackHandle`
+        PerfectBacktrackHandle objects associated with extant population
+        members.
+
+    Returns
+    -------
+    pd.DataFrame
+        Phylogenetic record in alife data standards format.
+    """
 
     seen_handle_ids = set()
 
--- hstrat/test_drive/perfect_tracking/_iter_perfect_backtrack_lineage.py
+++ hstrat/test_drive/perfect_tracking/_iter_perfect_backtrack_lineage.py
@@ -6,6 +6,8 @@ from ._PerfectBacktrackHandle import PerfectBacktrackHandle
 def iter_perfect_backtrack_lineage(
     handle: PerfectBacktrackHandle,
 ) -> typing.Iterator[PerfectBacktrackHandle]:
+    """Iterate up `PerfectBacktrackHandle` breadcrumbs for line of descent
+    ending in `handle`, including `handle`."""
     while True:
         yield handle
 
