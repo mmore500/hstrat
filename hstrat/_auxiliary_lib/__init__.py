8d3245578cfaaaf7adece84e1daeaf477f1d2aa1
Matthew Andres Moreno <mmore500.login+gpg@gmail.com>
2023-01-21 11:48:34 -0500

    Delete unused helper

--- hstrat/_auxiliary_lib/__init__.py
+++ hstrat/_auxiliary_lib/__init__.py
@@ -14,7 +14,6 @@ from ._div_range import div_range
 from ._find_bounds import find_bounds
 from ._get_hstrat_version import get_hstrat_version
 from ._indices_of_unique import indices_of_unique
-from ._interleave import interleave
 from ._is_base64 import is_base64
 from ._is_in_coverage_run import is_in_coverage_run
 from ._is_in_unit_test import is_in_unit_test
@@ -52,7 +51,6 @@ __all__ = [
     "find_bounds",
     "get_hstrat_version",
     "indices_of_unique",
-    "interleave",
     "is_base64",
     "is_nondecreasing",
     "is_nonincreasing",
--- hstrat/_auxiliary_lib/_interleave.py
+++
@@ -1,9 +0,0 @@
-import numpy as np
-
-
-# adapted from https://stackoverflow.com/a/5347492
-def interleave(a: np.array, b: np.array) -> np.array:
-    res = np.empty(a.size + b.size, dtype=a.dtype)
-    res[0::2] = a
-    res[1::2] = b
-    return res
