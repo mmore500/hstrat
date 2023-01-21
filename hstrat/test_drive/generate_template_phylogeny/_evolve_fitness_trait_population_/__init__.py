51add22ee0f129f0b55481226c7d204338c1266f
Matthew Andres Moreno <mmore500.login+gpg@gmail.com>
2023-01-20 22:09:44 -0500

    Update submodule __all__'s

--- hstrat/test_drive/generate_template_phylogeny/_evolve_fitness_trait_population_/__init__.py
+++ hstrat/test_drive/generate_template_phylogeny/_evolve_fitness_trait_population_/__init__.py
@@ -4,3 +4,13 @@ from ._apply_niche_invasions import _apply_niche_invasions
 from ._get_island_id import _get_island_id
 from ._get_niche_id import _get_niche_id
 from ._select_parents import _select_parents
+
+# adapted from https://stackoverflow.com/a/31079085
+__all__ = [
+    "_apply_island_migrations",
+    "_apply_mutation",
+    "_apply_niche_invasions",
+    "_get_island_id",
+    "_get_niche_id",
+    "_select_parents",
+]
