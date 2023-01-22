c44e0cfbfd6a504a52b8bbd92b1f04e1110872b6
Matthew Andres Moreno <mmore500.login+gpg@gmail.com>
2023-01-22 00:17:59 -0500

    Lint: consistently avoid redef of class name

--- hstrat/stratum_retention_strategy/stratum_retention_algorithms/perfect_resolution_algo/_invar/_CalcMrcaUncertaintyAbsUpperBoundAtPessimalRank.py
+++ hstrat/stratum_retention_strategy/stratum_retention_algorithms/perfect_resolution_algo/_invar/_CalcMrcaUncertaintyAbsUpperBoundAtPessimalRank.py
@@ -1,9 +1,11 @@
-from ..._impl import CalcMrcaUncertaintyAbsUpperBoundAtPessimalRank
+from ..._impl import (
+    CalcMrcaUncertaintyAbsUpperBoundAtPessimalRank as CalcMrcaUncertaintyAbsUpperBoundAtPessimalRank_,
+)
 
 
 # must inherit rather than assign due to failure of attribute lookup when
 # pickling otherwise
 class CalcMrcaUncertaintyAbsUpperBoundAtPessimalRank(
-    CalcMrcaUncertaintyAbsUpperBoundAtPessimalRank
+    CalcMrcaUncertaintyAbsUpperBoundAtPessimalRank_
 ):
     pass
