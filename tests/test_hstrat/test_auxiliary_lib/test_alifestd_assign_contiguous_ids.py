f5662e0402a003543e0f486f16f5e2fcfdb59b74
Matthew Andres Moreno <mmore500.login+gpg@gmail.com>
2023-01-30 14:27:33 -0800

    Test assign contig on multiroot, working form phylos

--- tests/test_hstrat/test_auxiliary_lib/test_alifestd_assign_contiguous_ids.py
+++ tests/test_hstrat/test_auxiliary_lib/test_alifestd_assign_contiguous_ids.py
@@ -6,11 +6,13 @@ import pandas as pd
 import pytest
 
 from hstrat._auxiliary_lib import (
+    alifestd_aggregate_phylogenies,
     alifestd_assign_contiguous_ids,
     alifestd_find_leaf_ids,
     alifestd_has_contiguous_ids,
     alifestd_is_asexual,
     alifestd_make_ancestor_id_col,
+    alifestd_to_working_format,
 )
 
 assets_path = os.path.join(os.path.dirname(__file__), "assets")
@@ -22,15 +24,37 @@ assets_path = os.path.join(os.path.dirname(__file__), "assets")
         pd.read_csv(
             f"{assets_path}/example-standard-toy-sexual-phylogeny.csv"
         ),
+        alifestd_aggregate_phylogenies(
+            [
+                pd.read_csv(
+                    f"{assets_path}/example-standard-toy-sexual-phylogeny.csv"
+                ),
+                pd.read_csv(f"{assets_path}/nk_lexicaseselection.csv"),
+            ]
+        ),
         pd.read_csv(
             f"{assets_path}/example-standard-toy-asexual-phylogeny.csv"
         ),
         pd.read_csv(f"{assets_path}/nk_ecoeaselection.csv"),
         pd.read_csv(f"{assets_path}/nk_lexicaseselection.csv"),
+        alifestd_aggregate_phylogenies(
+            [
+                pd.read_csv(f"{assets_path}/nk_ecoeaselection.csv"),
+                pd.read_csv(f"{assets_path}/nk_lexicaseselection.csv"),
+            ]
+        ),
         pd.read_csv(f"{assets_path}/nk_tournamentselection.csv"),
     ],
 )
-def test_alifestd_assign_contiguous_ids(phylogeny_df):
+@pytest.mark.parametrize(
+    "apply",
+    [
+        alifestd_to_working_format,
+        lambda x: x,
+    ],
+)
+def test_alifestd_assign_contiguous_ids(phylogeny_df, apply):
+    phylogeny_df = apply(phylogeny_df)
     phylogeny_df_ = phylogeny_df.copy()
     reassigned_df = alifestd_assign_contiguous_ids(phylogeny_df)
     # check for side effects
@@ -52,17 +76,29 @@ def test_alifestd_assign_contiguous_ids(phylogeny_df):
         ).all()
 
     if alifestd_is_asexual(phylogeny_df):
-        phylogeny_tree = apc.alife_dataframe_to_dendropy_tree(phylogeny_df)
-        reassigned_tree = apc.alife_dataframe_to_dendropy_tree(reassigned_df)
+        phylogeny_trees = apc.alife_dataframe_to_dendropy_trees(phylogeny_df)
+        reassigned_trees = apc.alife_dataframe_to_dendropy_trees(reassigned_df)
 
-        assert Counter(node.level() for node in phylogeny_tree) == Counter(
-            node.level() for node in reassigned_tree
+        assert Counter(
+            node.level() for tree in phylogeny_trees for node in tree
+        ) == Counter(
+            node.level() for tree in reassigned_trees for node in tree
         )
         assert Counter(
-            len(node.child_nodes()) for node in phylogeny_tree
-        ) == Counter(len(node.child_nodes()) for node in reassigned_tree)
-        assert Counter(
-            (node.level(), len(node.child_nodes())) for node in phylogeny_tree
+            len(node.child_nodes())
+            for tree in phylogeny_trees
+            for node in tree
         ) == Counter(
-            (node.level(), len(node.child_nodes())) for node in reassigned_tree
+            len(node.child_nodes())
+            for tree in reassigned_trees
+            for node in tree
+        )
+        assert Counter(
+            (node.level(), len(node.child_nodes()))
+            for tree in phylogeny_trees
+            for node in tree
+        ) == Counter(
+            (node.level(), len(node.child_nodes()))
+            for tree in reassigned_trees
+            for node in tree
         )
