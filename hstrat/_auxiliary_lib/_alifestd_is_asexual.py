629c5329874d765be2aad26e5ac71951ae7af97d
Matthew Andres Moreno <mmore500.login+gpg@gmail.com>
2023-01-30 12:51:14 -0800

    Add, test is_asexual optimization

--- hstrat/_auxiliary_lib/_alifestd_is_asexual.py
+++ hstrat/_auxiliary_lib/_alifestd_is_asexual.py
@@ -5,4 +5,6 @@ from ._alifestd_is_sexual import alifestd_is_sexual
 
 def alifestd_is_asexual(phylogeny_df: pd.DataFrame) -> bool:
     """Do all organisms in the phylogeny have one or no immediate ancestor?"""
-    return not alifestd_is_sexual(phylogeny_df)
+    return "ancestor_id" in phylogeny_df or not alifestd_is_sexual(
+        phylogeny_df
+    )
