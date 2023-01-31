a89066cd2527a77a11072ac9c648fae46926536e
Matthew Andres Moreno <mmore500.login+gpg@gmail.com>
2023-01-30 20:55:58 -0800

    Bugfix: treat numpy num types as vals, not objs

--- hstrat/_auxiliary_lib/_demark.py
+++ hstrat/_auxiliary_lib/_demark.py
@@ -1,6 +1,7 @@
+from numbers import Number
 import typing
 
 
 def demark(object: typing.Any) -> int:
     """Provide a hashable identifier for `object`."""
-    return object if isinstance(object, (str, int, float)) else id(object)
+    return object if isinstance(object, (str, Number)) else id(object)
--- tests/test_hstrat/test_auxiliary_lib/test_demark.py
+++ tests/test_hstrat/test_auxiliary_lib/test_demark.py
@@ -1,3 +1,5 @@
+from numbers import Number
+
 import numpy as np
 import pytest
 
@@ -9,6 +11,8 @@ from hstrat._auxiliary_lib import demark
     [
         range(20),
         "abcdefghij",
+        np.arange(10),
+        np.linspace(1, 42),
         np.arange(20, dtype=float),
         [{x} for x in range(20)],
         [{3} for x in range(20)],
@@ -18,5 +22,5 @@ def test_demark(collection):
     assert len(collection) == len(set(map(demark, collection)))
     for x in collection:
         assert demark(x) == demark(x)
-        if isinstance(x, (int, float, str)):
+        if isinstance(x, (int, float, Number, str)):
             assert demark(x) == x
