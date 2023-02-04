b2ddc0b4a219557a27fe7fbf49b6fe8230f94596
Matthew Andres Moreno <mmore500.login+gpg@gmail.com>
2023-02-04 13:32:11 -0800

    Speed up pack_differentia
    Use native bitarray package instead of pure python bitstring
    package

--- hstrat/serialization/_pack_differentiae.py
+++ hstrat/serialization/_pack_differentiae.py
@@ -1,9 +1,42 @@
 from base64 import b64encode
 import typing
 
+from bitarray import util as bitarray_util
 from bitstring import BitArray
 
+from .._auxiliary_lib import iter_slices, zip_strict
 from ..genome_instrumentation import HereditaryStratum
+
+
+def _make_buffer(strata, differentia_bit_width):
+    try:
+        len(strata)
+    except TypeError:
+        strata = [*strata]
+
+    needs_header = bool(differentia_bit_width % 8)
+    buffer_size = 8 * needs_header + len(strata) * differentia_bit_width
+    buffer = bitarray_util.zeros(buffer_size)
+
+    # use first byte to tell how many padding bits if any are required
+    if needs_header:
+        num_jagged_bits = len(buffer) % 8
+        num_padding_bits = (8 - num_jagged_bits) % 8
+        header = bitarray_util.int2ba(
+            num_padding_bits,
+            length=8,
+        )
+        buffer[:8] |= header
+
+    for slice_, stratum in zip_strict(
+        iter_slices(len(buffer), differentia_bit_width, needs_header * 8),
+        strata,
+    ):
+        buffer[slice_] |= bitarray_util.int2ba(
+            stratum.GetDifferentia(), length=differentia_bit_width
+        )
+
+    return buffer
 
 
 def pack_differentiae(
@@ -18,29 +51,10 @@ def pack_differentiae(
     byte tells how many empty padding bits, if any, were placed at the end of
     the concatenation in order to align the bitstring end to byte boundaries.
     """
-    # this is a concatenation, not a mathematical sum
-    differentia_bits = sum(
-        (
-            BitArray(
-                uint=stratum.GetDifferentia(),
-                length=differentia_bit_width,
-            )
-            for stratum in strata
-        ),
-        BitArray(),
-    )
 
-    # use first byte to tell how many padding bits if any are required
-    if differentia_bit_width % 8:
-        num_jagged_bits = len(differentia_bits) % 8
-        num_padding_bits = (8 - num_jagged_bits) % 8
-        header = BitArray(
-            uint=num_padding_bits,
-            length=8,
-        )
-        differentia_bits = header + differentia_bits
+    buffer = _make_buffer(strata, differentia_bit_width)
 
-    encoded_bytes = b64encode(differentia_bits.tobytes())
+    encoded_bytes = b64encode(buffer.tobytes())
 
     encoded_str = encoded_bytes.decode()
     return encoded_str
