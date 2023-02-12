ddd39e21c82ac33bbcfe7393f6395a4aed32b388
Matthew Andres Moreno <mmore500.login+gpg@gmail.com>
2023-02-11 19:59:51 -0800

    Implement, test prior classes

--- hstrat/phylogenetic_inference/__init__.py
+++ hstrat/phylogenetic_inference/__init__.py
@@ -1,8 +1,9 @@
 """Tools to infer phylogenetic history from extant genome annotations."""
 
-from . import pairwise, population
+from . import pairwise, population, priors
 from .pairwise import *  # noqa: F401
 from .population import *  # noqa: F401
+from .priors import *  # noqa: F401
 from .tree_reconstruction import *  # noqa: F401
 
-__all__ = pairwise.__all__ + population.__all__ + tree_reconstruction.__all__
+__all__ = pairwise.__all__ + population.__all__ + priors.__all__
