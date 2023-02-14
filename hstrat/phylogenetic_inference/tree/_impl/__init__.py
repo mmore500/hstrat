7d5cf69ea74c63d95f1008ace96682e0694b6607
Matthew Andres Moreno <mmore500.login+gpg@gmail.com>
2023-02-13 17:19:27 -0800

    Implement, test find_chronological_root{,s}

--- hstrat/phylogenetic_inference/tree/_impl/__init__.py
+++ hstrat/phylogenetic_inference/tree/_impl/__init__.py
@@ -1,8 +1,12 @@
 """Implementation helpers."""
 
 from ._build_tree_biopython_distance import build_tree_biopython_distance
+from ._find_chronological_root import find_chronological_root
+from ._find_chronological_roots import find_chronological_roots
 
 # adapted from https://stackoverflow.com/a/31079085
 __all__ = [
     "build_tree_biopython_distance",
+    "find_chronological_root",
+    "find_chronological_roots",
 ]
---
+++ hstrat/phylogenetic_inference/tree/_impl/_find_chronological_roots.py
@@ -0,0 +1,70 @@
+import numbers
+import statistics
+import typing
+
+from iterpop import iterpop as ip
+import networkx as nx
+import sortedcontainers as sc
+
+
+def find_chronological_roots(
+    graph: nx.Graph,
+    leaf_node_depths: typing.Dict[str, numbers.Number],
+) -> typing.List[str]:
+    """Work backward from known phylogenetic depths of leaf nodes to identify
+    a likely most ancient ancestor."""
+
+    # ensure clean slate for origin time setup
+    for _node, data in graph.nodes(data=True):
+        if "origin_time" in data:
+            del data["origin_time"]
+
+    # setup origin times of leaf nodes in graph
+    leaf_nodes = leaf_node_depths.keys()
+    for leaf_node in leaf_nodes:
+        taxon_label = graph.nodes[leaf_node]["taxon_label"]
+        graph.nodes[leaf_node]["origin_time"] = leaf_node_depths[taxon_label]
+
+    # starting from leaf nodes...
+    visited_nodes = set()  # fully explored nodes
+    discovered_nodes = sc.SortedSet(
+        leaf_nodes,
+        key=lambda node: graph.nodes[node]["origin_time"],
+    )  # nodes eligible to visit next, sorted chronologically (deepest last)
+
+    # ... traverse graph: discover nodes and calculate their origin times
+    # in reverse chronological order
+    while len(visited_nodes) < len(graph):
+        # pop oldest discovered node, mark visited, discover its neighbors
+        cur_node = discovered_nodes.pop(-1)
+        visited_nodes.add(cur_node)
+
+        newly_discovered_nodes = set(graph.neighbors(cur_node)) - (
+            visited_nodes | discovered_nodes
+        )
+        # give newly discovered nodes origin time estimates
+        for newly_discovered_node in newly_discovered_nodes:
+            assert "origin_time" not in graph[newly_discovered_node]
+            # estimate origin_time of cur node
+            descendant_origin_times = [
+                graph.nodes[neighbor_node]["origin_time"]
+                - graph[newly_discovered_node][neighbor_node]["length"]
+                for neighbor_node in graph.neighbors(newly_discovered_node)
+                if "origin_time" in graph.nodes[neighbor_node]
+            ]
+            assert descendant_origin_times
+            origin_time = statistics.mean(descendant_origin_times)
+            graph.nodes[newly_discovered_node]["origin_time"] = origin_time
+
+        # register newly discovered nodes as eligible to visit next
+        discovered_nodes.update(newly_discovered_nodes)
+
+    # extract root node of each connected component
+    root_nodes = []
+    for component in nx.connected_components(graph):
+        root_node = min(
+            component, key=lambda node: graph.nodes[node]["origin_time"]
+        )
+        root_nodes.append(root_node)
+
+    return [graph.nodes[root_node]["taxon_label"] for root_node in root_nodes]
