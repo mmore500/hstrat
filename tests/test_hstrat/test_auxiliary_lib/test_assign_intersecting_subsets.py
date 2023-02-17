0cda3417d8b6f96ee771ae06bfb9b714cc464ca3
Matthew Andres Moreno <mmore500.login+gpg@gmail.com>
2023-02-16 23:33:00 -0800

    Fixup tests for assign_intersecting_subsets

--- tests/test_hstrat/test_auxiliary_lib/test_assign_intersecting_subsets.py
+++ tests/test_hstrat/test_auxiliary_lib/test_assign_intersecting_subsets.py
@@ -2,7 +2,11 @@ import random
 
 import pytest
 
-from hstrat._auxiliary_lib import assign_intersecting_subsets
+from hstrat._auxiliary_lib import (
+    assign_intersecting_subsets,
+    coshuffled,
+    make_intersecting_subsets,
+)
 
 
 @pytest.mark.parametrize(
@@ -28,7 +32,7 @@ from hstrat._auxiliary_lib import assign_intersecting_subsets
             [[0, 1], [2], [3]],
             [[(0, 5), (1, 2)], [(2, 3)], [(3, 3)]],
         ),
-        ([(0, 1)], [[0, 1]], [[(0, 1)]]),
+        ([(0, 1)], [[0]], [[(0, 1)]]),
         ([], [], []),
         (
             [(0, 2), (1, 2), (2, 3)],
@@ -61,24 +65,37 @@ def test_assign_intersecting_subsets(
     ranges, expected_subsets_indices, expected_subsets_ranges
 ):
 
+    range_indices = [*range(len(ranges))]
+
     for _rep in range(20):
+        created_subsets_ranges = make_intersecting_subsets(ranges)
         created_subsets_indices = assign_intersecting_subsets(ranges)
         for (
             created_subset_indices,
+            created_subset_ranges,
             expected_subset_indices,
             expected_subset_ranges,
         ) in zip(
             created_subsets_indices,
+            created_subsets_ranges,
             expected_subsets_indices,
             expected_subsets_ranges,
         ):
-            assert sorted(created_subset_indices) == sorted(
-                expected_subset_indices
-            ) or len(ranges) > len(set(ranges))
 
-            created_subset_ranges = [ranges[i] for i in created_subset_indices]
-            assert sorted(created_subset_ranges) == sorted(
-                expected_subset_ranges
+            assert len(created_subset_indices) == len(created_subset_ranges)
+            assert len(created_subset_indices) == len(expected_subset_indices)
+
+            assert sorted(
+                [range_indices[i] for i in created_subset_indices]
+            ) == sorted(expected_subset_indices) or len(ranges) > len(
+                set(ranges)
             )
 
-        random.shuffle(ranges)
+            recreated_subset_ranges = [
+                ranges[i] for i in created_subset_indices
+            ]
+            assert sorted(recreated_subset_ranges) == sorted(
+                expected_subset_ranges
+            ) == sorted(created_subset_ranges)
+
+        ranges, range_indices = coshuffled(ranges, range_indices)
