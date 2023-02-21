f28ff2d38a56eed9898b192f9a35e6afcb435cfd
Matthew Andres Moreno <mmore500.login+gpg@gmail.com>
2023-02-21 04:26:22 -0800

    Add tests, docstring for iter_chunks

--- hstrat/_auxiliary_lib/_iter_chunks.py
+++ hstrat/_auxiliary_lib/_iter_chunks.py
@@ -6,6 +6,31 @@ def iter_chunks(
     seq: typing.Sequence,
     chunk_size: int,
 ) -> typing.Iterator[typing.Sequence]:
+    """Yield successive chunks of size `chunk_size` from sequence `seq`.
+
+    Parameters
+    ----------
+    seq : Sequence
+        The sequence to be chunked.
+    chunk_size : int
+        The size of each chunk.
+
+    Returns
+    -------
+    Iterator[Sequence]
+        A generator object that yields chunks of `seq` of size `chunk_size`.
+
+    Examples
+    --------
+    >>> seq = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+    >>> chunk_size = 3
+    >>> for chunk in iter_chunks(seq, chunk_size):
+    ...     print(chunk)
+    [1, 2, 3]
+    [4, 5, 6]
+    [7, 8, 9]
+    [10]
+    """
     return (
         seq[pos : pos + chunk_size] for pos in range(0, len(seq), chunk_size)
     )
