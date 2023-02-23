9a95c78004313a2fcd2a29ddc0eb4b57d0354895
Matthew Andres Moreno <mmore500.login+gpg@gmail.com>
2023-02-23 04:55:52 -0800

    Impl, test calc_rank_of_first_disparity_between_specimen_naive

--- hstrat/juxtaposition/_impl/__init__.py
+++ hstrat/juxtaposition/_impl/__init__.py
@@ -6,6 +6,9 @@ from ._calc_rank_of_first_retained_disparity_between_bsearch import (
 from ._calc_rank_of_first_retained_disparity_between_generic import (
     calc_rank_of_first_retained_disparity_between_generic,
 )
+from ._calc_rank_of_first_retained_disparity_between_specimen_naive import (
+    calc_rank_of_first_retained_disparity_between_specimen_naive,
+)
 from ._calc_rank_of_last_retained_commonality_between_bsearch import (
     calc_rank_of_last_retained_commonality_between_bsearch,
 )
@@ -20,6 +23,7 @@ from ._iter_ranks_of_retained_commonality_between_generic import (
 __all__ = [
     "calc_rank_of_first_retained_disparity_between_bsearch",
     "calc_rank_of_first_retained_disparity_between_generic",
+    "calc_rank_of_first_retained_disparity_between_specimen_naive",
     "calc_rank_of_last_retained_commonality_between_bsearch",
     "calc_rank_of_last_retained_commonality_between_generic",
     "iter_ranks_of_retained_commonality_between_generic",
