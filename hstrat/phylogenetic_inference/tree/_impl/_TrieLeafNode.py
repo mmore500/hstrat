f988b0f0dddc382666579a17952b7409de8454f1
Matthew Andres Moreno <mmore500.login+gpg@gmail.com>
2023-02-27 01:13:26 -0800

    Document, test TrieLeafNode

--- hstrat/phylogenetic_inference/tree/_impl/_TrieLeafNode.py
+++ hstrat/phylogenetic_inference/tree/_impl/_TrieLeafNode.py
@@ -4,6 +4,27 @@ from ...._auxiliary_lib import render_to_base64url
 
 
 class TrieLeafNode(anytree.NodeMixin):
+    """A leaf node in a  phylogenetic reconstruction trie.
+
+    Represents an single, specific extant annotation among the population
+    subject to phylogenetic inference.
+
+    Parameters
+    ----------
+    parent : TrieInnerNode
+        The parent node of this leaf node.
+    taxon_label : str
+        The taxon label for this leaf node.
+
+    Attributes
+    ----------
+    taxon_label : str
+        The taxon label for this leaf node.
+    taxon : str
+        Synonym for taxon_label.
+    origin_time : str
+        Synonym for taxon_label.
+    """
 
     taxon_label: str
 
@@ -12,22 +33,30 @@ class TrieLeafNode(anytree.NodeMixin):
         parent: "TrieInnerNode",
         taxon_label: str,
     ) -> None:
+        """Initialize a new TrieLeafNode.
+
+        Parameters
+        ----------
+        parent : TrieInnerNode
+            The parent node of this leaf node.
+        taxon_label : str
+            The taxon label for this leaf node.
+        """
         self.parent = parent
         self.taxon_label = taxon_label
 
     @property
-    def name(self: "TrieLeafNode") -> str:
+    def taxon(self: "TrieLeafNode") -> str:
+        """Return the taxon label for this leaf node."""
         return self.taxon_label
 
     @property
-    def taxon(self: "TrieLeafNode") -> str:
-        return self.name
-
-    @property
     def origin_time(self: "TrieLeafNode") -> int:
+        """Return the origin time for this leaf node."""
         return self.parent.origin_time
 
     def __repr__(self: "TrieLeafNode") -> str:
+        """Return a string representation of this leaf node."""
         return f"""{self.taxon_label} @ {
             render_to_base64url(id(self) % 8192)
         }"""
--- tests/test_hstrat/test_phylogenetic_inference/test_tree/test_impl/test_TrieLeafNode.py
+++ tests/test_hstrat/test_phylogenetic_inference/test_tree/test_impl/test_TrieLeafNode.py
@@ -1,1 +1,32 @@
-#TODO
+from unittest.mock import Mock
+
+import anytree
+import pytest
+
+import hstrat.phylogenetic_inference.tree._impl as impl
+
+
+@pytest.fixture
+def inner_node():
+    return anytree.AnyNode()
+
+
+def test_taxon_label(inner_node):
+    leaf_node = impl.TrieLeafNode(inner_node, "taxon")
+    assert leaf_node.taxon_label == "taxon"
+
+
+def test_taxon(inner_node):
+    leaf_node = impl.TrieLeafNode(inner_node, "taxon")
+    assert leaf_node.taxon == "taxon"
+
+
+def test_origin_time(inner_node):
+    inner_node.origin_time = 42
+    leaf_node = impl.TrieLeafNode(inner_node, "taxon")
+    assert leaf_node.origin_time == 42
+
+
+def test_parent(inner_node):
+    leaf_node = impl.TrieLeafNode(inner_node, "taxon")
+    assert leaf_node.parent is inner_node
