1aa5f4c52d625908b7828e6209c61c66100f51d1
Matthew Andres Moreno <mmore500.login+gpg@gmail.com>
2023-03-01 14:52:35 -0800

    Refactor estimator logic out of impl details

--- hstrat/phylogenetic_inference/pairwise/_impl/_estimate_rank_of_mrca_maximum_likelihood.py
+++ hstrat/phylogenetic_inference/pairwise/_impl/_estimate_rank_of_mrca_maximum_likelihood.py
@@ -5,6 +5,11 @@ import opytional as opyt
 
 from ...._auxiliary_lib import cmp_approx, pairwise, unzip
 from ....genome_instrumentation import HereditaryStratigraphicColumn
+from ....juxtaposition import calc_probability_differentia_collision_between
+from ...estimators import (
+    estimate_rank_of_mrca_maximum_likelihood as estimate_rank_of_mrca_maximum_likelihood_,
+)
+
 from ._extract_common_retained_ranks_through_first_retained_disparity import (
     extract_common_retained_ranks_through_first_retained_disparity,
 )
@@ -21,57 +26,11 @@ def estimate_rank_of_mrca_maximum_likelihood(
             first, second
         )
     )
-    if len(waypoints_ascending) == 1:
-        return None
-
+    p_collision = calc_probability_differentia_collision_between(first, second)
     base = 1 / 2 ** first.GetStratumDifferentiaBitWidth()
 
-    max_weight, best_expected_rank = -1.0, None
-    for (
-        num_spurious_collisions,
-        (
-            end_exclusive,
-            begin_inclusive,
-        ),
-    ) in enumerate(pairwise(reversed(waypoints_ascending))):
-        cur_weight = (
-            base**num_spurious_collisions
-            * prior.CalcIntervalProbabilityProxy(
-                begin_inclusive, end_exclusive
-            )
-        )
-
-        if cur_weight > max_weight:
-            max_weight = cur_weight
-            # slide to most likely end of interval
-            # assumes monotonicity of prior
-            best_expected_rank = {
-                1: end_exclusive - 1,
-                0: statistics.mean((end_exclusive - 1, begin_inclusive)),
-                -1: begin_inclusive,
-            }[
-                cmp_approx(
-                    prior.CalcIntervalConditionedMean(
-                        begin_inclusive, end_exclusive
-                    ),
-                    statistics.mean((end_exclusive - 1, begin_inclusive)),
-                )
-            ]
-
-        # if whole rest of the remaining record cannot have higher cur_weight
-        # than best, terminate early
-        if (
-            begin_inclusive
-            and base ** (num_spurious_collisions + 1)
-            * prior.CalcIntervalProbabilityProxy(
-                0,
-                begin_inclusive,
-            )
-            < max_weight
-        ):
-            break
-
-    assert best_expected_rank is not None
-    assert max_weight > 0.0
-
-    return best_expected_rank
+    return estimate_rank_of_mrca_maximum_likelihood_(
+        reversed(waypoints_ascending),
+        p_collision,
+        prior,
+    )
--- hstrat/phylogenetic_inference/pairwise/_impl/_estimate_rank_of_mrca_unbiased.py
+++ hstrat/phylogenetic_inference/pairwise/_impl/_estimate_rank_of_mrca_unbiased.py
@@ -1,18 +1,20 @@
 import statistics
 import typing
 
-import numpy as np
 import opytional as opyt
 
-from ...._auxiliary_lib import pairwise, unzip
+from ...._auxiliary_lib import cmp_approx, pairwise, unzip
 from ....genome_instrumentation import HereditaryStratigraphicColumn
+from ....juxtaposition import calc_probability_differentia_collision_between
+from ...estimators import (
+    estimate_rank_of_mrca_unbiased as estimate_rank_of_mrca_unbiased_,
+)
+
 from ._extract_common_retained_ranks_through_first_retained_disparity import (
     extract_common_retained_ranks_through_first_retained_disparity,
 )
 
 
-# tried a numpy-based implementation but it was slower
-# https://gist.github.com/mmore500/fc4caba18a98b20fbda3fb223cf49552
 def estimate_rank_of_mrca_unbiased(
     first: HereditaryStratigraphicColumn,
     second: HereditaryStratigraphicColumn,
@@ -24,45 +26,11 @@ def estimate_rank_of_mrca_unbiased(
             first, second
         )
     )
-    if len(waypoints_ascending) == 1:
-        return None
-
+    p_collision = calc_probability_differentia_collision_between(first, second)
     base = 1 / 2 ** first.GetStratumDifferentiaBitWidth()
 
-    weights, expected_ranks, sum_weight = [], [], 0.0
-    for (
-        num_spurious_collisions,
-        (
-            end_exclusive,
-            begin_inclusive,
-        ),
-    ) in enumerate(pairwise(reversed(waypoints_ascending))):
-        expected_ranks.append(
-            prior.CalcIntervalConditionedMean(begin_inclusive, end_exclusive)
-        )
-        weights.append(
-            base**num_spurious_collisions
-            * prior.CalcIntervalProbabilityProxy(
-                begin_inclusive, end_exclusive
-            )
-        )
-        sum_weight += weights[-1]
-
-        # if whole rest of the remaining record cannot meaningfully impact
-        # average, exit early
-        if (
-            begin_inclusive
-            and base ** (num_spurious_collisions + 1)
-            * prior.CalcIntervalProbabilityProxy(
-                0,
-                begin_inclusive,
-            )
-            * begin_inclusive
-            < sum_weight * 10e-6  # numpy uses reltol 10e-5
-        ):
-            break
-
-    return np.average(
-        np.array(expected_ranks),
-        weights=np.array(weights),
+    return estimate_rank_of_mrca_unbiased_(
+        reversed(waypoints_ascending),
+        p_collision,
+        prior,
     )
