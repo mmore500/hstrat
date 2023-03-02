148e87d1b90a9f20456c0b701e6fea810a8db5f8
Matthew Andres Moreno <mmore500.login+gpg@gmail.com>
2023-03-02 13:39:48 -0800

    Fixup, reactivate build_tree tests

--- tests/test_hstrat/test_phylogenetic_inference/test_tree/test_build_tree.py
+++ tests/test_hstrat/test_phylogenetic_inference/test_tree/test_build_tree.py
@@ -49,6 +49,7 @@ def test_dual_population_no_mrca(version_pin):
         population, version_pin, taxon_labels=names, force_common_ancestry=True
     )
     assert alifestd_validate(tree)
+    tree.loc[:, "name"] = tree["taxon_label"]
 
     root_clade = BaseTree.Clade(name="Inner1")
     root_clade.clades = [
@@ -84,6 +85,7 @@ def test_dual_population_with_mrca(version_pin):
         population, version_pin=version_pin, taxon_labels=names
     )
     assert alifestd_validate(tree)
+    tree["name"] = tree["taxon_label"]
 
     root_clade = BaseTree.Clade(name="Inner")
     root_clade.clades = [
@@ -104,27 +106,29 @@ def test_dual_population_with_mrca(version_pin):
 @pytest.mark.parametrize(
     "orig_tree",
     [
-        # TODO
-        # pytest.param(
-        #     impl.setup_dendropy_tree(f"{assets_path}/nk_ecoeaselection.csv"),
-        #     marks=pytest.mark.heavy,
-        # ),
-        # impl.setup_dendropy_tree(f"{assets_path}/nk_lexicaseselection.csv"),
+        pytest.param(
+            impl.setup_dendropy_tree(f"{assets_path}/nk_ecoeaselection.csv"),
+            marks=pytest.mark.heavy,
+        ),
+        impl.setup_dendropy_tree(f"{assets_path}/nk_lexicaseselection.csv"),
         impl.setup_dendropy_tree(f"{assets_path}/nk_tournamentselection.csv"),
     ],
 )
-# @pytest.mark.parametrize(
-#     "retention_policy",
-#     [
-#       TODO
-#     ]
-# )
-def test_reconstructed_mrca(orig_tree):
+@pytest.mark.parametrize(
+    "retention_policy",
+    [
+      hstrat.fixed_resolution_algo.Policy(3),
+      hstrat.recency_proportional_resolution_algo.Policy(3),
+    ]
+)
+def test_reconstructed_mrca(orig_tree, retention_policy):
     num_depositions = 10
 
     extant_population = hstrat.descend_template_phylogeny_dendropy(
         orig_tree,
-        seed_column=hstrat.HereditaryStratigraphicColumn().CloneNthDescendant(
+        seed_column=hstrat.HereditaryStratigraphicColumn(
+            stratum_retention_policy=retention_policy,
+        ).CloneNthDescendant(
             num_depositions
         ),
     )
@@ -136,17 +140,20 @@ def test_reconstructed_mrca(orig_tree):
     )
     pdm = reconst_tree.phylogenetic_distance_matrix()
 
-    assert len(list(reconst_tree.leaf_node_iter())) == len(extant_population)
+    sorted_leaf_nodes = sorted(
+        reconst_tree.leaf_node_iter(), key=lambda x: int(x.taxon.label)
+    )
+    assert len(sorted_leaf_nodes) == len(extant_population)
     assert [
         leaf_node.distance_from_root()
-        for leaf_node in reconst_tree.leaf_node_iter()
+        for leaf_node in sorted_leaf_nodes
     ] == [
         extant_col.GetNumStrataDeposited() - 1
         for extant_col in extant_population
     ]
 
     for reconst_node_pair, extant_column_pair in zip(
-        it.combinations(reconst_tree.leaf_node_iter(), 2),
+        it.combinations(sorted_leaf_nodes, 2),
         it.combinations(extant_population, 2),
     ):
         reconst_mrca = impl.descend_unifurcations(
@@ -194,21 +201,10 @@ def test_reconstructed_mrca(orig_tree):
     ],
 )
 @pytest.mark.parametrize(
-    "estimator",
-    [
-        "maximum_likelihood",
-        "unbiased",
-    ],
+    "version_pin",
+    [hstrat.__version__],
 )
-@pytest.mark.parametrize(
-    "prior",
-    [
-        "arbitrary",
-        "uniform",
-        hstrat.ExponentialPrior(1.01),
-    ],
-)
-def test_determinism(orig_tree, retention_policy, wrap, estimator, prior):
+def test_determinism(orig_tree, retention_policy, wrap, version_pin):
     num_depositions = 10
 
     extant_population = hstrat.descend_template_phylogeny_dendropy(
@@ -218,13 +214,9 @@ def test_determinism(orig_tree, retention_policy, wrap, estimator, prior):
         ).CloneNthDescendant(num_depositions),
     )
 
-    first_reconst = hstrat.build_tree_upgma(
-        extant_population, estimator, prior
-    )
+    first_reconst = hstrat.build_tree(extant_population, version_pin)
     for rep in range(3):
-        second_reconst = hstrat.build_tree_upgma(
-            [wrap(col) for col in extant_population],
-            estimator,
-            prior,
+        second_reconst = hstrat.build_tree(
+            [wrap(col) for col in extant_population], version_pin
         )
         assert first_reconst.equals(second_reconst)
