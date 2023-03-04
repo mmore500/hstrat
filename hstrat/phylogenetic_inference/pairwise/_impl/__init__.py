2e876ae71316bdd37b0714407cb85d978f395d8d
Matthew Andres Moreno <mmore500.login+gpg@gmail.com>
2023-03-04 15:18:07 -0800

    Bugfix: add missing import

--- hstrat/phylogenetic_inference/pairwise/_impl/__init__.py
+++ hstrat/phylogenetic_inference/pairwise/_impl/__init__.py
@@ -3,6 +3,7 @@
 from ._estimate_rank_of_mrca_maximum_likelihood import (
     estimate_rank_of_mrca_maximum_likelihood,
 )
+from ._estimate_rank_of_mrca_naive import estimate_rank_of_mrca_naive
 from ._estimate_rank_of_mrca_unbiased import estimate_rank_of_mrca_unbiased
 from ._extract_common_retained_ranks_through_first_retained_disparity import (
     extract_common_retained_ranks_through_first_retained_disparity,
@@ -11,6 +12,7 @@ from ._extract_common_retained_ranks_through_first_retained_disparity import (
 # adapted from https://stackoverflow.com/a/31079085
 __all__ = [
     "estimate_rank_of_mrca_maximum_likelihood",
+    "estimate_rank_of_mrca_naive",
     "estimate_rank_of_mrca_unbiased",
     "extract_common_retained_ranks_through_first_retained_disparity",
 ]
