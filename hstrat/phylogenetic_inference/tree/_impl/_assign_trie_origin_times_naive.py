77952a0be00e89a3d7e9dd392b37e3b9b1c07fb5
Matthew Andres Moreno <mmore500.login+gpg@gmail.com>
2023-03-03 17:33:32 -0800

    Allow configuration with arbitrary prior

--- hstrat/phylogenetic_inference/tree/_impl/_assign_trie_origin_times_naive.py
+++ hstrat/phylogenetic_inference/tree/_impl/_assign_trie_origin_times_naive.py
@@ -1,12 +1,16 @@
 import anytree
 import numpy as np
 
+from ...priors import ArbitraryPrior
+
 from ._TrieInnerNode import TrieInnerNode
 from ._TrieLeafNode import TrieLeafNode
 
 
 def assign_trie_origin_times_naive(
-    trie: TrieInnerNode, assigned_property: str = "origin_time"
+    trie: TrieInnerNode,
+    assigned_property: str = "origin_time",
+    prior: object = ArbitraryPrior(),  # ok --- ArbitraryPrior is immutable
 ) -> None:
     for node in anytree.PreOrderIter(trie):
         if node.is_leaf:
@@ -18,18 +22,15 @@ def assign_trie_origin_times_naive(
             setattr(
                 node,
                 assigned_property,
-                np.mean(
-                    (
-                        node.rank,
-                        min(
-                            (
-                                child.rank
-                                for child in node.children
-                                if not child.is_leaf
-                            ),
-                            default=node.rank + 1,
-                        )
-                        - 1,
-                    )
+                prior.CalcIntervalConditionedMean(
+                    node.rank,
+                    min(
+                        (
+                            child.rank
+                            for child in node.children
+                            if not child.is_leaf
+                        ),
+                        default=node.rank + 1,
+                    ),
                 ),
             )
