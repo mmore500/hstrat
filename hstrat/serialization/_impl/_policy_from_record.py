63124f8caea3e03c4e5bad430cc2044115665d2e
Matthew Andres Moreno <mmore500.login+gpg@gmail.com>
2023-03-06 02:14:01 -0800

    Bugfix: import for eval'd content stripped by autoflake

--- hstrat/serialization/_impl/_policy_from_record.py
+++ hstrat/serialization/_impl/_policy_from_record.py
@@ -3,6 +3,9 @@ import typing
 
 def policy_from_record(policy_record: str) -> typing.Any:
     """Parse a policy record string and return the corresponding policy object."""
-    # noqa
+    from ...stratum_retention_strategy import (  # noqa
+        stratum_retention_algorithms as hstrat,
+    )
 
+    hstrat  # mark as used to prevent autoflake purge
     return eval(policy_record)  # noqa
