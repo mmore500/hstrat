fc5f1d61c2ce14c6b445b4feecb859ab3b7c53bf
Matthew Andres Moreno <mmore500.login+gpg@gmail.com>
2023-03-18 12:31:01 -0700

    Consolidate find, chron inconsist; test

--- hstrat/_auxiliary_lib/_alifestd_find_chronological_inconsistency.py
+++ hstrat/_auxiliary_lib/_alifestd_find_chronological_inconsistency.py
@@ -1,78 +1,79 @@
 import typing
 
+import numpy as np
 import pandas as pd
 
+from ._alifestd_has_compact_ids import alifestd_has_compact_ids
+from ._alifestd_has_contiguous_ids import alifestd_has_contiguous_ids
 from ._alifestd_parse_ancestor_ids import alifestd_parse_ancestor_ids
+from ._jit import jit
 
 
-#
-# @jit(nopython=True)
-# def _is_chronologically_ordered_contiguous(
-#     ancestor_ids: np.array, origin_times: np.array
-# ) -> bool:
-#     for id_, ancestor_id in enumerate(ancestor_ids):
-#         # self comparison ok for geneses
-#         if origin_times[ancestor_id] > origin_times[id_]:
-#             return False
-#     return True
-#
-#
-# @jit(nopython=True)
-# def _is_chronologically_ordered_compact(
-#     ids: np.array, ancestor_ids: np.array, origin_times: np.array
-# ) -> bool:
-#     origin_time_lookup = np.empty(len(ids))
-#     for (id_, origin_time) in zip(ids, origin_times):
-#         origin_time_lookup[id_] = origin_time
-#
-#     for id_, ancestor_id in zip(ids, ancestor_ids):
-#         # self comparison ok for geneses
-#         if origin_time_lookup[ancestor_id] > origin_time_lookup[id_]:
-#             return False
-#     return True
-#
-#
-# @jit(nopython=True)
-# def _is_chronologically_ordered(
-#     ids: np.array, ancestor_ids: np.array, origin_times: np.array
-# ) -> bool:
-#     origin_time_lookup = dict()
-#     for (id_, origin_time) in zip(ids, origin_times):
-#         origin_time_lookup[id_] = origin_time
-#
-#     for id_, ancestor_id in zip(ids, ancestor_ids):
-#         # self comparison ok for geneses
-#         if origin_time_lookup[ancestor_id] > origin_time_lookup[id_]:
-#             return False
-#     return True
-#
-#
+@jit(nopython=True)
+def _alifestd_find_chronological_inconsistency_contiguous(
+    ancestor_ids: np.array, origin_times: np.array
+) -> typing.Optional[int]:
+    for id_, ancestor_id in enumerate(ancestor_ids):
+        # self comparison ok for geneses
+        if origin_times[ancestor_id] > origin_times[id_]:
+            return id_
+    return None
+
+
+@jit(nopython=True)
+def _alifestd_find_chronological_inconsistency_compact(
+    ids: np.array, ancestor_ids: np.array, origin_times: np.array
+) -> typing.Optional[int]:
+    origin_time_lookup = np.empty(len(ids))
+    for (id_, origin_time) in zip(ids, origin_times):
+        origin_time_lookup[id_] = origin_time
+
+    for id_, ancestor_id in zip(ids, ancestor_ids):
+        # self comparison ok for geneses
+        if origin_time_lookup[ancestor_id] > origin_time_lookup[id_]:
+            return id_
+    return None
+
+
+@jit(nopython=True)
+def _alifestd_find_chronological_inconsistency(
+    ids: np.array, ancestor_ids: np.array, origin_times: np.array
+) -> typing.Optional[int]:
+    origin_time_lookup = dict()
+    for (id_, origin_time) in zip(ids, origin_times):
+        origin_time_lookup[id_] = origin_time
+
+    for id_, ancestor_id in zip(ids, ancestor_ids):
+        # self comparison ok for geneses
+        if origin_time_lookup[ancestor_id] > origin_time_lookup[id_]:
+            return id_
+    return None
+
+
 def alifestd_find_chronological_inconsistency(
     phylogeny_df: pd.DataFrame,
 ) -> typing.Optional[int]:
-    """Are all organisms listed after members of their `ancestor_list`?
+    """Return the id of a taxon with origin time precedint its parent's, if
+    any are present."""
 
-    Input dataframe is not mutated by this operation.
-    """
-
-    # if "ancestor_id" in phylogeny_df:
-    #     if alifestd_has_contiguous_ids(phylogeny_df):
-    #         return _is_chronologically_ordered_contiguous(
-    #             phylogeny_df["ancestor_id"].to_numpy(),
-    #             phylogeny_df["origin_time"].to_numpy(),
-    #         )
-    #     elif alifestd_has_compact_ids(phylogeny_df):
-    #         return _is_chronologically_ordered_compact(
-    #             phylogeny_df["id"].to_numpy(),
-    #             phylogeny_df["ancestor_id"].to_numpy(),
-    #             phylogeny_df["origin_time"].to_numpy(),
-    #         )
-    #     else:
-    #         return _is_chronologically_ordered(
-    #             phylogeny_df["id"].to_numpy(),
-    #             phylogeny_df["ancestor_id"].to_numpy(),
-    #             phylogeny_df["origin_time"].to_numpy(),
-    #         )
+    if "ancestor_id" in phylogeny_df:
+        if alifestd_has_contiguous_ids(phylogeny_df):
+            return _alifestd_find_chronological_inconsistency_contiguous(
+                phylogeny_df["ancestor_id"].to_numpy(),
+                phylogeny_df["origin_time"].to_numpy(),
+            )
+        elif alifestd_has_compact_ids(phylogeny_df):
+            return _alifestd_find_chronological_inconsistency_compact(
+                phylogeny_df["id"].to_numpy(),
+                phylogeny_df["ancestor_id"].to_numpy(),
+                phylogeny_df["origin_time"].to_numpy(),
+            )
+        else:
+            return _alifestd_find_chronological_inconsistency(
+                phylogeny_df["id"].to_numpy(),
+                phylogeny_df["ancestor_id"].to_numpy(),
+                phylogeny_df["origin_time"].to_numpy(),
+            )
 
     phylogeny_df = phylogeny_df.set_index("id", drop=False)
 
@@ -83,6 +84,6 @@ def alifestd_find_chronological_inconsistency(
                 phylogeny_df.iloc[ancestor_loc]["origin_time"]
                 > row["origin_time"]
             ):
-                return _pos
+                return _idx
 
     return None
