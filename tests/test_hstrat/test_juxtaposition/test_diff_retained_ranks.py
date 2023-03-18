18976207d8584fea0ff52a37d1c3102878a8bd10
Matthew Andres Moreno <mmore500.login+gpg@gmail.com>
2023-03-18 14:03:05 -0700

    Test diff retained ranks for specimens

--- tests/test_hstrat/test_juxtaposition/test_diff_retained_ranks.py
+++ tests/test_hstrat/test_juxtaposition/test_diff_retained_ranks.py
@@ -11,7 +11,14 @@ from hstrat import hstrat
         hstrat.HereditaryStratumOrderedStoreTree,
     ],
 )
-def test_DiffRetainedRanks(ordered_store):
+@pytest.mark.parametrize(
+    "wrap",
+    [
+        lambda x: x,
+        hstrat.col_to_specimen,
+    ],
+)
+def test_DiffRetainedRanks(ordered_store, wrap):
     first = hstrat.HereditaryStratigraphicColumn(
         stratum_ordered_store=ordered_store,
         stratum_retention_policy=hstrat.nominal_resolution_algo.Policy(),
@@ -21,21 +28,45 @@ def test_DiffRetainedRanks(ordered_store):
         stratum_retention_policy=hstrat.perfect_resolution_algo.Policy(),
     )
 
-    assert hstrat.diff_retained_ranks(first, second) == (set(), set())
-    assert hstrat.diff_retained_ranks(second, first) == (set(), set())
+    assert hstrat.diff_retained_ranks(wrap(first), wrap(second)) == (
+        set(),
+        set(),
+    )
+    assert hstrat.diff_retained_ranks(wrap(second), wrap(first)) == (
+        set(),
+        set(),
+    )
 
     first.DepositStratum()
-    assert hstrat.diff_retained_ranks(first, second) == ({1}, set())
-    assert hstrat.diff_retained_ranks(second, first) == (set(), {1})
+    assert hstrat.diff_retained_ranks(wrap(first), wrap(second)) == (
+        {1},
+        set(),
+    )
+    assert hstrat.diff_retained_ranks(wrap(second), wrap(first)) == (
+        set(),
+        {1},
+    )
 
     second.DepositStratum()
-    assert hstrat.diff_retained_ranks(first, second) == (set(), set())
-    assert hstrat.diff_retained_ranks(second, first) == (set(), set())
+    assert hstrat.diff_retained_ranks(wrap(first), wrap(second)) == (
+        set(),
+        set(),
+    )
+    assert hstrat.diff_retained_ranks(wrap(second), wrap(first)) == (
+        set(),
+        set(),
+    )
 
     first.DepositStratum()
-    assert hstrat.diff_retained_ranks(first, second) == ({2}, {1})
-    assert hstrat.diff_retained_ranks(second, first) == ({1}, {2})
+    assert hstrat.diff_retained_ranks(wrap(first), wrap(second)) == ({2}, {1})
+    assert hstrat.diff_retained_ranks(wrap(second), wrap(first)) == ({1}, {2})
 
     second.DepositStratum()
-    assert hstrat.diff_retained_ranks(first, second) == (set(), {1})
-    assert hstrat.diff_retained_ranks(second, first) == ({1}, set())
+    assert hstrat.diff_retained_ranks(wrap(first), wrap(second)) == (
+        set(),
+        {1},
+    )
+    assert hstrat.diff_retained_ranks(wrap(second), wrap(first)) == (
+        {1},
+        set(),
+    )
