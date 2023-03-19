10984ade488909efcd1c6bf5f11137f83baf4fcc
Matthew Andres Moreno <mmore500.login+gpg@gmail.com>
2023-03-18 22:20:37 -0700

    Add missing short column test cases

--- tests/test_hstrat/test_phylogenetic_inference/test_estimators/test_estimate_rank_of_mrca_maximum_likelihood.py
+++ tests/test_hstrat/test_phylogenetic_inference/test_estimators/test_estimate_rank_of_mrca_maximum_likelihood.py
@@ -14,6 +14,17 @@ def test_estimate_rank_of_mrca_maximum_likelihood_no_common_ancestor1():
     assert result is None
 
 
+def test_estimate_rank_of_mrca_maximum_likelihood_with_common_ancestor1():
+    coincident_ranks = iter([1, 0])
+    p_differentia_collision = 0.5
+
+    result = hstrat.estimate_rank_of_mrca_maximum_likelihood(
+        coincident_ranks, p_differentia_collision, hstrat.ArbitraryPrior()
+    )
+
+    assert result == 0
+
+
 def test_estimate_rank_of_mrca_maximum_likelihood_with_common_ancestor2():
     coincident_ranks = iter([7, 3, 0])
     p_differentia_collision = 0.5
--- tests/test_hstrat/test_phylogenetic_inference/test_estimators/test_estimate_rank_of_mrca_naive.py
+++ tests/test_hstrat/test_phylogenetic_inference/test_estimators/test_estimate_rank_of_mrca_naive.py
@@ -14,6 +14,16 @@ def test_estimate_rank_of_mrca_naive_no_common_ancestor():
 
     assert result is None
 
+def test_estimate_rank_of_mrca_naive_with_common_ancestor1b():
+    coincident_ranks = iter([1, 0])
+    p_differentia_collision = 0.5
+
+    result = hstrat.estimate_rank_of_mrca_naive(
+        coincident_ranks, p_differentia_collision, hstrat.ArbitraryPrior()
+    )
+
+    assert result == 0
+
 
 def test_estimate_rank_of_mrca_naive_with_common_ancestor1():
     coincident_ranks = iter([7, 3, 0])
