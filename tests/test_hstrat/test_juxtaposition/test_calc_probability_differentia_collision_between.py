4aa3f1d4dd3a395b4dc8d3a0c045af37b426a574
Matthew Andres Moreno <mmore500.login+gpg@gmail.com>
2023-03-18 20:34:55 -0700

    Test calc prob diff collision for specimens

--- tests/test_hstrat/test_juxtaposition/test_calc_probability_differentia_collision_between.py
+++ tests/test_hstrat/test_juxtaposition/test_calc_probability_differentia_collision_between.py
@@ -1,47 +1,69 @@
+import pytest
+
 from hstrat import hstrat
 
 
-def test_calc_probability_differentia_collision_between():
+@pytest.mark.parametrize(
+    "wrap",
+    [lambda x: x, hstrat.col_to_specimen],
+)
+def test_calc_probability_differentia_collision_between(wrap):
     assert (
         hstrat.calc_probability_differentia_collision_between(
-            hstrat.HereditaryStratigraphicColumn(
-                stratum_differentia_bit_width=1
+            wrap(
+                hstrat.HereditaryStratigraphicColumn(
+                    stratum_differentia_bit_width=1
+                )
             ),
-            hstrat.HereditaryStratigraphicColumn(
-                stratum_differentia_bit_width=1
+            wrap(
+                hstrat.HereditaryStratigraphicColumn(
+                    stratum_differentia_bit_width=1
+                )
             ),
         )
         == 0.5
     )
     assert (
         hstrat.calc_probability_differentia_collision_between(
-            hstrat.HereditaryStratigraphicColumn(
-                stratum_differentia_bit_width=2
+            wrap(
+                hstrat.HereditaryStratigraphicColumn(
+                    stratum_differentia_bit_width=2
+                )
             ),
-            hstrat.HereditaryStratigraphicColumn(
-                stratum_differentia_bit_width=2
+            wrap(
+                hstrat.HereditaryStratigraphicColumn(
+                    stratum_differentia_bit_width=2
+                )
             ),
         )
         == 0.25
     )
     assert (
         hstrat.calc_probability_differentia_collision_between(
-            hstrat.HereditaryStratigraphicColumn(
-                stratum_differentia_bit_width=3
+            wrap(
+                hstrat.HereditaryStratigraphicColumn(
+                    stratum_differentia_bit_width=3
+                )
             ),
-            hstrat.HereditaryStratigraphicColumn(
-                stratum_differentia_bit_width=3
+            wrap(
+                hstrat.HereditaryStratigraphicColumn(
+                    stratum_differentia_bit_width=3
+                )
             ),
         )
         == 0.125
     )
     assert (
         hstrat.calc_probability_differentia_collision_between(
-            hstrat.HereditaryStratigraphicColumn(
-                stratum_differentia_bit_width=8
+            wrap(
+                hstrat.HereditaryStratigraphicColumn(
+                    stratum_differentia_bit_width=8
+                )
             ),
-            hstrat.HereditaryStratigraphicColumn(
-                stratum_differentia_bit_width=8
+            wrap(
+                hstrat.HereditaryStratigraphicColumn(
+                    stratum_differentia_bit_width=8
+                )
             ),
         )
         == 1.0 / 256.0
