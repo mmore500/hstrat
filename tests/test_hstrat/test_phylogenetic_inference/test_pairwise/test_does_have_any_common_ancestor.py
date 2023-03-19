0e4d59be8926981f2d226644ee2bc987cee383c6
Matthew Andres Moreno <mmore500.login+gpg@gmail.com>
2023-03-18 17:17:41 -0700

    Fixup, test has any common ancest for specimens

--- tests/test_hstrat/test_phylogenetic_inference/test_pairwise/test_does_have_any_common_ancestor.py
+++ tests/test_hstrat/test_phylogenetic_inference/test_pairwise/test_does_have_any_common_ancestor.py
@@ -9,6 +9,84 @@ from hstrat import hstrat
         hstrat.perfect_resolution_algo.Policy(),
         hstrat.nominal_resolution_algo.Policy(),
         hstrat.fixed_resolution_algo.Policy(fixed_resolution=10),
+        hstrat.recency_proportional_resolution_algo.Policy(
+            recency_proportional_resolution=2
+        ),
+    ],
+)
+@pytest.mark.parametrize(
+    "differentia_width",
+    [1, 2, 8, 64],
+)
+@pytest.mark.parametrize(
+    "confidence_level",
+    [0.95, 0.88],
+)
+def test_does_have_any_common_ancestor_specimen(
+    retention_policy, differentia_width, confidence_level
+):
+    column = hstrat.HereditaryStratigraphicColumn(
+        stratum_retention_policy=retention_policy,
+        stratum_differentia_bit_width=differentia_width,
+    )
+    column2 = hstrat.HereditaryStratigraphicColumn(
+        stratum_retention_policy=retention_policy,
+        stratum_differentia_bit_width=differentia_width,
+    )
+    for generation in range(100):
+        column.DepositStratum()
+
+    child1 = column.CloneDescendant()
+    child2 = column.CloneDescendant()
+
+    assert hstrat.does_have_any_common_ancestor(
+        hstrat.col_to_specimen(column),
+        hstrat.col_to_specimen(column2),
+        confidence_level=confidence_level,
+    ) == hstrat.does_have_any_common_ancestor(
+        column, column2, confidence_level=confidence_level
+    )
+
+    assert hstrat.does_have_any_common_ancestor(
+        hstrat.col_to_specimen(column),
+        hstrat.col_to_specimen(column),
+        confidence_level=confidence_level,
+    ) == hstrat.does_have_any_common_ancestor(
+        column, column, confidence_level=confidence_level
+    )
+
+    assert hstrat.does_have_any_common_ancestor(
+        hstrat.col_to_specimen(column),
+        hstrat.col_to_specimen(child1),
+        confidence_level=confidence_level,
+    ) == hstrat.does_have_any_common_ancestor(
+        column, child1, confidence_level=confidence_level
+    )
+
+    assert hstrat.does_have_any_common_ancestor(
+        hstrat.col_to_specimen(child1),
+        hstrat.col_to_specimen(child2),
+        confidence_level=confidence_level,
+    ) == hstrat.does_have_any_common_ancestor(
+        child1, child2, confidence_level=confidence_level
+    )
+
+    child1.DepositStrata(10)
+    assert hstrat.does_have_any_common_ancestor(
+        hstrat.col_to_specimen(child1),
+        hstrat.col_to_specimen(child2),
+        confidence_level=confidence_level,
+    ) == hstrat.does_have_any_common_ancestor(
+        child1, child2, confidence_level=confidence_level
+    )
+
+
+@pytest.mark.parametrize(
+    "retention_policy",
+    [
+        hstrat.perfect_resolution_algo.Policy(),
+        hstrat.nominal_resolution_algo.Policy(),
+        hstrat.fixed_resolution_algo.Policy(fixed_resolution=10),
     ],
 )
 @pytest.mark.parametrize(
