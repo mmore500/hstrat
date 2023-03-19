34801283bc2708004833d672bf6e9d70a16f7787
Matthew Andres Moreno <mmore500.login+gpg@gmail.com>
2023-03-19 15:23:52 -0700

    Strip out dead code

--- tests/test_hstrat/test_phylogenetic_inference/test_tree/_impl/_tree_distance_metric.py
+++ tests/test_hstrat/test_phylogenetic_inference/test_tree/_impl/_tree_distance_metric.py
@@ -3,11 +3,9 @@ import dendropy as dp
 
 
 def tree_distance_metric(x, y) -> float:
-    # use dendropy trees
     tree_a = apc.RosettaTree(x).as_dendropy
     tree_b = apc.RosettaTree(y).as_dendropy
 
-    # tree_a = copy.deepcopy(tree_b)
     common_namespace = dp.TaxonNamespace()
     tree_a.migrate_taxon_namespace(common_namespace)
     tree_b.migrate_taxon_namespace(common_namespace)
@@ -18,42 +16,6 @@ def tree_distance_metric(x, y) -> float:
     tree_b.encode_bipartitions()
     for bp in tree_b.bipartition_encoding:
         bp.is_mutable = False
-
-    # sort_by_taxa_name(tree_a)
-    # sort_by_taxa_name(tree_b)
-
-    # tree_b.reseed_at(
-    #     tree_b.find_node_with_taxon_label("Inner2"),
-    #     collapse_unrooted_basal_bifurcation=False,
-    # )
-    # tree_a.print_plot(
-    #     show_internal_node_labels=True,
-    #     plot_metric='level'
-    # )
-    # tree_b.print_plot(
-    #     show_internal_node_labels=True,
-    #     plot_metric='level'
-    # )
-
-    # tree_a.collapse_unweighted_edges()
-    # tree_b.collapse_unweighted_edges()
-
-    # tree_a.print_plot(
-    #     show_internal_node_labels=True,
-    #     plot_metric='level'
-    # )
-    # tree_b.print_plot(
-    #     show_internal_node_labels=True,
-    #     plot_metric='level'
-    # )
-    # print("tree_a, tree_b", dp.calculate.treecompare.false_positives_and_negatives(tree_a, tree_b))
-    # print("tree_b, tree_a", dp.calculate.treecompare.false_positives_and_negatives(tree_b, tree_a))
-
-    # print("tree_a", len(set(tree_a.bipartition_encoding)))
-    # print("tree_b", len(set(tree_b.bipartition_encoding)))
-
-    # print(sorted([x.level() for x in tree_a]))
-    # print(sorted([x.level() for x in tree_b]))
 
     return dp.calculate.treecompare.symmetric_difference(
         tree_a, tree_b, is_bipartitions_updated=True
