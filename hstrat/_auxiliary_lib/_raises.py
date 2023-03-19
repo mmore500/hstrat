da4db5dee61ce1130180bd47fa91dd5f5da86f0d
Matthew Andres Moreno <mmore500.login+gpg@gmail.com>
2023-03-18 23:18:30 -0700

    Revert "Remove unused named except"
    This reverts commit b13df630c61b8d91bfc60bb6844b0b54de5d877a.

--- hstrat/_auxiliary_lib/_raises.py
+++ hstrat/_auxiliary_lib/_raises.py
@@ -31,5 +31,5 @@ def raises(callable: typing.Callable, exception: BaseException) -> bool:
     try:
         callable()
         return False
-    except:
+    except exception:
         return True
