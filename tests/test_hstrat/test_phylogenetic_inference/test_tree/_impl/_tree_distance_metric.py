9d49c005c20af70496c2a7b9927b78fd17d070ae
Matthew Andres Moreno <mmore500.login+gpg@gmail.com>
2023-03-20 12:03:28 -0700

    Attempt to normalize dendropy RF distance

--- tests/test_hstrat/test_phylogenetic_inference/test_tree/_impl/_tree_distance_metric.py
+++ tests/test_hstrat/test_phylogenetic_inference/test_tree/_impl/_tree_distance_metric.py
@@ -1,5 +1,6 @@
 import alifedata_phyloinformatics_convert as apc
 import dendropy as dp
+from iterpop import iterpop as ip
 
 
 def tree_distance_metric(x, y) -> float:
@@ -17,6 +18,18 @@ def tree_distance_metric(x, y) -> float:
     for bp in tree_b.bipartition_encoding:
         bp.is_mutable = False
 
-    return dp.calculate.treecompare.symmetric_difference(
-        tree_a, tree_b, is_bipartitions_updated=True
+    # see https://rdrr.io/cran/phangorn/man/treedist.html
+    estimated_max_rf_dist = (
+        2
+        * ip.pophomogeneous(
+            (len(tree_a.leaf_nodes()), len(tree_b.leaf_nodes()))
+        )
+        - 6
+    )
+
+    return (
+        dp.calculate.treecompare.symmetric_difference(
+            tree_a, tree_b, is_bipartitions_updated=True
+        )
+        / estimated_max_rf_dist
     )
