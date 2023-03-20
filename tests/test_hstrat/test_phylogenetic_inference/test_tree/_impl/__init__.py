2bb7005b4528be4012491d716520d217ce636d1b
Matthew Andres Moreno <mmore500.login+gpg@gmail.com>
2023-03-19 22:50:20 -0700

    Delete unused sort_by_taxa_name

--- tests/test_hstrat/test_phylogenetic_inference/test_tree/_impl/__init__.py
+++ tests/test_hstrat/test_phylogenetic_inference/test_tree/_impl/__init__.py
@@ -3,7 +3,6 @@
 from ._descend_unifurcations import descend_unifurcations
 from ._load_dendropy_tree import load_dendropy_tree
 from ._setup_dendropy_tree import setup_dendropy_tree
-from ._sort_by_taxa_name import sort_by_taxa_name
 from ._tree_distance_metric import tree_distance_metric
 
 # adapted from https://stackoverflow.com/a/31079085
@@ -11,6 +10,5 @@ __all__ = [
     "descend_unifurcations",
     "load_dendropy_tree",
     "setup_dendropy_tree",
-    "sort_by_taxa_name",
     "tree_distance_metric",
 ]
