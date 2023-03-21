3d854d8cd64fe3ce225c62f0d4f20309bd26af86
Matthew Andres Moreno <mmore500.login+gpg@gmail.com>
2023-03-20 21:21:43 -0700

    Setup dist tree build quality tests

--- tests/test_hstrat/test_phylogenetic_inference/test_tree/_impl/_tree_distance_metric.py
+++ tests/test_hstrat/test_phylogenetic_inference/test_tree/_impl/_tree_distance_metric.py
@@ -1,35 +1,30 @@
 import alifedata_phyloinformatics_convert as apc
-import dendropy as dp
-from iterpop import iterpop as ip
+import tqdist
 
 
 def tree_distance_metric(x, y) -> float:
     tree_a = apc.RosettaTree(x).as_dendropy
     tree_b = apc.RosettaTree(y).as_dendropy
 
-    common_namespace = dp.TaxonNamespace()
-    tree_a.migrate_taxon_namespace(common_namespace)
-    tree_b.migrate_taxon_namespace(common_namespace)
+    # must suppress root unifurcations or tqdist barfs
+    # see https://github.com/uym2/tripVote/issues/15
+    tree_a.unassign_taxa(exclude_leaves=True)
+    tree_a.suppress_unifurcations()
+    tree_b.unassign_taxa(exclude_leaves=True)
+    tree_b.suppress_unifurcations()
 
-    tree_a.encode_bipartitions()
-    for bp in tree_a.bipartition_encoding:
-        bp.is_mutable = False
-    tree_b.encode_bipartitions()
-    for bp in tree_b.bipartition_encoding:
-        bp.is_mutable = False
+    tree_a_taxon_labels = [
+        leaf.taxon.label for leaf in tree_a.leaf_node_iter()
+    ]
+    tree_b_taxon_labels = [
+        leaf.taxon.label for leaf in tree_b.leaf_node_iter()
+    ]
+    assert {*tree_a_taxon_labels} == {*tree_b_taxon_labels}
+    for taxon_label in tree_a_taxon_labels:
+        assert taxon_label
+        assert taxon_label.strip()
 
-    # see https://rdrr.io/cran/phangorn/man/treedist.html
-    estimated_max_rf_dist = (
-        2
-        * ip.pophomogeneous(
-            (len(tree_a.leaf_nodes()), len(tree_b.leaf_nodes()))
-        )
-        - 6
-    )
-
-    return (
-        dp.calculate.treecompare.symmetric_difference(
-            tree_a, tree_b, is_bipartitions_updated=True
-        )
-        / estimated_max_rf_dist
+    return tqdist.quartet_distance(
+        tree_a.as_string(schema="newick").strip(),
+        tree_b.as_string(schema="newick").strip(),
     )
