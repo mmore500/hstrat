c3f51dcadc27f776bf34dec95eae357c74be9f65
Matthew Andres Moreno <mmore500.login+gpg@gmail.com>
2023-05-03 22:03:36 -0700

    Refactor docplot paramaterizations to fixture

--- tests/test_hstrat/test_stratum_retention_viz/test_plot/test_strata_retained_frac_lineplot.py
+++ tests/test_hstrat/test_stratum_retention_viz/test_plot/test_strata_retained_frac_lineplot.py
@@ -21,79 +21,14 @@ def test(policy):
     release_cur_mpl_fig()
 
 
-@pytest.mark.parametrize(
-    "policy",
-    [
-        policy_t(
-            parameterizer=hstrat.PropertyExactlyParameterizer(
-                target_value=target_value,
-                policy_evaluator=hstrat.MrcaUncertaintyAbsExactEvaluator(
-                    at_num_strata_deposited=256,
-                    at_rank=0,
-                ),
-                param_lower_bound=lb,
-                param_upper_bound=1024,
-            )
-        )
-        for policy_t, lb in (
-            (hstrat.fixed_resolution_algo.Policy, 1),
-            (hstrat.depth_proportional_resolution_algo.Policy, 1),
-            (hstrat.depth_proportional_resolution_tapered_algo.Policy, 1),
-            (hstrat.recency_proportional_resolution_algo.Policy, 0),
-        )
-        for target_value in (31, 127)
-    ]
-    + [
-        policy_t(
-            parameterizer=hstrat.PropertyAtLeastParameterizer(
-                target_value=31,
-                policy_evaluator=hstrat.MrcaUncertaintyAbsExactEvaluator(
-                    at_num_strata_deposited=256,
-                    at_rank=0,
-                ),
-                param_lower_bound=1,
-                param_upper_bound=1024,
-            )
-        )
-        for policy_t in (
-            hstrat.geom_seq_nth_root_algo.Policy,
-            hstrat.geom_seq_nth_root_tapered_algo.Policy,
-        )
-    ]
-    + [
-        hstrat.geom_seq_nth_root_algo.Policy(
-            parameterizer=hstrat.PropertyExactlyParameterizer(
-                target_value=127,
-                policy_evaluator=hstrat.MrcaUncertaintyAbsExactEvaluator(
-                    at_num_strata_deposited=256,
-                    at_rank=0,
-                ),
-                param_lower_bound=1,
-                param_upper_bound=1024,
-            )
-        )
-    ]
-    + [
-        hstrat.geom_seq_nth_root_tapered_algo.Policy(
-            parameterizer=hstrat.PropertyAtMostParameterizer(
-                target_value=127,
-                policy_evaluator=hstrat.MrcaUncertaintyAbsExactEvaluator(
-                    at_num_strata_deposited=256,
-                    at_rank=0,
-                ),
-                param_lower_bound=1,
-                param_upper_bound=1024,
-            )
-        )
-    ],
-)
-def test_docplots(policy):
+@pytest.mark.heavy
+def test_docplots(docplot_policy):
     tp.tee(
         hstrat.strata_retained_frac_lineplot,
-        policy,
+        docplot_policy,
         256,
         teeplot_outattrs={
-            "policy": slugify(str(policy)),
+            "policy": slugify(str(docplot_policy)),
             "num_generations": "256",
         },
         teeplot_transparent=False,
