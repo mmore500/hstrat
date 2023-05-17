9fbe39354d11ad9a917d0ef1d26fb41d36ae215c
Matthew Andres Moreno <mmore500.login+gpg@gmail.com>
2023-05-17 14:57:31 -0700

    Break GenDropRanks tests into own files

---
+++ tests/test_hstrat/test_stratum_retention_strategy/test_stratum_retention_algorithms/test_recency_proportional_resolution_curbed_algo/test_GenDropRanks/test_eq.py
@@ -0,0 +1,27 @@
+import pytest
+
+from hstrat.hstrat import recency_proportional_resolution_curbed_algo
+
+
+@pytest.mark.parametrize(
+    "size_curb",
+    [
+        2,
+        3,
+        7,
+        42,
+        97,
+        100,
+    ],
+)
+def test_eq(size_curb):
+    policy = recency_proportional_resolution_curbed_algo.Policy(size_curb)
+    spec = policy.GetSpec()
+    instance = recency_proportional_resolution_curbed_algo.GenDropRanks(spec)
+
+    assert instance == instance
+    assert (
+        instance
+        == recency_proportional_resolution_curbed_algo.GenDropRanks(spec)
+    )
+    assert instance is not None
