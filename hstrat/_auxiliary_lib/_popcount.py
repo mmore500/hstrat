23b00b962e36ea29de678d317e654f4b097a0ac6
Matthew Andres Moreno <mmore500.login+gpg@gmail.com>
2023-05-17 22:40:29 -0700

    Cast to int to make popcount robust to np.int64 etc

--- hstrat/_auxiliary_lib/_popcount.py
+++ hstrat/_auxiliary_lib/_popcount.py
@@ -11,6 +11,7 @@ def popcount(val: int) -> int:
         return val.bit_count()
     except AttributeError:
         return bitstring.Bits(
-            uint=val,
-            length=(val + 1).bit_length(),
+            # int casts makes robust to numpy int64 etc.
+            uint=int(val),
+            length=int(val + 1).bit_length(),
         ).count(1)
