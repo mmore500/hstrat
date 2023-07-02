3ef45012ae8e92b66b03aa9e1c17c00b2f55fecf
Matthew Andres Moreno <mmore500.login+gpg@gmail.com>
2023-07-02 10:26:06 -0700

    Note inhertance as pickling workaround

--- hstrat/stratum_retention_strategy/stratum_retention_algorithms/pseudostochastic_algo/_invar/_CalcMrcaUncertaintyRelUpperBound.py
+++ hstrat/stratum_retention_strategy/stratum_retention_algorithms/pseudostochastic_algo/_invar/_CalcMrcaUncertaintyRelUpperBound.py
@@ -1,6 +1,8 @@
 from ..._impl import CalcMrcaUncertaintyRelUpperBoundWorstCase
 
 
+# must inherit rather than assign due to failure of attribute lookup when
+# pickling otherwise
 class CalcMrcaUncertaintyRelUpperBound(
     CalcMrcaUncertaintyRelUpperBoundWorstCase
 ):
--- hstrat/stratum_retention_strategy/stratum_retention_algorithms/recency_proportional_resolution_algo/_scry/_IterRetainedRanks.py
+++ hstrat/stratum_retention_strategy/stratum_retention_algorithms/recency_proportional_resolution_algo/_scry/_IterRetainedRanks.py
@@ -1,5 +1,7 @@
 from ._IterRetainedRanks_ import IterRetainedRanksProduction
 
 
+# must inherit rather than assign due to failure of attribute lookup when
+# pickling otherwise
 class IterRetainedRanks(IterRetainedRanksProduction):
     pass
