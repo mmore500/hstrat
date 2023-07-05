f4d06a834f66a0858f5966eeb7259f9479cad1d4
Matthew Andres Moreno <mmore500.login+gpg@gmail.com>
2023-07-05 14:38:42 -0700

    Fix whitespace

--- hstrat/_auxiliary_lib/_count_trailing_zeros.py
+++ hstrat/_auxiliary_lib/_count_trailing_zeros.py
@@ -2,7 +2,9 @@ def count_trailing_zeros(n: int) -> int:
     """Count the number of trailing zeros in the binary representation of the
     absolute value of an integer.
 
-    Trailing zeros are contiguous unset bits before the first set bit at the least significant (rightmost) position. For example, the number 8 (1000 in binary) has 3 trailing zeros. Returns 0 trailing bits for 0.
+    Trailing zeros are contiguous unset bits before the first set bit at the
+    least significant (rightmost) position. For example, the number 8 
+    (1000 in binary) has 3 trailing zeros. Returns 0 trailing bits for 0.
 
     Examples
     --------
