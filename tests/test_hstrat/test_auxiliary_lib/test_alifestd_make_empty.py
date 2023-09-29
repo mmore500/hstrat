c8ed12fa044f6e8ddf93f77f1b2ebbe371540777
Matthew Andres Moreno <mmore500.login+gpg@gmail.com>
2023-09-29 18:42:45 -0400

    Test new alifestd_make_empty kwarg

--- tests/test_hstrat/test_auxiliary_lib/test_alifestd_make_empty.py
+++ tests/test_hstrat/test_auxiliary_lib/test_alifestd_make_empty.py
@@ -6,3 +6,11 @@ def test_alifestd_make_empty():
     assert alifestd_validate(alifestd_make_empty())
     assert "id" in alifestd_make_empty()
     assert "ancestor_list" in alifestd_make_empty()
+
+
+def test_alifestd_make_empty_ancestor_id():
+    assert len(alifestd_make_empty(ancestor_id=True)) == 0
+    assert alifestd_validate(alifestd_make_empty(ancestor_id=True))
+    assert "id" in alifestd_make_empty(ancestor_id=True)
+    assert "ancestor_list" in alifestd_make_empty(ancestor_id=True)
+    assert "ancestor_id" in alifestd_make_empty(ancestor_id=True)
