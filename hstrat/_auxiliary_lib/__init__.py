30b9c5cc7fe4bd30f92d020630383980aecd277d
Matthew Andres Moreno <mmore500.login+gpg@gmail.com>
2023-09-29 01:00:13 -0400

    Impl, test alifestd_mark_num_leaves_asexual

--- hstrat/_auxiliary_lib/__init__.py
+++ hstrat/_auxiliary_lib/__init__.py
@@ -42,6 +42,7 @@ from ._alifestd_make_empty import alifestd_make_empty
 from ._alifestd_mark_num_descendants_asexual import (
     alifestd_mark_num_descendants_asexual,
 )
+from ._alifestd_mark_num_leaves_asexual import alifestd_mark_num_leaves_asexual
 from ._alifestd_parse_ancestor_id import alifestd_parse_ancestor_id
 from ._alifestd_parse_ancestor_ids import alifestd_parse_ancestor_ids
 from ._alifestd_prune_extinct_lineages_asexual import (
@@ -183,6 +184,7 @@ __all__ = [
     "alifestd_make_ancestor_list_col",
     "alifestd_make_empty",
     "alifestd_mark_num_descendants_asexual",
+    "alifestd_mark_num_leaves_asexual",
     "alifestd_parse_ancestor_id",
     "alifestd_parse_ancestor_ids",
     "alifestd_prune_extinct_lineages_asexual",
