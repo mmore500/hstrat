14023c1a4138407b6ac2f868da1948cdfcce26c5
Matthew Andres Moreno <mmore500.login+gpg@gmail.com>
2023-10-01 11:38:56 -0400

    Clarify docstring

--- hstrat/_auxiliary_lib/_alifestd_is_chronologically_ordered.py
+++ hstrat/_auxiliary_lib/_alifestd_is_chronologically_ordered.py
@@ -6,7 +6,8 @@ from ._alifestd_find_chronological_inconsistency import (
 
 
 def alifestd_is_chronologically_ordered(phylogeny_df: pd.DataFrame) -> bool:
-    """Are all organisms listed after members of their `ancestor_list`?
+    """Do any organisms have `origin_time`s preceding membersof their
+    `ancestor_list`?
 
     Input dataframe is not mutated by this operation.
     """
