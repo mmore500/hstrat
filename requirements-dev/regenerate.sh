b77ee28a0f837ce9615a5ef8cb141ac18b66ee3c
Matthew Andres Moreno <mmore500.login+gpg@gmail.com>
2023-10-02 00:25:20 -0400

    Drop Python3.7 support
    py 3.7 was eol'ed in June 2023

--- requirements-dev/regenerate.sh
+++ requirements-dev/regenerate.sh
@@ -4,13 +4,13 @@
 # adapted from https://stackoverflow.com/a/6393573
 cd "${0%/*}"
 
-# python versions 3.8 and 3.9 are symlinked to 3.7
-./py37/regenerate.sh
+# python version 3.9 is symlinked to 3.8
+./py38/regenerate.sh
 
 ./py310/regenerate.sh
 
 ./py311/regenerate.sh
 
-for f in py37/*.txt; do
+for f in py38*.txt; do
   cat "${f}" | sed "s/==/>=/g" > "$(basename "${f}")"
 done
