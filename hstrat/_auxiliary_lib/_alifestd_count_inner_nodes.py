f6e51bf5e7339e9c29c92989e3f742941680ccfd
Matthew Andres Moreno <mmore500.login+gpg@gmail.com>
2024-01-22 01:35:41 -0500

    Drop collapse_unifurcations kwarg

--- hstrat/_auxiliary_lib/_alifestd_count_inner_nodes.py
+++ hstrat/_auxiliary_lib/_alifestd_count_inner_nodes.py
@@ -6,19 +6,11 @@ from ._alifestd_count_leaf_nodes import alifestd_count_leaf_nodes
 
 def alifestd_count_inner_nodes(
     phylogeny_df: pd.DataFrame,
-    collapse_unifurcations: bool = False,
     mutate: bool = False,
 ) -> int:
     """Count how many non-leaf nodes are contained in phylogeny."""
-    if collapse_unifurcations:
-        if not mutate:
-            phylogeny_df = phylogeny_df.copy()
-        phylogeny_df = alifestd_collapse_unifurcations(
-            phylogeny_df, mutate=True
-        )
 
     num_leaves = alifestd_count_leaf_nodes(phylogeny_df)
     res = len(phylogeny_df) - num_leaves
     assert res >= 0
-    assert res < num_leaves or not collapse_unifurcations
     return res
