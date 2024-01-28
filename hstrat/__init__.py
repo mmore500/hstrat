21d0e97857ba70cbe5b07cefed701dc982171e46
Matthew Andres Moreno <mmore500.login+gpg@gmail.com>
2024-01-28 03:31:53 -0500

    bump version to v1.9.0

--- hstrat/__init__.py
+++ hstrat/__init__.py
@@ -2,7 +2,7 @@
 
 __author__ = """Matthew Andres Moreno"""
 __email__ = "m.more500@gmail.com"
-__version__ = "1.8.8"
+__version__ = "1.9.0"
 
 from . import (
     _auxiliary_lib,
--- hstrat/_auxiliary_lib/_get_hstrat_version.py
+++ hstrat/_auxiliary_lib/_get_hstrat_version.py
@@ -1,2 +1,2 @@
 def get_hstrat_version() -> str:
-    return "1.8.8"
+    return "1.9.0"
