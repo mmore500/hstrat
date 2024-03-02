abf4a157ae1563dfed1ec6a4202b5b0f8e7e821e
Matthew Andres Moreno <mmore500.login+gpg@gmail.com>
2024-03-01 23:36:24 -0500

    Strip debugging leftovers

--- hstrat/serialization/_col_to_int.py
+++ hstrat/serialization/_col_to_int.py
@@ -32,11 +32,7 @@ def col_to_int(
     sentry_bit = 1 << buffer_num_bits
     assert sentry_bit.bit_length() % 8 == 1
     semantic_value = int.from_bytes(buffer, byteorder="big", signed=False)
-    assert semantic_value.bit_length() <= buffer_num_bits, (
-        semantic_value,
-        semantic_value.bit_length(),
-        buffer_num_bits,
-    )
+    assert semantic_value.bit_length() <= buffer_num_bits
     assert semantic_value & sentry_bit == 0
 
     return semantic_value | sentry_bit
