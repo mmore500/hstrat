373f34efe80cc053e25299a9f968d46f643173ce
Matthew Andres Moreno <mmore500.login+gpg@gmail.com>
2024-03-08 14:36:55 -0500

    Add triplet distance sensitivity tests

--- tests/test_hstrat/test_auxiliary_lib/test_alifestd_estimate_triplet_distance_asexual.py
+++ tests/test_hstrat/test_auxiliary_lib/test_alifestd_estimate_triplet_distance_asexual.py
@@ -1,3 +1,4 @@
+import functools
 import itertools as it
 import typing
 
@@ -9,6 +10,8 @@ from hstrat._auxiliary_lib import (
     alifestd_estimate_triplet_distance_asexual,
     alifestd_make_ancestor_list_col,
     alifestd_make_empty,
+    alifestd_to_working_format,
+    alifestd_validate,
 )
 
 
@@ -407,4 +410,253 @@ def test_differing_polytomy_asymmetrical_strict():
         confidence=0.95,
         precision=0.05,
         strict=(False, False),
+    )
+
+
+@functools.lru_cache
+def make_ref_df(working_format: bool) -> pd.DataFrame:
+    #        |
+    #      __|_
+    #    __|_ |
+    #  __|_ | |
+    # _|_ | | |
+    # | | | | |
+    # A B C D E
+    ref_df = pd.DataFrame.from_records(
+        [
+            {"id": 0, "taxon_label": "A", "ancestor_id": 5},
+            {"id": 1, "taxon_label": "B", "ancestor_id": 5},
+            {"id": 2, "taxon_label": "C", "ancestor_id": 6},
+            {"id": 3, "taxon_label": "D", "ancestor_id": 7},
+            {"id": 4, "taxon_label": "E", "ancestor_id": 8},
+            {"id": 5, "taxon_label": "AB", "ancestor_id": 6},
+            {"id": 6, "taxon_label": "ABC", "ancestor_id": 7},
+            {"id": 7, "taxon_label": "ABCD", "ancestor_id": 8},
+            {"id": 8, "taxon_label": "ABCDE", "ancestor_id": 8},
+        ]
+    )
+    ref_df["ancestor_list"] = alifestd_make_ancestor_list_col(
+        ref_df["id"], ref_df["ancestor_id"]
+    )
+    if working_format:
+        ref_df = alifestd_to_working_format(ref_df)
+
+    assert alifestd_validate(ref_df)
+    assert not alifestd_estimate_triplet_distance_asexual(
+        ref_df,
+        ref_df.sample(frac=1),
+        "taxon_label",
+        confidence=0.85,
+        precision=0.05,
+        strict=True,
+    )
+    return ref_df
+
+
+@functools.lru_cache
+def make_swap_df(working_format: bool) -> pd.DataFrame:
+    #       |
+    #    ___|__
+    #  __|__  |
+    # _|_ _|_ |
+    # | | | | |
+    # * B C D E
+    # A
+    swap_df = pd.DataFrame.from_records(
+        [
+            {"id": 0, "taxon_label": "A", "ancestor_id": 9},
+            {"id": 1, "taxon_label": "B", "ancestor_id": 5},
+            {"id": 2, "taxon_label": "C", "ancestor_id": 6},
+            {"id": 3, "taxon_label": "D", "ancestor_id": 6},
+            {"id": 4, "taxon_label": "E", "ancestor_id": 8},
+            {"id": 5, "taxon_label": "AB", "ancestor_id": 7},
+            {"id": 6, "taxon_label": "CD", "ancestor_id": 7},
+            {"id": 7, "taxon_label": "ABCD", "ancestor_id": 8},
+            {"id": 8, "taxon_label": "ABCDE", "ancestor_id": 8},
+            {"id": 9, "taxon_label": "A*", "ancestor_id": 5},
+        ]
+    )
+    swap_df["ancestor_list"] = alifestd_make_ancestor_list_col(
+        swap_df["id"], swap_df["ancestor_id"]
+    )
+    if working_format:
+        swap_df = alifestd_to_working_format(swap_df)
+
+    assert alifestd_validate(swap_df)
+    assert not alifestd_estimate_triplet_distance_asexual(
+        swap_df,
+        swap_df.sample(frac=1),
+        "taxon_label",
+        confidence=0.85,
+        precision=0.05,
+        strict=True,
+    )
+    return swap_df
+
+
+@functools.lru_cache
+def make_polytomy_df(working_format: bool) -> pd.DataFrame:
+    #       |
+    #     __|__
+    #  ___|__ |
+    # _|_ | | |
+    # | | | | |
+    # A B C D E
+    polytomy_df = pd.DataFrame.from_records(
+        [
+            {"id": 0, "taxon_label": "A", "ancestor_id": 5},
+            {"id": 1, "taxon_label": "B", "ancestor_id": 5},
+            {"id": 2, "taxon_label": "C", "ancestor_id": 7},
+            {"id": 3, "taxon_label": "D", "ancestor_id": 7},
+            {"id": 4, "taxon_label": "E", "ancestor_id": 8},
+            {"id": 5, "taxon_label": "AB", "ancestor_id": 7},
+            {"id": 7, "taxon_label": "ABCD", "ancestor_id": 8},
+            {"id": 8, "taxon_label": "ABCDE", "ancestor_id": 8},
+        ]
+    )
+    polytomy_df["ancestor_list"] = alifestd_make_ancestor_list_col(
+        polytomy_df["id"], polytomy_df["ancestor_id"]
+    )
+    if working_format:
+        polytomy_df = alifestd_to_working_format(polytomy_df)
+
+    assert alifestd_validate(polytomy_df)
+    assert not alifestd_estimate_triplet_distance_asexual(
+        polytomy_df,
+        polytomy_df.sample(frac=1),
+        "taxon_label",
+        confidence=0.85,
+        precision=0.05,
+        strict=True,
+    )
+    return polytomy_df
+
+
+@pytest.mark.parametrize(
+    "strict",
+    [
+        True,
+        False,
+        *it.product([True, False], repeat=2),
+    ],
+)
+@pytest.mark.parametrize(
+    "working_format", [True, pytest.param(False, marks=pytest.mark.heavy)]
+)
+def test_sensitivity1(
+    strict: typing.Union[bool, typing.Tuple[bool, bool]],
+    working_format: bool,
+):
+
+    assert alifestd_estimate_triplet_distance_asexual(
+        make_ref_df(working_format),
+        make_swap_df(working_format),
+        "taxon_label",
+        confidence=0.85,
+        precision=0.05,
+        strict=strict,
+    )
+
+
+@pytest.mark.parametrize(
+    "strict",
+    [
+        True,
+        (False, True),
+    ],
+)
+@pytest.mark.parametrize(
+    "working_format", [True, pytest.param(False, marks=pytest.mark.heavy)]
+)
+def test_sensitivity2(
+    strict: typing.Union[bool, typing.Tuple[bool, bool]],
+    working_format: bool,
+):
+    assert alifestd_estimate_triplet_distance_asexual(
+        make_ref_df(working_format),
+        make_polytomy_df(working_format),
+        "taxon_label",
+        confidence=0.85,
+        precision=0.05,
+        strict=strict,
+    )
+    assert alifestd_estimate_triplet_distance_asexual(
+        make_ref_df(working_format).sample(frac=1),
+        make_polytomy_df(working_format).sample(frac=1),
+        "taxon_label",
+        confidence=0.85,
+        precision=0.05,
+        strict=strict,
+    )
+
+
+@pytest.mark.parametrize(
+    "strict",
+    [
+        False,
+        (True, False),
+    ],
+)
+@pytest.mark.parametrize(
+    "working_format", [True, pytest.param(False, marks=pytest.mark.heavy)]
+)
+def test_sensitivity3(
+    strict: typing.Union[bool, typing.Tuple[bool, bool]],
+    working_format: bool,
+):
+    assert not alifestd_estimate_triplet_distance_asexual(
+        make_ref_df(working_format),
+        make_polytomy_df(working_format),
+        "taxon_label",
+        confidence=0.85,
+        precision=0.05,
+        strict=strict,
+    )
+
+
+@pytest.mark.parametrize(
+    "strict",
+    [
+        True,
+        (False, True),
+    ],
+)
+@pytest.mark.parametrize(
+    "working_format", [True, pytest.param(False, marks=pytest.mark.heavy)]
+)
+def test_sensitivity4(
+    strict: typing.Union[bool, typing.Tuple[bool, bool]],
+    working_format: bool,
+):
+    assert alifestd_estimate_triplet_distance_asexual(
+        make_swap_df(working_format),
+        make_polytomy_df(working_format),
+        "taxon_label",
+        confidence=0.85,
+        precision=0.05,
+        strict=strict,
+    )
+
+
+@pytest.mark.parametrize(
+    "strict",
+    [
+        False,
+        (False, True),
+    ],
+)
+@pytest.mark.parametrize(
+    "working_format", [True, pytest.param(False, marks=pytest.mark.heavy)]
+)
+def test_sensitivity5(
+    strict: typing.Union[bool, typing.Tuple[bool, bool]],
+    working_format: bool,
+):
+    assert not alifestd_estimate_triplet_distance_asexual(
+        make_polytomy_df(working_format),
+        make_swap_df(working_format),
+        "taxon_label",
+        confidence=0.85,
+        precision=0.05,
+        strict=strict,
     )
