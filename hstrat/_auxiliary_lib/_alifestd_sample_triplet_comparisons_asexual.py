c748c79a2b1bbe6830d55671ff7f89daf7a0df0c
Matthew Andres Moreno <mmore500.login+gpg@gmail.com>
2024-03-10 01:20:48 -0500

    Impl, test alifestd_sample_triplet_comparisons

--- hstrat/_auxiliary_lib/_alifestd_sample_triplet_comparisons_asexual.py
+++ hstrat/_auxiliary_lib/_alifestd_sample_triplet_comparisons_asexual.py
@@ -7,23 +7,24 @@ from ._alifestd_categorize_triplet_asexual import (
     alifestd_categorize_triplet_asexual,
 )
 from ._alifestd_find_leaf_ids import alifestd_find_leaf_ids
-from ._estimate_binomial_p import estimate_binomial_p
+from ._alifestd_find_mrca_id_asexual import alifestd_find_mrca_id_asexual
 
 
-def alifestd_estimate_triplet_distance_asexual(
+def alifestd_sample_triplet_comparisons_asexual(
     first_df: pd.DataFrame,
     second_df: pd.DataFrame,
     taxon_label_key: str,
-    confidence: float = 0.99,
-    precision: float = 0.01,
-    strict: typing.Union[bool, typing.Tuple[bool, bool]] = True,
-    detail: bool = False,
+    n: int = 1000,
     progress_wrap: typing.Callable = lambda x: x,
     mutate: bool = False,
-) -> typing.Union[float, typing.Tuple[float, typing.Tuple[float, float, int]]]:
-    """Estimate the triplet distance between two asexual phylogenetic trees in
-    alife sampling sets of three leaf taxa and counting the fraction whose
-    phylogenetic connectivity mismatch between trees.
+) -> pd.DataFrame:
+    """Sample triplet comparisons between two asexual phylogenetic trees in
+    alife standard form, creating a DataFrame with the triplet categorizations
+    and comparison results as well as corresponding data from MRCA row within
+    the first tree.
+
+    The MRCA row corresponds to the most recent common ancestor of two of the
+    three taxa in the triplet.
 
     Parameters
     ----------
@@ -33,24 +34,10 @@ def alifestd_estimate_triplet_distance_asexual(
         The DataFrame representing the second phylogenetic tree.
     taxon_label_key : str
         The key in the DataFrame to identify the taxon labels.
-    confidence : float, default 0.99
-        The confidence level for the estimation.
+    n : int, default 1000
+        The number of samples to take.
 
-        See `estimate_binomial_p` for details.
-    precision : float, default 0.01
-        The precision of the estimation.
-
-        See `estimate_binomial_p` for details.
-    strict : bool or Tuple[bool, bool], default True
-        A flag or a tuple of flags indicating how to treat tuples.
-
-        If False, triplets that form a polytomy in either tree are not counted
-        as mismatching. If True, they are counted as mismatching. If a tuple is
-        given, polytomies in the first and second trees are treated according
-        to the first and second elements of the tuple, respectively.
-    detail : bool, default False
-        If True, returns a detailed result including the estimated distance,
-        confidence interval, and sample size.
+        Corresponds to number of rows in the returned DataFrame.
     progress_wrap : typing.Callable, default lambda x: x
         A callable to display progress, e.g., tqdm.
     mutate : bool, default False
@@ -58,11 +45,12 @@ def alifestd_estimate_triplet_distance_asexual(
 
     Returns
     -------
-    float or Tuple[float, Tuple[float, float, int]]
-        The estimated distance between the two trees.
-
-        If `detail` is True, returns a tuple containing the estimated distance,
-        the confidence interval, and the sample size.
+    pd.DataFrame
+        A DataFrame with rows corresponding to sampled triplet comparisons and
+        the following columns:
+        - "triplet code, {first,second}": the categorization of the triplet in the first or second tree.
+        - "triplet match, {lax,lax/strict,strict,strict/lax}": whether the triplet categorizations match with differing treatment of polytomies.
+        - all columns from the first tree.
 
     Notes
     -----
@@ -74,17 +62,11 @@ def alifestd_estimate_triplet_distance_asexual(
     See Also
     --------
     alifestd_categorize_triplet_asexual
-    alifestd_sample_triplet_comparisons_asexual
+    alifestd_estimate_triplet_distance_asexual
     """
     if not mutate:
         first_df = first_df.copy()
         second_df = second_df.copy()
-    try:
-        strict1, strict2 = strict
-    except TypeError:
-        strict1 = strict2 = strict
-        assert strict1 == strict2 == strict
-    lax = (not bool(strict1), not bool(strict2))
 
     first_df = first_df.reset_index(drop=True).set_index("id", drop=False)
     second_df = second_df.reset_index(drop=True).set_index("id", drop=False)
@@ -102,10 +84,10 @@ def alifestd_estimate_triplet_distance_asexual(
     ).all()
 
     if len(first_leaf_ids) < 3:
-        return (0.0, [0.0, 0.0], 0) if detail else 0.0
+        raise ValueError("Too few leaves to sample any triplets.")
 
-    def sample_triplet_comparison() -> bool:
-        """Return True if the two categorizations differ."""
+    def sample_triplet_comparison() -> dict:
+        """Sample details of one triplet comparison between trees."""
         labels = np.random.choice(first_taxon_labels, 3, replace=False)
         first_triplet_ids = [
             first_df.index[first_df[taxon_label_key] == label].item()
@@ -124,20 +106,25 @@ def alifestd_estimate_triplet_distance_asexual(
             second_df, second_triplet_ids, mutate=True
         )
 
+        first_sampled_leaf_pair = np.random.choice(
+            first_leaf_ids, 2, replace=False
+        )
+        sampled_mrca_id = alifestd_find_mrca_id_asexual(
+            first_df, first_sampled_leaf_pair, mutate=True
+        )
         polytomy = -1
-        if cat1 == cat2:
-            return False
-        elif lax[0] and cat1 == polytomy:
-            return False
-        elif lax[1] and cat2 == polytomy:
-            return False
-        else:
-            return True
+        return {
+            "triplet code, first": cat1,
+            "triplet code, second": cat2,
+            "triplet match, lax": (
+                (cat1 == cat2) or (cat1 == polytomy) or (cat2 == polytomy)
+            ),
+            "triplet match, lax/strict": (cat1 == cat2) or (cat1 == polytomy),
+            "triplet match, strict": cat1 == cat2,
+            "triplet match, strict/lax": (cat1 == cat2) or (cat2 == polytomy),
+            **first_df.loc[sampled_mrca_id].to_dict(),
+        }
 
-    res = estimate_binomial_p(
-        sample_triplet_comparison,
-        confidence=confidence,
-        precision=precision,
-        progress_wrap=progress_wrap,
+    return pd.DataFrame.from_records(
+        [sample_triplet_comparison() for _ in progress_wrap(range(n))],
     )
-    return res if detail else res[0]
