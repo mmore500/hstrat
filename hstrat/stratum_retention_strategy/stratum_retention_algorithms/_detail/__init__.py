41f17638d07ac44f370b0b7e6319d589da048480
vivaansinghvi07 <vivaansinghvi8@gmail.com>
2024-10-27 20:05:50 -0400

    added lazy loading for stratum retention algorithms

--- hstrat/stratum_retention_strategy/stratum_retention_algorithms/_detail/__init__.py
+++ hstrat/stratum_retention_strategy/stratum_retention_algorithms/_detail/__init__.py
@@ -1,13 +1,3 @@
-from ._PolicyCouplerBase import PolicyCouplerBase
-from ._PolicyCouplerFactory import PolicyCouplerFactory
-from ._PolicySpecBase import PolicySpecBase
-from ._UnsatisfiableParameterizationRequestError import (
-    UnsatisfiableParameterizationRequestError,
-)
-
-__all__ = [
-    "PolicyCouplerBase",
-    "PolicyCouplerFactory",
-    "PolicySpecBase",
-    "UnsatisfiableParameterizationRequestError",
-]
+from ...._auxiliary_lib import lazy_attach_stub
+__getattr__, __dir__, __all__ = lazy_attach_stub(__name__, __file__, launder=False)
+del lazy_attach_stub
--- hstrat/stratum_retention_strategy/stratum_retention_algorithms/nominal_resolution_algo/__init__.py
+++ hstrat/stratum_retention_strategy/stratum_retention_algorithms/nominal_resolution_algo/__init__.py
@@ -12,57 +12,6 @@ space complexity) scales as O(1) with respect to the number of strata
 deposited.
 """
 
-from ._Policy import Policy
-from ._PolicySpec import PolicySpec
-from ._enact._GenDropRanks import GenDropRanks
-from ._enact._GenDropRanks_ import impls as _GenDropRanks_impls
-from ._invar._CalcMrcaUncertaintyAbsUpperBound import (
-    CalcMrcaUncertaintyAbsUpperBound,
-)
-from ._invar._CalcMrcaUncertaintyAbsUpperBoundAtPessimalRank import (
-    CalcMrcaUncertaintyAbsUpperBoundAtPessimalRank,
-)
-from ._invar._CalcMrcaUncertaintyAbsUpperBoundPessimalRank import (
-    CalcMrcaUncertaintyAbsUpperBoundPessimalRank,
-)
-from ._invar._CalcMrcaUncertaintyRelUpperBound import (
-    CalcMrcaUncertaintyRelUpperBound,
-)
-from ._invar._CalcMrcaUncertaintyRelUpperBoundAtPessimalRank import (
-    CalcMrcaUncertaintyRelUpperBoundAtPessimalRank,
-)
-from ._invar._CalcMrcaUncertaintyRelUpperBoundPessimalRank import (
-    CalcMrcaUncertaintyRelUpperBoundPessimalRank,
-)
-from ._invar._CalcNumStrataRetainedUpperBound import (
-    CalcNumStrataRetainedUpperBound,
-)
-from ._scry._CalcMrcaUncertaintyAbsExact import CalcMrcaUncertaintyAbsExact
-from ._scry._CalcMrcaUncertaintyRelExact import CalcMrcaUncertaintyRelExact
-from ._scry._CalcNumStrataRetainedExact import CalcNumStrataRetainedExact
-from ._scry._CalcRankAtColumnIndex import CalcRankAtColumnIndex
-from ._scry._IterRetainedRanks import IterRetainedRanks
-
-__all__ = [
-    "CalcMrcaUncertaintyAbsExact",
-    "CalcMrcaUncertaintyAbsUpperBound",
-    "CalcMrcaUncertaintyAbsUpperBoundAtPessimalRank",
-    "CalcMrcaUncertaintyAbsUpperBoundPessimalRank",
-    "CalcMrcaUncertaintyRelExact",
-    "CalcMrcaUncertaintyRelUpperBound",
-    "CalcMrcaUncertaintyRelUpperBoundAtPessimalRank",
-    "CalcMrcaUncertaintyRelUpperBoundPessimalRank",
-    "CalcNumStrataRetainedExact",
-    "CalcNumStrataRetainedUpperBound",
-    "CalcRankAtColumnIndex",
-    "GenDropRanks",
-    "_GenDropRanks_impls",
-    "IterRetainedRanks",
-    "Policy",
-    "PolicySpec",
-]
-
-from ...._auxiliary_lib import launder_impl_modules as _launder
-
-_launder([eval(item) for item in __all__], __name__)
-del _launder  # prevent name from leaking
+from ...._auxiliary_lib import lazy_attach_stub
+__getattr__, __dir__, __all__ = lazy_attach_stub(__name__, __file__, launder=True)
+del lazy_attach_stub
