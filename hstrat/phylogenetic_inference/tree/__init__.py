f5c8c59907a6415fadcdb344aad96c1f72c72c47
Matthew Andres Moreno <mmore500.login+gpg@gmail.com>
2024-10-31 10:30:44 -0400

    Draft tabular build-search implementation

--- hstrat/phylogenetic_inference/tree/__init__.py
+++ hstrat/phylogenetic_inference/tree/__init__.py
@@ -3,6 +3,7 @@
 from . import trie_postprocess
 from ._build_tree import build_tree
 from ._build_tree_nj import build_tree_nj
+from ._build_tree_searchtable import build_tree_searchtable
 from ._build_tree_trie import build_tree_trie
 from ._build_tree_trie_ensemble import build_tree_trie_ensemble
 from ._build_tree_upgma import build_tree_upgma
@@ -20,6 +21,7 @@ __all__ = [
     "build_tree_trie_ensemble",
     "build_tree_upgma",
     "build_tree",
+    "build_tree_searchtable",
     "trie_postprocess",
     "build_trie_from_artifacts_matrix",
     "build_trie_from_artifacts_progressive",
@@ -32,6 +34,7 @@ from ..._auxiliary_lib import launder_impl_modules as _launder
 _launder(
     [
         build_tree_nj,
+        build_tree_searchtable,
         build_tree_trie,
         build_tree_trie_ensemble,
         build_tree_upgma,
