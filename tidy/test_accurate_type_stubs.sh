7acfe990e9af6d19c0ebef670b46d12dbd5f3447
vivaansinghvi07 <vivaansinghvi8@gmail.com>
2024-10-31 17:47:04 -0400

    code review changes

--- tidy/test_accurate_type_stubs.sh
+++ tidy/test_accurate_type_stubs.sh
@@ -4,10 +4,12 @@ set -e
 
 cd "$(dirname "$0")/.."
 
-python3 -m venv .temp
+tmpdir="$(mktemp -d)"
+trap 'rm -rf -- "${tmpdir}"' EXIT
+python3 -m venv "${tmpdir}"
+
 source .temp/bin/activate
 python3 -m pip install uv wheel
 python3 -m uv pip install .
+
 python3 ./tidy/impl/test_accurate_type_stubs.py
-deactivate
-rm -rf .temp
