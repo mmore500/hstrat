bd67496fe858b72f2d5cd5d310f0b4d54c3b2281
Matthew Andres Moreno <mmore500.login+gpg@gmail.com>
2024-11-05 17:29:48 -0500

    Update __init__.py

--- hstrat/hstrat/__init__.py
+++ hstrat/hstrat/__init__.py
@@ -1,37 +1,30 @@
 """Convenient library repack as a flat namespace."""
 
-from .. import frozen_instrumentation as __frozen_instrumentation
-from .. import genome_instrumentation as __genome_instrumentation
-from .. import juxtaposition as __juxtaposition
-from .. import phylogenetic_inference as __phylogenetic_inference
-from .. import serialization as __serialization
-from .. import stratum_retention_strategy as __stratum_retention_strategy
-from .. import stratum_retention_viz as __stratum_retention_viz
-from .. import test_drive as __test_drive
-from .._auxiliary_lib import get_hstrat_version
-from ..frozen_instrumentation import __all__ as __frozen_instrumentation_all__
-from ..genome_instrumentation import __all__ as __genome_instrumentation_all__
-from ..juxtaposition import __all__ as __juxtaposition_all__
-from ..phylogenetic_inference import __all__ as __phylogenetic_inference_all__
-from ..serialization import __all__ as __serialization_all__
-from ..stratum_retention_strategy import (
-    __all__ as __stratum_retention_strategy_all__,
+from .. import frozen_instrumentation as _frozen_instrumentation
+from .. import genome_instrumentation as _genome_instrumentation
+from .. import juxtaposition as _juxtaposition
+from .. import phylogenetic_inference as _phylogenetic_inference
+from .. import serialization as _serialization
+from .. import stratum_retention_strategy as _stratum_retention_strategy
+from .. import stratum_retention_viz as _stratum_retention_viz
+from .. import test_drive as _test_drive
+from .._auxiliary_lib import get_hstrat_version as _get_hstrat_version
+
+_top_level_modules = (
+    _frozen_instrumentation,
+    _genome_instrumentation,
+    _juxtaposition,
+    _phylogenetic_inference,
+    _serialization,
+    _stratum_retention_strategy,
+    _stratum_retention_viz,
+    _test_drive,
 )
-from ..stratum_retention_viz import __all__ as __stratum_retention_viz_all__
-from ..test_drive import __all__ as __test_drive_all__
 
-__version__ = get_hstrat_version()
-del get_hstrat_version
+__version__ = _get_hstrat_version()
 
-__all__ = (
-    __frozen_instrumentation_all__
-    + __genome_instrumentation_all__
-    + __juxtaposition_all__
-    + __phylogenetic_inference_all__
-    + __serialization_all__
-    + __stratum_retention_strategy_all__
-    + __stratum_retention_viz_all__
-    + __test_drive_all__
+__all__ = tuple(
+    _item for _module in _top_level_modules for _item in _module.__all__
 )
 
 
@@ -47,16 +40,7 @@ def __getattr__(name: str) -> object:
     below packages. Now, the symbol is only imported if determined
     to be in said package.
     """
-    for mod, all__ in [
-        (__frozen_instrumentation, __frozen_instrumentation_all__),
-        (__genome_instrumentation, __genome_instrumentation_all__),
-        (__juxtaposition, __juxtaposition_all__),
-        (__phylogenetic_inference, __phylogenetic_inference_all__),
-        (__serialization, __serialization_all__),
-        (__stratum_retention_strategy, __stratum_retention_strategy_all__),
-        (__stratum_retention_viz, __stratum_retention_viz_all__),
-        (__test_drive, __test_drive_all__),
-    ]:
-        if name in all__:
-            return getattr(mod, name)
+    for _module in _top_level_modules:
+        if name in _module.__all__:
+            return getattr(_module, name)
     raise AttributeError(f"Flat namespace 'hstrat' has no attribute '{name}'")
