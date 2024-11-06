5ad15a6b732e7562aeb8127daf9803e693410114
Matthew Andres Moreno <mmore500.login+gpg@gmail.com>
2024-11-05 19:51:38 -0500

    Fix venv path in type stub script

--- tidy/test_type_stubs_up_to_date.sh
+++ tidy/test_type_stubs_up_to_date.sh
@@ -8,7 +8,7 @@ tmpdir="$(mktemp -d)"
 trap 'rm -rf -- "${tmpdir}"' EXIT
 python3 -m venv "${tmpdir}"
 
-source .temp/bin/activate
+source "${tmpdir}/bin/activate"
 python3 -m pip install uv wheel
 python3 -m uv pip install .
 
