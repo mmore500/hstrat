56e328153baaeeed43a760df6ffbce439cf33b6b
Matthew Andres Moreno <mmore500.login+gpg@gmail.com>
2024-11-13 10:27:44 -0500

    Add downstream, joinem, py3.12; drop py{3.8,3.9}

--- requirements-dev/py310/regenerate.sh
+++ requirements-dev/py310/regenerate.sh
@@ -5,19 +5,16 @@
 cd "${0%/*}"
 
 echo "generating requirements-all.txt"
-python3 -m uv pip compile --python-version=3.10 ../../pyproject.toml --extra pinned_dependencies_py310 --extra docs --extra pinned_jit --extra release --extra testing_py310 -o requirements-all.txt
+python3 -m uv pip compile --python-version=3.10 ../../pyproject.toml --extra docs --extra release --extra testing --extra pinned_jit -o requirements-all.txt
 
 echo "generating requirements-docs.txt"
-python3 -m uv pip compile --python-version=3.10 ../../pyproject.toml --extra pinned_dependencies_py310 --extra docs -o requirements-docs.txt
-
-echo "generating requirements-jit.txt"
-python3 -m uv pip compile --python-version=3.10 ../../pyproject.toml --extra pinned_dependencies_py310 --extra pinned_jit -o requirements-jit.txt
+python3 -m uv pip compile --python-version=3.10 ../../pyproject.toml --extra docs -o requirements-docs.txt
 
 echo "generating requirements-minimal.txt"
-python3 -m uv pip compile --python-version=3.10 ../../pyproject.toml --extra pinned_dependencies_py310 -o requirements-minimal.txt
+python3 -m uv pip compile --python-version=3.10 ../../pyproject.toml -o requirements-minimal.txt
 
 echo "generating requirements-release.txt"
-python3 -m uv pip compile --python-version=3.10 --allow-unsafe ../../pyproject.toml --extra pinned_dependencies_py310 --extra release -o requirements-release.txt
+python3 -m uv pip compile --python-version=3.10  --allow-unsafe ../../pyproject.toml --extra release -o requirements-release.txt
 
 echo "generating requirements-testing.txt"
-python3 -m uv pip compile --python-version=3.10 ../../pyproject.toml --extra pinned_dependencies_py310 --extra pinned_jit --extra testing_py310 -o requirements-testing.txt
+python3 -m uv pip compile --python-version=3.10 ../../pyproject.toml --extra testing --extra pinned_jit -o requirements-testing.txt
