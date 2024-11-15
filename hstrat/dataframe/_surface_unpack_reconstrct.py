530559c999fce6c5a68e9ba55802b612d5cd2aa8
Matthew Andres Moreno <mmore500.login+gpg@gmail.com>
2024-11-15 02:02:10 -0500

    Bump pins, including numpy and pandas

--- hstrat/dataframe/_surface_unpack_reconstrct.py
+++ hstrat/dataframe/_surface_unpack_reconstrct.py
@@ -1,3 +1,4 @@
+import logging
 import sys
 
 from downstream import dataframe as dstream_dataframe
@@ -14,37 +15,130 @@ from ..phylogenetic_inference.tree._build_tree_searchtable import (
 
 
 def surface_unpack_reconstruct(df: pl.DataFrame) -> pl.DataFrame:
-    """Unpack dstream buffer and counter from genome data  serialized into a single hexadecimal data field, TODO."""
+    """Unpack dstream buffer and counter from genome data and construct an
+    estimated phylogenetic tree for the genomes.
+
+    Parameters
+    ----------
+    df : pl.DataFrame
+        The input DataFrame containing packed data with required columns, one
+        row per dstream buffer.
+
+        Required schema:
+            - 'data_hex' : pl.String
+                - Raw binary data, with serialized dstream buffer and counter.
+                - Represented as a hexadecimal string.
+            - 'dstream_algo' : pl.Categorical
+                - Name of downstream curation algorithm used.
+                - e.g., 'dstream.steady_algo'
+            - 'dstream_storage_bitoffset' : pl.UInt64
+                - Position of dstream buffer field in 'data_hex'.
+            - 'dstream_storage_bitwidth' : pl.UInt64
+                - Size of dstream buffer field in 'data_hex'.
+            - 'dstream_T_bitoffset' : pl.UInt64
+                - Position of dstream counter field in 'data_hex'.
+            - 'dstream_T_bitwidth' : pl.UInt64
+                - Size of dstream counter field in 'data_hex'.
+            - 'dstream_S' : pl.Uint32
+                - Capacity of dstream buffer, in number of data items.
+
+        Optional schema:
+            - 'downstream_version' : pl.Categorical
+                - Version of downstream library used to curate data items.
+
+    Returns
+    -------
+    pl.DataFrame
+        The output DataFrame containing the estimated phylogenetic tree in
+        alife standard format, with the following columns:
+
+        - 'taxon_id' : pl.UInt64
+            - Unique identifier for each taxon.
+        - 'taxon_label' : pl.String
+            - Name of taxon.
+        - 'ancestor_id' : pl.UInt64
+            - Unique identifier for ancestor taxon.
+        - 'ancestor_list' : str
+            - List of ancestor taxon identifiers.
+        - 'origin_time' : pl.Float64
+            - Estimated origin time of phylogeny nodes.
+        - 'dstream_T_bitwidth' : pl.UInt64
+            - Size of annotation differentiae, in bits.
+        - 'dstream_data_id' : pl.UInt64
+            - Unique identifier for each data item.
+        - 'dstream_T' : pl.UInt64
+            - Num generations elapsed for tip taxon.
+        - 'dstream_Tbar' : pl.Float64
+            - Num generations elapsed for ancestral differentia.
+        - 'dstream_data_id' : pl.UInt64
+            - Unique identifier for each data item.
+
+        User-defined columns, except some prefixed with 'downstream_' or
+        'dstream_', will be forwarded from the input DataFrame.
+    """
+
+
+
+
+    num_rows = df.lazy().select(pl.len()).collect().item()
+    with pl.Config() as cfg:
+        cfg.set_tbl_cols(df.lazy().collect_schema().len())
+        head = repr(df.lazy().head().collect())
+        message = " ".join(["packed df:", str(num_rows), "rows\n", head])
+        logging.info(message)
 
     # for simplicity, return early for this special case
-    if len(df.lazy().limit(1).collect()) == 0:
+    if num_rows == 0:
         return pl.from_pandas(alifestd_make_empty())
 
     df = dstream_dataframe.unpack_data_packed(df)
+    with pl.Config() as cfg:
+        cfg.set_tbl_cols(df.lazy().collect_schema().len())
+        head = repr(df.lazy().head().collect())
+        message = " ".join(["unpacked df:", str(num_rows), "rows\n", head])
+        logging.info(message)
+
     long_df = dstream_dataframe.explode_lookup_unpacked(
         df, value_type="uint64"
     )
+    with pl.Config() as cfg:
+        cfg.set_tbl_cols(long_df.lazy().collect_schema().len())
+        head = repr(long_df.lazy().head().collect())
+        message = " ".join(["exploded df:", str(num_rows), "rows\n", head])
+        logging.info(message)
 
-    group_keys = [*map(pl.col, ("dstream_T", "data_id", "dstream_Tbar"))]
-    sorted_df = long_df.lazy().sort(group_keys)
-
+    logging.info("building tree...")
+    # TODO pass whole columns to c++ implementation
     records = [Record(taxon_label=sys.maxsize)]
-    for (dstream_T, data_id, _dstream_Tbar), group_df in tqdm(
-        sorted_df.collect().group_by(
-            group_keys,
-            maintain_order=True,
-        ),
-    ):
+    for frame in tqdm(long_df.iter_slices(64), total=len(long_df) // 64):
         insert_artifact(
             records,
-            # map int to make invariant to numpy input (?)
-            group_df["dstream_Tbar"].to_list(),
-            group_df["dstream_value"].to_list(),
-            data_id,  # TODO ensure data_id is sequential from downstream
-            dstream_T,
+            frame["dstream_Tbar"],
+            frame["dstream_value"],
+            frame["dstream_data_id"].first(),
+            frame["dstream_T"].first(),
         )
 
+    logging.info("finalizing tree...")
     phylo_df = finalize_records(records, force_common_ancestry=True)
-    phylo_df["data_id"] = phylo_df["data_id"].astype(np.uint64)
+    phylo_df["dstream_data_id"] = phylo_df["data_id"].astype(np.uint64)
+
+    logging.info("joining frames...")
     phylo_df = pl.from_pandas(phylo_df)
-    return phylo_df.join(df, on="data_id", how="left")
+    res = phylo_df.join(
+        df.select(pl.exclude("^dstream_*$", "^downstream_*$")).cast(
+            {"dstream_data_id": pl.UInt64},
+        ),
+        on="dstream_data_id",
+        how="left",
+    )
+
+    logging.info("surface_unpack_reconstruct complete")
+    with pl.Config() as cfg:
+        cfg.set_tbl_cols(phylo_df.lazy().collect_schema().len())
+        cfg.set_tbl_cols(phylo_df.width)
+        head = repr(phylo_df.lazy().head().collect())
+        message = " ".join(["reconst df:", str(num_rows), "rows\n", head])
+        logging.info(message)
+
+    return res
--- hstrat/dataframe/surface_unpack_reconstruct.py
+++ hstrat/dataframe/surface_unpack_reconstruct.py
@@ -1,9 +1,16 @@
+import logging
+
 from joinem import dataframe_cli
 
 from .._auxiliary_lib import get_hstrat_version
 from ._surface_unpack_reconstrct import surface_unpack_reconstruct
 
 if __name__ == "__main__":
+    logging.basicConfig(
+        datefmt="%Y-%m-%d %H:%M:%S",
+        format="%(asctime)s %(levelname)-8s %(message)s",
+        level=logging.INFO,
+    )
     dataframe_cli(
         description="Unpack dstream buffer and counter from genome data  serialized into a single hexadecimal data field, TODO.",
         module="hstrat.dataframe.surface_unpack_reconstruct",
