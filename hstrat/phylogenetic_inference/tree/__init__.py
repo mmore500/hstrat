82e8b1448c6e8a12492f6f011095b7e26e2de845
vivaansinghvi07 <vivaansinghvi8@gmail.com>
2024-11-18 15:06:08 -0500

    noqa on the cppimport import hook

--- hstrat/phylogenetic_inference/tree/__init__.py
+++ hstrat/phylogenetic_inference/tree/__init__.py
@@ -1,6 +1,6 @@
 """Functions to reconstruct a phylogenetic tree from extant hereditary strata."""
 
-from cppimport import import_hook  # @MAM seems necessary, why??
+from cppimport import import_hook  # noqa: F401 to the cppimport
 from . import trie_postprocess
 from ..._auxiliary_lib import lazy_attach
 
