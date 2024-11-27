a9e8cf55fe21b9aa37a1ae399867186c69a2ea80
vivaansinghvi07 <vivaansinghvi8@gmail.com>
2024-11-27 01:08:05 -0500

    testing build system using pybind11

--- setup.py
+++ setup.py
@@ -5,6 +5,17 @@
 # shim for compatibility with setuptools build workflow
 # adapted from https://stackoverflow.com/a/58754298
 
+from pybind11.setup_helpers import Pybind11Extension, build_ext
+
 from setuptools import setup
 
-setup()
+setup(
+    ext_modules=[
+        Pybind11Extension(
+            "hstrat.phylogenetic_inference.tree.build_tree_searchtable_cpp",
+            ["hstrat/phylogenetic_inference/tree/build_tree_searchtable_cpp.cpp"],
+            cxx_std=23
+        ),
+    ],
+    cmdclass={"build_ext": build_ext},
+)
