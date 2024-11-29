3f2e91b2b619ae1b868decb10bde6f1d81234901
Matthew Andres Moreno <mmore500.login+gpg@gmail.com>
2024-11-29 16:30:40 -0500

    Remove redundant setup.py pybind11 plugins

--- setup.py
+++ setup.py
@@ -5,18 +5,6 @@
 # shim for compatibility with setuptools build workflow
 # adapted from https://stackoverflow.com/a/58754298
 
-from pybind11.setup_helpers import Pybind11Extension, build_ext
 from setuptools import setup
 
-setup(
-    ext_modules=[
-        Pybind11Extension(
-            "hstrat.phylogenetic_inference.tree._build_tree_searchtable_cpp",
-            [
-                "hstrat/phylogenetic_inference/tree/_build_tree_searchtable_cpp.cpp"
-            ],
-            cxx_std=20,
-        ),
-    ],
-    cmdclass={"build_ext": build_ext},
-)
+setup()
