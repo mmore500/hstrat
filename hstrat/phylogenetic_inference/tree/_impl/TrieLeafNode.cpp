e5bd3c9692c9851ce2882fddb0aa750f95d56460
Matthew Andres Moreno <mmore500.login+gpg@gmail.com>
2024-11-29 13:45:04 -0500

    fixup! Use std::format instead of sstream for windows compat

--- hstrat/phylogenetic_inference/tree/_impl/TrieLeafNode.cpp
+++ hstrat/phylogenetic_inference/tree/_impl/TrieLeafNode.cpp
@@ -1,10 +1,16 @@
-#include <cstdlib>
+#include <iostream>
 #include <format>
+#include <random>
 
 #include "TrieLeafNode.hpp"
 
 TrieLeafNode::TrieLeafNode() {
-  this->_taxon_label = std::format("{}", std::random());
+  std::mt19937 gen(std::random_device{}());
+  std::uniform_int_distribution<uint32_t> dis(0, 0xFFFFFFFF);
+
+  this->_taxon_label = std::format(
+    "{:08x}{:08x}{:08x}{:08x}", dis(gen), dis(gen), dis(gen), dis(gen)
+  );
 }
 
 TrieLeafNode::TrieLeafNode(std::string taxon_label) : _taxon_label(taxon_label) {}
