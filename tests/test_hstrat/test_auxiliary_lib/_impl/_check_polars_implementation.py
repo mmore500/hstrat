37838fcdde82d7efe39cf5c8b9c354c27b8d968b
vivaansinghvi07 <vivaansinghvi8@gmail.com>
2024-11-30 14:26:46 -0500

    use coerce to pandas function

--- tests/test_hstrat/test_auxiliary_lib/_impl/_check_polars_implementation.py
+++ tests/test_hstrat/test_auxiliary_lib/_impl/_check_polars_implementation.py
@@ -4,18 +4,16 @@ import typing
 import pandas as pd
 import polars as pl
 
-
-def _convert(arg: typing.Any) -> typing.Any:
-    if isinstance(arg, (pd.Series, pd.DataFrame)):
-        return pl.from_pandas(arg)
-    return arg
+from hstrat._auxiliary_lib._delegate_polars_implementation import (
+    _coerce_to_pandas,
+)
 
 
 def check_polars_implementation(func: typing.Callable):
     @functools.wraps(func)
     def polars_equality_checker(*args, **kwargs):
-        pl_args = tuple(_convert(arg) for arg in args)
-        pl_kwargs = {kw: _convert(arg) for kw, arg in kwargs.items()}
+        pl_args = tuple(_coerce_to_pandas(arg) for arg in args)
+        pl_kwargs = {kw: _coerce_to_pandas(arg) for kw, arg in kwargs.items()}
 
         pd_result = func(*args, **kwargs)
         pl_result = func(*pl_args, **pl_kwargs)
