c029dd576d3ce0a941e775f5d66b7c3190eaa2d6
Matthew Andres Moreno <mmore500.login+gpg@gmail.com>
2024-12-08 21:38:12 -0500

    Copy files into place in build.sh

--- build.sh
+++ build.sh
@@ -2,5 +2,17 @@
 
 set -e
 
+shopt -s nullglob
+
+cd "$(dirname "$0")"
+
 cmake .
 make
+
+for object in *.so; do
+    filename="${object%%.*}"
+    sourcename="$(find hstrat -name "${filename}.cpp" -type f)"
+    where="$(dirname "${sourcename}")"
+    echo "Copying ${object} to ${where}"
+    cp "${object}" "${where}"
+done
