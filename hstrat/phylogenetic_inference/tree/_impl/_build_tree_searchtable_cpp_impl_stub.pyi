91633250671c184b8917a658857962a110339bd3
vivaansinghvi07 <vivaansinghvi8@gmail.com>
2024-12-10 13:28:44 -0500

    changed optional to using a mock object:

--- hstrat/phylogenetic_inference/tree/_impl/_build_tree_searchtable_cpp_impl_stub.pyi
+++ hstrat/phylogenetic_inference/tree/_impl/_build_tree_searchtable_cpp_impl_stub.pyi
@@ -1,4 +1,5 @@
 import typing
+from unittest import mock
 
 import numpy as np
 import tqdm
@@ -8,12 +9,12 @@ def build_tree_searchtable_cpp_from_exploded(
     num_strata_depositeds: np.ndarray,
     ranks: np.ndarray,
     differentiae: np.ndarray,
-    tqdm_progress_bar: typing.Optional[typing.Type[tqdm.tqdm]] = None,
+    tqdm_progress_bar: typing.Union[typing.Type[tqdm.tqdm], mock.Mock],
 ) -> dict[str, np.ndarray]: ...
 def build_tree_searchtable_cpp_from_nested(
     data_ids: typing.List[int],
     num_strata_depositeds: typing.List[int],
     ranks: typing.List[typing.List[int]],
     differentiae: typing.List[typing.List[int]],
-    tqdm_progress_bar: typing.Optional[typing.Type[tqdm.tqdm]] = None,
+    tqdm_progress_bar: typing.Union[typing.Type[tqdm.tqdm], mock.Mock],
 ) -> dict[str, np.ndarray]: ...
