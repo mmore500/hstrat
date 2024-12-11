7cbc796877d2283378679b440e9f50fa5b8d593b
vivaansinghvi07 <vivaansinghvi8@gmail.com>
2024-12-10 21:15:51 -0500

    enforce black

--- tests/test_hstrat/test_auxiliary_lib/_impl/_check_polars_implementation.py
+++ tests/test_hstrat/test_auxiliary_lib/_impl/_check_polars_implementation.py
@@ -7,7 +7,9 @@ import polars as pl
 from hstrat._auxiliary_lib import coerce_to_pandas, coerce_to_polars
 
 
-def check_polars_implementation(func: typing.Callable, recurse_type_checks: bool = False):
+def check_polars_implementation(
+    func: typing.Callable, recurse_type_checks: bool = False
+):
     """
     Takes in `func`, which was supposed to have been wrapped with
     `_auxiliary_lib.delegate_polars_implementation`, asserts that
@@ -17,16 +19,25 @@ def check_polars_implementation(func: typing.Callable, recurse_type_checks: bool
 
     @functools.wraps(func)
     def polars_equality_checker(*args, **kwargs):
-        pl_args = tuple(coerce_to_polars(arg, recurse=recurse_type_checks) for arg in args)
-        pl_kwargs = {kw: coerce_to_polars(arg, recurse=recurse_type_checks) for kw, arg in kwargs.items()}
+        pl_args = tuple(
+            coerce_to_polars(arg, recurse=recurse_type_checks) for arg in args
+        )
+        pl_kwargs = {
+            kw: coerce_to_polars(arg, recurse=recurse_type_checks)
+            for kw, arg in kwargs.items()
+        }
 
         pd_result = func(*args, **kwargs)
         pl_result = func(*pl_args, **pl_kwargs)
         if isinstance(pl_result, pl.DataFrame):
-            pl_result = coerce_to_pandas(pl_result, recurse=recurse_type_checks)
+            pl_result = coerce_to_pandas(
+                pl_result, recurse=recurse_type_checks
+            )
             pd.testing.assert_frame_equal(pl_result, pd_result)
         elif isinstance(pl_result, pl.Series):
-            pl_result = coerce_to_pandas(pl_result, recurse=recurse_type_checks)
+            pl_result = coerce_to_pandas(
+                pl_result, recurse=recurse_type_checks
+            )
             pd.testing.assert_series_equal(
                 pl_result, pd_result, check_names=False
             )
