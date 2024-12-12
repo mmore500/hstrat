b65216d8323c7bb5c6e10c8418111cbb9572a302
Matthew Andres Moreno <mmore500.login+gpg@gmail.com>
2024-12-12 12:50:35 -0500

    Flatten branching

--- hstrat/_auxiliary_lib/_coerce_to_pandas.py
+++ hstrat/_auxiliary_lib/_coerce_to_pandas.py
@@ -10,22 +10,20 @@ def coerce_to_pandas(obj: typing.Any, *, recurse: bool = False) -> typing.Any:
     """
     If a Polars type is detected, coerce it to corresponding Pandas type.
     """
-    if recurse:
-        if isinstance(obj, _supported_iterables):
-            return type(obj)(
-                map(lambda x: coerce_to_pandas(x, recurse=recurse), obj)
-            )
-        elif isinstance(obj, _supported_mappings):  # includes defaultdict etc
-            return type(obj)(
-                {
-                    k: coerce_to_pandas(v, recurse=recurse)
-                    for k, v in obj.items()
-                }
-            )
-
     if hasattr(obj, "__dataframe__"):
         return pd.api.interchange.from_dataframe(obj, allow_copy=True)
     elif hasattr(obj, "to_pandas"):
         return obj.to_pandas()  # pyarrow is required for this operation
+    elif recurse and isinstance(obj, _supported_iterables):
+        return type(obj)(
+            map(lambda x: coerce_to_pandas(x, recurse=recurse), obj)
+        )
+    elif recurse and isinstance(obj, _supported_mappings):  # includes defaultdict etc
+        return type(obj)(
+            {
+                k: coerce_to_pandas(v, recurse=recurse)
+                for k, v in obj.items()
+            },
+        )
     else:
         return obj
