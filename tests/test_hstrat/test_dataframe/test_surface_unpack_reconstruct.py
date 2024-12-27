18ec134942186c65a9645b9f3550babbb80f329b
Matthew Andres Moreno <mmore500.login+gpg@gmail.com>
2024-12-27 06:31:24 -0500

    Beef up smoke test

--- tests/test_hstrat/test_dataframe/test_surface_unpack_reconstruct.py
+++ tests/test_hstrat/test_dataframe/test_surface_unpack_reconstruct.py
@@ -2,11 +2,19 @@ import os
 
 import polars as pl
 
+from hstrat._auxiliary_lib import (
+    alifestd_validate, alifestd_try_add_ancestor_list_col
+)
 from hstrat.dataframe import surface_unpack_reconstruct
+
 
 assets_path = os.path.join(os.path.dirname(__file__), "assets")
 
 
 def test_smoke():
     df = pl.read_csv(f"{assets_path}/packed.csv")
-    surface_unpack_reconstruct(df)
+    res = surface_unpack_reconstruct(df)
+    assert len(res) > len(df)
+    assert alifestd_validate(
+        alifestd_try_add_ancestor_list_col(res.to_pandas()),
+    )
