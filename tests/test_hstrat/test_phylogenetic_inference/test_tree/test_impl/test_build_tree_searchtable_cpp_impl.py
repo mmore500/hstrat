d8f3b3ba094149c9ce5771f27896e44129e32fbb
Matthew Andres Moreno <mmore500.login+gpg@gmail.com>
2025-01-01 08:14:45 -0500

    Add distilled version of regression case

--- tests/test_hstrat/test_phylogenetic_inference/test_tree/test_impl/test_build_tree_searchtable_cpp_impl.py
+++ tests/test_hstrat/test_phylogenetic_inference/test_tree/test_impl/test_build_tree_searchtable_cpp_impl.py
@@ -11,7 +11,7 @@ from hstrat.phylogenetic_inference.tree._impl._build_tree_searchtable_cpp_impl_s
 )
 
 
-def test_regression():
+def test_regression_original():
     exploded = pl.DataFrame(
         {
             "dstream_data_id": pl.Series(
@@ -37,6 +37,54 @@ def test_regression():
             "dstream_Tbar": pl.Series(
                 "dstream_Tbar",
                 [0, 1, 3, 7, 2, 5, 4, 6, 0, 1, 3, 7, 2, 5, 9, 6],
+                dtype=pl.UInt64,
+            ),
+        },
+    )
+    res = build_tree_searchtable_cpp_from_exploded(
+        exploded["dstream_data_id"].to_numpy(),
+        exploded["dstream_T"].to_numpy(),
+        exploded["dstream_Tbar"].to_numpy(),
+        exploded["dstream_value"].to_numpy(),
+        tqdm,
+    )
+    res = pl.DataFrame(res)
+    assert alifestd_validate(
+        alifestd_try_add_ancestor_list_col(res.to_pandas()),
+    )
+    assert alifestd_is_chronologically_ordered(
+        alifestd_try_add_ancestor_list_col(
+            res.with_columns(origin_time=pl.col("rank")).to_pandas(),
+        ),
+    )
+
+
+def test_regression_distilled():
+    exploded = pl.DataFrame(
+        {
+            "dstream_data_id": pl.Series(
+                "dstream_data_id",
+                [0, 0, 1],
+                dtype=pl.UInt64,
+            ),
+            "dstream_T": pl.Series(
+                "dstream_T",
+                [8, 8, 11],
+                dtype=pl.UInt64,
+            ),
+            "dstream_value_bitwidth": pl.Series(
+                "dstream_value_bitwidth",
+                [8, 8, 8,],
+                dtype=pl.UInt32,
+            ),
+            "dstream_value": pl.Series(
+                "dstream_value",
+                [7, 6, 6],
+                dtype=pl.UInt64,
+            ),
+            "dstream_Tbar": pl.Series(
+                "dstream_Tbar",
+                [7, 6, 6],
                 dtype=pl.UInt64,
             ),
         },
