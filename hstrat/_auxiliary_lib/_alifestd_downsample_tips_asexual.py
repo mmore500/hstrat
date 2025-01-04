a4cfd1c3d2541e440aa3fe61c4b03419645d1b36
Matthew Andres Moreno <mmore500.login+gpg@gmail.com>
2025-01-04 10:19:08 -0500

    Draft downsample tips CLI

--- hstrat/_auxiliary_lib/_alifestd_downsample_tips_asexual.py
+++ hstrat/_auxiliary_lib/_alifestd_downsample_tips_asexual.py
@@ -1,6 +1,10 @@
+import argparse
+import functools
+import logging
 import random
 import typing
 
+from joinem._dataframe_cli import _add_parser_base, _run_dataframe_cli
 import pandas as pd
 
 from ._alifestd_find_leaf_ids import alifestd_find_leaf_ids
@@ -8,6 +12,11 @@ from ._alifestd_prune_extinct_lineages_asexual import (
     alifestd_prune_extinct_lineages_asexual,
 )
 from ._alifestd_try_add_ancestor_id_col import alifestd_try_add_ancestor_id_col
+from ._configure_prod_logging import configure_prod_logging
+from ._delegate_polars_implementation import delegate_polars_implementation
+from ._format_cli_description import format_cli_description
+from ._get_hstrat_version import get_hstrat_version
+from ._log_context_duration import log_context_duration
 from ._with_rng_state_context import with_rng_state_context
 
 
@@ -56,3 +65,68 @@ def alifestd_downsample_tips_asexual(
     )
 
     return impl(phylogeny_df, n_downsample)
+
+
+_raw_description = """Create a subsample phylogeny containing `num_tips` tips.
+
+If `num_tips` is greater than the number of tips in the phylogeny, the whole phylogeny is returned.
+
+Data is assumed to be in alife standard format.
+Only supports asexual phylogenies.
+
+Additional Notes
+================
+- Requires 'ancestor_id' column to be present in input DataFrame.
+Otherwise, no action is taken.
+
+- Use `--eager-read` if modifying data file inplace.
+
+- This CLI entrypoint is experimental and may be subject to change.
+"""
+
+
+def _create_parser() -> argparse.ArgumentParser:
+    parser = argparse.ArgumentParser(
+        add_help=False,
+        description=format_cli_description(_raw_description),
+        formatter_class=argparse.RawTextHelpFormatter,
+    )
+    parser = _add_parser_base(
+        parser=parser,
+        dfcli_module="hstrat._auxiliary_lib._alifestd_downsample_tips_asexual",
+        dfcli_version=get_hstrat_version(),
+    )
+    parser.add_argument(
+        "-n",
+        type=int,
+        help="Number of tips to subsample.",
+    )
+    parser.add_argument(
+        "--seed",
+        default=None,
+        dest="seed",
+        help="Integer seed for deterministic behavior.",
+        type=int,
+    )
+    return parser
+
+
+if __name__ == "__main__":
+    configure_prod_logging()
+
+    parser = _create_parser()
+    args, __ = parser.parse_known_args()
+    with log_context_duration(
+        "hstrat._auxiliary_lib._alifestd_downsample_tips_asexual", logging.info
+    ):
+        _run_dataframe_cli(
+            allow_overridden_arguments=True,  # seed is overridden
+            base_parser=parser,
+            output_dataframe_op=delegate_polars_implementation()(
+                functools.partial(
+                    alifestd_downsample_tips_asexual,
+                    n_downsample=args.n,
+                    seed=args.seed,
+                ),
+            ),
+        )
