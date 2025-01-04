78ae9db2e35f9a418983e60091db8a8a593f03c1
Matthew Andres Moreno <mmore500.login+gpg@gmail.com>
2025-01-04 10:34:36 -0500

    Update CLI command listing

--- hstrat/__main__.py
+++ hstrat/__main__.py
@@ -4,11 +4,15 @@ if __name__ == "__main__":
     print(f"hstrat v{get_hstrat_version()}")
     print()
     print("Available commands (stabilized API):")
+    print("$ python3 -m hstrat.dataframe.surface_build_tree")
     print("$ python3 -m hstrat.dataframe.surface_unpack_reconstruct")
     print("$ python3 -m hstrat.dataframe.surface_postprocess_trie")
     print()
     print("Available commands (experimental API):")
     print("$ python3 -m hstrat._auxiliary_lib._alifestd_as_newick_asexual")
+    print(
+        "$ python3 -m hstrat._auxiliary_lib._alifestd_downsample_tips_asexual"
+    )
     print(
         "$ python3 -m hstrat._auxiliary_lib._alifestd_try_add_ancestor_list_col"
     )
