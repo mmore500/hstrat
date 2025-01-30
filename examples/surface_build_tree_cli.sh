0b667fa558a5bb472753a9367e5cd11b21948d3b
Matthew Andres Moreno <mmore500.login+gpg@gmail.com>
2025-01-29 22:39:25 -0500

    fixup! Add regression test for collapse bug

--- examples/surface_build_tree_cli.sh
+++ examples/surface_build_tree_cli.sh
@@ -2,11 +2,14 @@
 
 set -euo pipefail
 
+# get example genome data
 wget -O /tmp/genomes.pqt https://osf.io/gnkbc/download
 
+# unpack and reconstruct
 ls -1 /tmp/genomes.pqt \
     | python3 -O -m hstrat.dataframe.surface_build_tree /tmp/reconstructed.csv
 
+# convert from alifestd to newick
 python3 -O -m hstrat._auxiliary_lib._alifestd_as_newick_asexual \
     -i /tmp/reconstructed.csv \
     -o /tmp/reconstructed.newick
