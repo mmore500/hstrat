2cd0f23b7bfbde5416d75150142d9dd50e32b613
Matthew Andres Moreno <mmore500.login+gpg@gmail.com>
2025-01-31 13:57:08 -0500

    Fix bash syntax error

--- tests-cli/test_dataframe_surface_build_tree_iso.sh
+++ tests-cli/test_dataframe_surface_build_tree_iso.sh
@@ -51,7 +51,7 @@ for opt in \
     python3 -m hstrat._auxiliary_lib._alifestd_test_leaves_isomorphic_asexual \
         --taxon-label "dstream_data_id" \
         "${reference}" "${alternate}" \
-        > ${HSTRAT_TESTS_CLI_STDOUT} 2>&1
+        > ${HSTRAT_TESTS_CLI_STDOUT} 2>&1 \
         && echo "PASS $0"
 
 done
