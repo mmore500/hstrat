91ca52c6f8d778635583105b8bf19d6d276abeb6
Matthew Andres Moreno <mmore500.login+gpg@gmail.com>
2025-01-31 13:08:28 -0500

    Run tests to completion, with explicit report

--- tests-cli/run_all.sh
+++ tests-cli/run_all.sh
@@ -6,18 +6,23 @@ set -e
 # adapted from https://stackoverflow.com/a/6393573
 cd "${0%/*}"
 
+EXIT_CODE=0
+
 # adapted from https://unix.stackexchange.com/a/556639
 GLOBIGNORE='_*';
 # adapted from https://stackoverflow.com/a/34195247/17332200
 if compgen -G "*.py"; then
 for example in *.py; do
   echo "running example ${example}"
-  python3 "${example}"
+  python3 "${example}" || EXIT_CODE=1
 done
 fi
 
 GLOBIGNORE="$(basename "$0")";
 for example in *.sh; do
   echo "running example ${example}"
-  bash "${example}"
+  bash "${example}" || EXIT_CODE=1
 done
+
+echo "exiting with exit code ${EXIT_CODE}"
+exit ${EXIT_CODE}
