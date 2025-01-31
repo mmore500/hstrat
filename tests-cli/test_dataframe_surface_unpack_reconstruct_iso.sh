f3478b81cf969c6d39287133241bbe7b0af18f71
Matthew Andres Moreno <mmore500.login+gpg@gmail.com>
2025-01-30 22:35:24 -0500

    Draft cli tests for isomorphism

---
+++ tests-cli/test_dataframe_surface_unpack_reconstruct_iso.sh
@@ -0,0 +1,55 @@
+#!/bin/bash
+
+set -euo pipefail
+
+cd "$(dirname "${BASH_SOURCE[0]}")/.."
+
+HSTRAT_TESTS_CLI_STDOUT="${HSTRAT_TESTS_CLI_STDOUT:-/dev/null}"
+
+genomes="$(mktemp).pqt"
+reference="$(mktemp).pqt"
+alternate="$(mktemp).pqt"
+
+function cleanup {
+    rm -f "${genomes}"
+    rm -f "${reference}"
+    rm -f "${alternate}"
+}
+trap cleanup EXIT
+
+err() {
+    echo "FAIL line $(caller)" >&2
+}
+trap err ERR
+
+# get example genome data
+wget -O "${genomes}" https://osf.io/gnkbc/download \
+    > ${HSTRAT_TESTS_CLI_STDOUT} 2>&1
+
+# unpack and reconstruct reference
+ls -1 "${genomes}" \
+    | python3 -O -m hstrat.dataframe.surface_unpack_reconstruct "${reference}" \
+    ${HSTRAT_TESTS_CLI_HEAD:-} --collapse-unif-freq=0 \
+    > ${HSTRAT_TESTS_CLI_STDOUT} 2>&1
+
+for opt in \
+    "--exploded-slice-size=4_000_000 --collapse-unif-freq=0" \
+    "--exploded-slice-size=1_000 --collapse-unif-freq=0" \
+    "" \
+    "--exploded-slice-size=1_000_000" \
+    "--exploded-slice-size=4_000_000" \
+; do
+    echo "opt=${opt}"
+
+    # unpack and reconstruct alternate
+    ls -1 "${genomes}" \
+        | python3 -O -m hstrat.dataframe.surface_unpack_reconstruct "${alternate}" \
+        ${HSTRAT_TESTS_CLI_HEAD:-} ${opt} \
+        > ${HSTRAT_TESTS_CLI_STDOUT} 2>&1
+
+    python3 -m hstrat._auxiliary_lib._alifestd_test_leaves_isomorphic_asexual \
+        --taxon-label "dstream_data_id" \
+        "${reference}" "${alternate}" \
+        && echo "PASS $0"
+
+done
