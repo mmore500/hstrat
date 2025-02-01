93fe503bd437de299b7ebf20eb07bcc09f0434fb
Matthew Andres Moreno <mmore500.login+gpg@gmail.com>
2025-01-31 21:51:43 -0500

    Add support for ancestor_id col to count_roots

--- hstrat/_auxiliary_lib/_alifestd_count_root_nodes.py
+++ hstrat/_auxiliary_lib/_alifestd_count_root_nodes.py
@@ -4,10 +4,13 @@ import pandas as pd
 
 def alifestd_count_root_nodes(phylogeny_df: pd.DataFrame) -> np.ndarray:  # int
     """How many root nodes are contained in phylogeny?"""
-    return (
-        phylogeny_df["ancestor_list"]
-        .astype(str)
-        .str.lower()
-        .isin(("[none]", "[]"))
-        .sum()
-    )
+    if "ancestor_id" in phylogeny_df.columns:
+        return (phylogeny_df["ancestor_id"] == phylogeny_df["id"]).sum()
+    else:
+        return (
+            phylogeny_df["ancestor_list"]
+            .astype(str)
+            .str.lower()
+            .isin(("[none]", "[]"))
+            .sum()
+        )
