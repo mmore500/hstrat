6b9da0ff7f896195542a0d53919fa2a7a6f4dfa2
vivaansinghvi07 <singhvi.vivaan@gmail.com>
2025-02-04 15:29:51 -0500

    removed unused visualization option

--- examples/end2end_tree_reconstruction_with_dstream_surf.sh
+++ examples/end2end_tree_reconstruction_with_dstream_surf.sh
@@ -9,18 +9,11 @@ if [ "${has_cppimport}" -eq 0 ]; then
     exit 1
 fi
 
-vis=1
-if [ "${1:-}" = "--skip-visualization" ]; then
-  shift 1
-  vis=0
-fi
-
 cd "$(dirname "$0")"
 
 genome_df_path="/tmp/end2end-raw-genome-evolve_surf_dstream.pqt"
 true_phylo_df_path="/tmp/end2end-true-phylo-evolve_surf_dstream.csv"
 reconst_phylo_df_path="/tmp/end2end-reconst-phylo-evolve_surf_dstream.pqt"
-img_path="/tmp/end2end-visualized-phylogenies-evolve_surf_dstream.png"
 
 # generate data
 ./evolve_dstream_surf.py \
@@ -39,6 +32,3 @@ ls "${genome_df_path}" | python3 -m \
 echo "genome_df_path = '${genome_df_path}'"
 echo "true_phylo_df_path = '${true_phylo_df_path}'"
 echo "reconst_phylo_df_path = '${reconst_phylo_df_path}'"
-if [ $vis -eq 1 ]; then
-  echo "visualized_phylogenies_path = '${img_path}'"
-fi
