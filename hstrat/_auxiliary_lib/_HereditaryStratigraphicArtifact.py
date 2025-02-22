983175c6183951cdbb93155b0820d55ac31eb92d
vivaansinghvi07 <singhvi.vivaan@gmail.com>
2025-02-22 13:31:11 -0500

    added surface to artifacts

--- hstrat/_auxiliary_lib/_HereditaryStratigraphicArtifact.py
+++ hstrat/_auxiliary_lib/_HereditaryStratigraphicArtifact.py
@@ -3,5 +3,6 @@ import typing
 # use string alias due to circular module initialization issue
 HereditaryStratigraphicArtifact = typing.Union[
     "HereditaryStratigraphicColumn",  # noqa: F821
+    "HereditaryStratigraphicSurface",  # noqa: F821
     "HereditaryStratigraphicSpecimen",  # noqa: F821
 ]
