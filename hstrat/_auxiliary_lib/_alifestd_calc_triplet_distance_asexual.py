2271c9bc250ef0571bbf1cb3b542c7970a9c8e56
vivaansinghvi07 <singhvi.vivaan@gmail.com>
2025-03-25 17:52:38 -0400

    added tests for triplet distance

--- hstrat/_auxiliary_lib/_alifestd_calc_triplet_distance_asexual.py
+++ hstrat/_auxiliary_lib/_alifestd_calc_triplet_distance_asexual.py
@@ -11,7 +11,9 @@ from . import (
 
 # adapted from https://github.com/mmore500/hstrat/blob/d23917cf03ba59061ff2f9b951efe79e995eb4d8/tests/test_hstrat/test_phylogenetic_inference/test_tree/_impl/_tree_quartet_distance.py
 def alifestd_calc_triplet_distance_asexual(
-    ref: pd.DataFrame, cmp: pd.DataFrame, taxon_label_key: str = "taxon_label",
+    ref: pd.DataFrame,
+    cmp: pd.DataFrame,
+    taxon_label_key: str = "taxon_label",
 ) -> float:
     """Calculate the triplet distance between two trees."""
 
@@ -27,7 +29,7 @@ def alifestd_calc_triplet_distance_asexual(
     if ref_labels != cmp_labels:
         raise ValueError("Taxon labels must match between trees")
     for taxon_label in ref_labels:
-        if not taxon_label.strip():
+        if isinstance(taxon_label, str) and not taxon_label.strip():
             raise ValueError("Cannot have empty taxon labels")
 
     if (
@@ -38,11 +40,15 @@ def alifestd_calc_triplet_distance_asexual(
             f"Cannot have disjunct trees in `{alifestd_calc_triplet_distance_asexual.__name__}`"
         )
 
-    return tqdist.triplet_distance(
+    ref_newick = (
         alifestd_as_newick_asexual(ref, taxon_label=taxon_label_key)
         .removeprefix("[&R]")
-        .strip(),
+        .strip()
+    )
+    cmp_newick = (
         alifestd_as_newick_asexual(cmp, taxon_label=taxon_label_key)
         .removeprefix("[&R]")
-        .strip(),
+        .strip()
     )
+    print(ref_newick, cmp_newick)
+    return tqdist.triplet_distance(ref_newick, cmp_newick)
