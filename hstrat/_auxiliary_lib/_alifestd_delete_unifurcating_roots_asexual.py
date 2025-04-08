9d21a8326201f8f2931c22bc76cab0eb4ded6172
Matthew Andres Moreno <mmore500.login+gpg@gmail.com>
2025-04-08 17:46:43 -0400

    Clean up perf and docstrings for delete unifurcating roots

--- hstrat/_auxiliary_lib/_alifestd_delete_unifurcating_roots_asexual.py
+++ hstrat/_auxiliary_lib/_alifestd_delete_unifurcating_roots_asexual.py
@@ -1,5 +1,7 @@
 import pandas as pd
 
+from ._alifestd_has_contiguous_ids import alifestd_has_contiguous_ids
+from ._alifestd_is_asexual import alifestd_is_asexual
 from ._alifestd_make_ancestor_list_col import alifestd_make_ancestor_list_col
 from ._alifestd_mark_num_children_asexual import (
     alifestd_mark_num_children_asexual,
@@ -15,8 +17,9 @@ def alifestd_delete_unifurcating_roots_asexual(
 ) -> pd.DataFrame:
     """Pare record to bypass root nodes with only one descendant.
 
-    May leave a root unifurcation present. See
-    `alifestd_collapse_root_unifurcations_asexual`.
+    Dataframe reindexing (e.g., df.index) may be applied.
+
+    See also `alifestd_collapse_unifurcations`.
 
     The option `root_ancestor_token` will be sandwiched in brackets to create
     the ancestor list entry for genesis organisms. For example, the token
@@ -31,24 +34,33 @@ def alifestd_delete_unifurcating_roots_asexual(
     if not mutate:
         phylogeny_df = phylogeny_df.copy()
 
-    phylogeny_df = alifestd_mark_num_children_asexual(
-        phylogeny_df, mutate=True
-    )
-    phylogeny_df = alifestd_mark_roots(phylogeny_df, mutate=True)
+    if not alifestd_is_asexual(phylogeny_df):
+        raise ValueError("phylogeny_df must be strictly asexual")
+
     phylogeny_df = alifestd_try_add_ancestor_id_col(phylogeny_df, mutate=True)
-    assert "ancestor_id" in phylogeny_df
+
+    if "num_children" not in phylogeny_df.columns:
+        phylogeny_df = alifestd_mark_num_children_asexual(
+            phylogeny_df, mutate=True
+        )
+    if "is_root" not in phylogeny_df.columns:
+        phylogeny_df = alifestd_mark_roots(phylogeny_df, mutate=True)
 
     phylogeny_df["is_unifurcating_root"] = (
         phylogeny_df["num_children"] == 1
     ) & phylogeny_df["is_root"]
 
-    phylogeny_df.set_index("id", drop=False, inplace=True)
+    if alifestd_has_contiguous_ids(phylogeny_df):
+        phylogeny_df.reset_index(drop=True, inplace=True)
+    else:
+        phylogeny_df.set_index("id", drop=False, inplace=True)
+
     phylogeny_df["ancestor_is_unifurcating_root"] = phylogeny_df.loc[
-        phylogeny_df["ancestor_id"], "is_unifurcating_root"
+        phylogeny_df["ancestor_id"].values, "is_unifurcating_root"
     ].values
 
     phylogeny_df.loc[
-        phylogeny_df["ancestor_is_unifurcating_root"],
+        phylogeny_df["ancestor_is_unifurcating_root"].values,
         "ancestor_id",
     ] = phylogeny_df.loc[
         phylogeny_df["ancestor_is_unifurcating_root"],
