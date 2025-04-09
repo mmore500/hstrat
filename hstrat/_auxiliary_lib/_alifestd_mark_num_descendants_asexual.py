7de650e507c073359a6243171bf948b6e7d744e1
Matthew Andres Moreno <mmore500.login+gpg@gmail.com>
2025-04-09 12:22:07 -0400

    fixup! Implement fast path for alifestd_mark_num_descendants_asexual

--- hstrat/_auxiliary_lib/_alifestd_mark_num_descendants_asexual.py
+++ hstrat/_auxiliary_lib/_alifestd_mark_num_descendants_asexual.py
@@ -70,10 +70,10 @@ def alifestd_mark_num_descendants_asexual(
         phylogeny_df = alifestd_topological_sort(phylogeny_df, mutate=True)
 
     if alifestd_has_contiguous_ids(phylogeny_df):
-        phylogeny_df["num_descendants"] = (
-            alifestd_mark_num_descendants_asexual_fast_path(
-                phylogeny_df["ancestor_id"].to_numpy()
-            )
+        phylogeny_df[
+            "num_descendants"
+        ] = alifestd_mark_num_descendants_asexual_fast_path(
+            phylogeny_df["ancestor_id"].to_numpy()
         )
         return phylogeny_df
     else:
