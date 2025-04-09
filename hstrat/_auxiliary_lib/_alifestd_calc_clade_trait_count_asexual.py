4f9882f2d790924d9cb10a3b04ea1614abc41283
Matthew Andres Moreno <mmore500.login+gpg@gmail.com>
2025-04-09 15:09:49 -0400

    Fix unnecessary casting

--- hstrat/_auxiliary_lib/_alifestd_calc_clade_trait_count_asexual.py
+++ hstrat/_auxiliary_lib/_alifestd_calc_clade_trait_count_asexual.py
@@ -81,7 +81,7 @@ def alifestd_calc_clade_trait_count_asexual(
 
     if alifestd_is_working_format_asexual(phylogeny_df):
         return _alifestd_calc_clade_trait_count_asexual_fast_path(
-            phylogeny_df["ancestor_id"].to_numpy(dtype=int),
+            pd.to_numeric(phylogeny_df["ancestor_id"]).to_numpy(),
             phylogeny_df["alifestd_calc_trait_count_asexual"].to_numpy(),
         )
     else:
