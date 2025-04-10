869594c85089e688777790a7570f590e6e6e295e
Matthew Andres Moreno <mmore500.login+gpg@gmail.com>
2025-04-10 00:04:39 -0400

    Implement fast path for splay polytomies

--- hstrat/_auxiliary_lib/_alifestd_splay_polytomies.py
+++ hstrat/_auxiliary_lib/_alifestd_splay_polytomies.py
@@ -1,27 +1,30 @@
 import itertools as it
 import warnings
 
+import numpy as np
 import pandas as pd
 
 from ._alifestd_has_contiguous_ids import alifestd_has_contiguous_ids
 from ._alifestd_is_asexual import alifestd_is_asexual
 from ._alifestd_make_ancestor_list_col import alifestd_make_ancestor_list_col
 from ._alifestd_try_add_ancestor_id_col import alifestd_try_add_ancestor_id_col
+from ._jit import jit
+from ._jit_numba_dict_t import jit_numba_dict_t
+from ._jit_numpy_int64_t import jit_numpy_int64_t
 
 
-def _alifestd_splay_polytomies_slow_path(
-    phylogeny_df: pd.DataFrame,
+@jit(nopython=True)
+def _alifestd_splay_polytomies_fast_path(
+    ancestor_ids: np.ndarray,
 ) -> pd.DataFrame:
     """Implementation detail for alifestd_splay_polytomies."""
-    splay_to_lookup = dict()
-    splay_from_lookup = dict()
-    phylogeny_df.set_index("id", drop=False, inplace=True)
+    i64 = jit_numpy_int64_t
+    splay_to_lookup = jit_numba_dict_t.empty(key_type=i64, value_type=i64)
+    splay_from_lookup = jit_numba_dict_t.empty(key_type=i64, value_type=i64)
 
-    new_id_generator = it.count(phylogeny_df["id"].max() + 1)
-
-    new_rows = dict()
-    for id_, row in phylogeny_df.iterrows():
-        ancestor_id = row["ancestor_id"]
+    new_rows = jit_numba_dict_t.empty(key_type=i64, value_type=i64)
+    new_row_sources = jit_numba_dict_t.empty(key_type=i64, value_type=i64)
+    for id_, ancestor_id in enumerate(ancestor_ids):
         if ancestor_id == id_:  # root
             continue
 
@@ -30,48 +33,39 @@ def _alifestd_splay_polytomies_slow_path(
             splayed_to_id = ancestor_id
         else:
             splayed_from_id = splay_to_lookup[ancestor_id]
-            splayed_to_id = next(new_id_generator)
+            splayed_to_id = len(ancestor_ids) + len(new_rows)
 
-            phylogeny_df.at[id_, "ancestor_id"] = splayed_to_id
-            new_row = phylogeny_df.loc[ancestor_id].copy()
-            new_row["id"] = splayed_to_id
-            new_row["ancestor_id"] = splayed_from_id
-            new_rows[splayed_to_id] = new_row
+            ancestor_ids[id_] = splayed_to_id
+            new_rows[splayed_to_id] = splayed_from_id
+            new_row_sources[splayed_to_id] = ancestor_id
 
         splay_from_lookup[splayed_to_id] = splayed_from_id
         splay_to_lookup[ancestor_id] = splayed_to_id
 
     # drop unnecessary splays
-    dropped_splay_remap = dict()
+    dropped_splay_remap = jit_numba_dict_t.empty(key_type=i64, value_type=i64)
     for ancestor_id, splayed_to_id in splay_to_lookup.items():
         if ancestor_id != splayed_to_id:
             splayed_from_id = splay_from_lookup[splayed_to_id]
-            assert new_rows[splayed_to_id]["ancestor_id"] == splayed_from_id
+            assert new_rows[splayed_to_id] == splayed_from_id
             dropped_splay_remap[splayed_to_id] = splayed_from_id
             del new_rows[splayed_to_id]
+            del new_row_sources[splayed_to_id]
 
     # correct dropped splays
-    phylogeny_df["ancestor_id"] = (
-        phylogeny_df["ancestor_id"]
-        .map(
-            {
-                **{
-                    ancestor_id: ancestor_id
-                    for ancestor_id in phylogeny_df["ancestor_id"]
-                },
-                **dropped_splay_remap,
-            }
-        )
-        .astype(int)
+    for idx, ancestor_id in enumerate(ancestor_ids):
+        splayed_to_id = dropped_splay_remap.get(ancestor_id, ancestor_id)
+        ancestor_ids[idx] = splayed_to_id
+
+    new_ids = np.array([*new_rows.keys()], dtype=np.int64)
+    new_ancestor_ids = np.array([*new_rows.values()], dtype=np.int64)
+    new_source_ids = np.array(
+        [new_row_sources[new_id] for new_id in new_ids], dtype=np.int64
     )
-
-    appendum_df = pd.DataFrame(new_rows.values())
-    phylogeny_df = pd.concat([phylogeny_df, appendum_df], ignore_index=True)
-
-    return phylogeny_df.reset_index(drop=True)
+    return ancestor_ids, new_source_ids, new_ids, new_ancestor_ids
 
 
-def _alifestd_splay_polytomies_fast_path(
+def _alifestd_splay_polytomies_slow_path(
     phylogeny_df: pd.DataFrame,
 ) -> pd.DataFrame:
     """Implementation detail for alifestd_splay_polytomies."""
@@ -183,7 +177,19 @@ def alifestd_splay_polytomies(
     phylogeny_df = alifestd_try_add_ancestor_id_col(phylogeny_df, mutate=True)
 
     if alifestd_has_contiguous_ids(phylogeny_df):
-        phylogeny_df = _alifestd_splay_polytomies_fast_path(phylogeny_df)
+        phylogeny_df.reset_index(drop=True, inplace=True)
+        (
+            phylogeny_df["ancestor_id"],
+            new_source_ids,
+            new_ids,
+            new_ancestor_ids,
+        ) = _alifestd_splay_polytomies_fast_path(
+            phylogeny_df["ancestor_id"].to_numpy(dtype=np.int64)
+        )
+        addendum = phylogeny_df.loc[new_source_ids].copy()
+        addendum["id"] = new_ids
+        addendum["ancestor_id"] = new_ancestor_ids
+        phylogeny_df = pd.concat([phylogeny_df, addendum], ignore_index=True)
     else:
         phylogeny_df = _alifestd_splay_polytomies_slow_path(phylogeny_df)
 
