d673b503f71a93ba113f7aadc8130b6f5d94d890
Matthew Andres Moreno <mmore500.login+gpg@gmail.com>
2025-04-24 09:30:24 -0400

    Fix bugs/defaults for surface hex serialization

--- hstrat/serialization/_surf_from_hex.py
+++ hstrat/serialization/_surf_from_hex.py
@@ -1,4 +1,5 @@
 import types
+import typing
 
 from downstream import dsurf
 
@@ -10,10 +11,10 @@ def surf_from_hex(
     dstream_algo: types.ModuleType,
     *,
     dstream_S: int,
-    dstream_storage_bitoffset: int,
+    dstream_storage_bitoffset: typing.Optional[int] = None,
     dstream_storage_bitwidth: int,
-    dstream_T_bitoffset: int,
-    dstream_T_bitwidth: int,
+    dstream_T_bitoffset: int = 0,
+    dstream_T_bitwidth: int = 32,
 ) -> HereditaryStratigraphicSurface:
     """
     Deserialize a HereditaryStratigraphicSurface object from a hex string
@@ -31,13 +32,13 @@ def surf_from_hex(
         Hex string to be parsed, which can be uppercase or lowercase.
     dstream_algo: module
         Dstream algorithm for curation of retained differentia.
-    dstream_storage_bitoffset: int
+    dstream_storage_bitoffset: int, default dstream_T_bitwidth
         Number of bits before the storage.
     dstream_storage_bitwidth: int
         Number of bits used for storage.
-    dstream_T_bitoffset: int
+    dstream_T_bitoffset: int, default 0
         Number of bits before dstream_T.
-    dstream_T_bitwidth: int
+    dstream_T_bitwidth: int, default 32
         Number of bits used to store dstream_T.
     dstream_S: int
         Number of buffer sites available to store differentiae.
@@ -49,6 +50,9 @@ def surf_from_hex(
     surf_to_hex()
         Serialize a surface into a hex string.
     """
+    if dstream_storage_bitoffset is None:
+        dstream_storage_bitoffset = dstream_T_bitwidth
+    assert dstream_storage_bitwidth % dstream_S == 0
     return HereditaryStratigraphicSurface(
         dsurf.Surface.from_hex(
             hex_string,
@@ -59,4 +63,5 @@ def surf_from_hex(
             T_bitoffset=dstream_T_bitoffset,
             T_bitwidth=dstream_T_bitwidth,
         ),
+        stratum_differentia_bit_width=dstream_storage_bitwidth // dstream_S,
     )
