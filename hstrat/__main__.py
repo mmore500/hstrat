11b611ba7315c55a64f2786094c83e83f9db7320
Matthew Andres Moreno <mmore500.login+gpg@gmail.com>
2025-05-14 12:50:03 -0400

    Add CLI for alifestd_add_inner_leaves

--- hstrat/__main__.py
+++ hstrat/__main__.py
@@ -10,6 +10,7 @@ $ python3 -m hstrat.dataframe.surface_unpack_reconstruct
 $ python3 -m hstrat.dataframe.surface_postprocess_trie
 
 Available commands (experimental API):
+$ python3 -m hstrat._auxiliary_lib._alifestd_add_inner_leaves
 $ python3 -m hstrat._auxiliary_lib._alifestd_as_newick_asexual
 $ python3 -m hstrat._auxiliary_lib._alifestd_count_root_nodes
 $ python3 -m hstrat._auxiliary_lib._alifestd_downsample_tips_asexual
--- hstrat/_auxiliary_lib/_alifestd_add_inner_leaves.py
+++ hstrat/_auxiliary_lib/_alifestd_add_inner_leaves.py
@@ -1,8 +1,19 @@
+import argparse
+import logging
+import os
+
 import numpy as np
+import joinem
+from joinem._dataframe_cli import _add_parser_base, _run_dataframe_cli
 import pandas as pd
 
 from ._alifestd_make_ancestor_list_col import alifestd_make_ancestor_list_col
 from ._alifestd_mark_leaves import alifestd_mark_leaves
+from ._configure_prod_logging import configure_prod_logging
+from ._delegate_polars_implementation import delegate_polars_implementation
+from ._format_cli_description import format_cli_description
+from ._get_hstrat_version import get_hstrat_version
+from ._log_context_duration import log_context_duration
 
 
 def alifestd_add_inner_leaves(
@@ -48,3 +59,47 @@ def alifestd_add_inner_leaves(
         del inner_df["ancestor_id"]
 
     return pd.concat([phylogeny_df, inner_df], ignore_index=True)
+
+
+_raw_description = f"""{os.path.basename(__file__)} | (hstrat v{get_hstrat_version()}/joinem v{joinem.__version__})
+
+Create a zero-length branch with leaf node for each inner node.
+
+Data is assumed to be in alife standard format.
+
+Additional Notes
+================
+- Use `--eager-read` if modifying data file inplace.
+
+- This CLI entrypoint is experimental and may be subject to change.
+"""
+
+
+def _create_parser() -> argparse.ArgumentParser:
+    parser = argparse.ArgumentParser(
+        add_help=False,
+        description=format_cli_description(_raw_description),
+        formatter_class=argparse.RawTextHelpFormatter,
+    )
+    parser = _add_parser_base(
+        parser=parser,
+        dfcli_module="hstrat._auxiliary_lib._alifestd_add_inner_leaves",
+        dfcli_version=get_hstrat_version(),
+    )
+    return parser
+
+
+if __name__ == "__main__":
+    configure_prod_logging()
+
+    parser = _create_parser()
+    args, __ = parser.parse_known_args()
+    with log_context_duration(
+        "hstrat._auxiliary_lib._alifestd_add_inner_leaves", logging.info
+    ):
+        _run_dataframe_cli(
+            base_parser=parser,
+            output_dataframe_op=delegate_polars_implementation()(
+                alifestd_add_inner_leaves,
+            ),
+        )
