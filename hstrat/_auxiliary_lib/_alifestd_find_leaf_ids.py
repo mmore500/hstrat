69c898ea1d79f796e92a57c686be12cfad4d4e64
Matthew Andres Moreno <mmore500.login+gpg@gmail.com>
2026-02-22 11:29:25 -0500

    Tweak and factor out alifestd_find_leaf_ids fast path

--- hstrat/_auxiliary_lib/_alifestd_find_leaf_ids.py
+++ hstrat/_auxiliary_lib/_alifestd_find_leaf_ids.py
@@ -7,6 +7,17 @@ from ._alifestd_parse_ancestor_ids import alifestd_parse_ancestor_ids
 from ._alifestd_try_add_ancestor_id_col import alifestd_try_add_ancestor_id_col
 
 
+def _alifestd_find_leaf_ids_asexual_fast_path(
+    ancestor_ids: np.ndarray,
+) -> np.ndarray:
+    # root is self ref, but must exclude to handle only-root phylo
+    leaf_pos_filter = np.ones(len(ancestor_ids), dtype=np.bool_)
+    leaf_pos_filter[ancestor_ids] = ancestor_ids == np.arange(
+        len(ancestor_ids),
+    )
+    return np.flatnonzero(leaf_pos_filter)
+
+
 def alifestd_find_leaf_ids(phylogeny_df: pd.DataFrame) -> np.ndarray:
     """What ids are not listed in any `ancestor_list`?
 
@@ -16,16 +27,9 @@ def alifestd_find_leaf_ids(phylogeny_df: pd.DataFrame) -> np.ndarray:
     if alifestd_has_contiguous_ids(phylogeny_df):
         phylogeny_df = alifestd_try_add_ancestor_id_col(phylogeny_df)
         if "ancestor_id" in phylogeny_df:
-
-            # root is self ref, but must exclude to handle only-root phylo
-            internal_node_idxs = phylogeny_df["ancestor_id"].to_numpy()[
-                phylogeny_df["ancestor_id"] != phylogeny_df["id"]
-            ]
-
-            leaf_pos_filter = np.ones(len(phylogeny_df), dtype=np.bool_)
-            leaf_pos_filter[internal_node_idxs] = False
-
-            return np.flatnonzero(leaf_pos_filter)
+            return _alifestd_find_leaf_ids_asexual_fast_path(
+                phylogeny_df["ancestor_id"].to_numpy(),
+            )
 
     all_ids = ods.OrderedSet(phylogeny_df["id"])
     internal_ids = (
