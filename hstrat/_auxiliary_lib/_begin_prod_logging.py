ab8e5dd592bdb4f580a78647caaca5345f088fc8
Matthew Andres Moreno <mmore500.login+gpg@gmail.com>
2026-02-25 08:36:22 -0500

    Reword version log message

--- hstrat/_auxiliary_lib/_begin_prod_logging.py
+++ hstrat/_auxiliary_lib/_begin_prod_logging.py
@@ -6,4 +6,4 @@ from ._get_hstrat_version import get_hstrat_version
 
 def begin_prod_logging() -> None:
     configure_prod_logging()
-    logging.info(f"hstrat version {get_hstrat_version()}")
+    logging.info(f"hstrat v{get_hstrat_version()}")
